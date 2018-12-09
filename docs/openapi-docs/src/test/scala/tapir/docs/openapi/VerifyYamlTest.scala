24b0b8164e4fd6b448b7606b2dddb6b259328803
adamw <adamw@warski.org>
2018-12-09 13:25:00 +0100

    WIP

--- docs/openapi-docs/src/test/scala/tapir/docs/openapi/VerifyYamlTest.scala
+++ docs/openapi-docs/src/test/scala/tapir/docs/openapi/VerifyYamlTest.scala
@@ -2,24 +2,29 @@ package tapir.docs.openapi
 
 import org.scalatest.{FunSuite, Matchers}
 import tapir._
+import tapir.json.circe._
 import tapir.tests._
 import tapir.openapi.circe.yaml._
+import io.circe.generic.auto._
 
 import scala.io.Source
 
 class VerifyYamlTest extends FunSuite with Matchers {
 
-  val all_the_way: Endpoint[(FruitAmount, String), Unit, FruitAmount] = endpoint
+  val all_the_way: Endpoint[(FruitAmount, String), Unit, (FruitAmount, Int)] = endpoint
     .in(("fruit" / path[String] / "amount" / path[Int]).mapTo(FruitAmount))
     .in(query[String]("color"))
-    .out(stringBody.and(header[Int]("X-Role")).mapTo(FruitAmount))
+    .out(jsonBody[FruitAmount])
+    .out(header[Int]("X-Role"))
 
   test("should match the expected yaml") {
     def noIndentation(s: String) = s.replaceAll("[ \t]", "").trim
 
     val expectedYaml = noIndentation(Source.fromResource("expected.yml").getLines().mkString("\n"))
-    val actualYaml = noIndentation(List(in_query_query_out_string, all_the_way).toOpenAPI("Fruits", "1.0").toYaml)
 
-    actualYaml shouldBe expectedYaml
+    val actualYaml = List(in_query_query_out_string, all_the_way).toOpenAPI("Fruits", "1.0").toYaml
+    val actualYamlNoIndent = noIndentation(actualYaml)
+
+    actualYamlNoIndent shouldBe expectedYaml
   }
 }
