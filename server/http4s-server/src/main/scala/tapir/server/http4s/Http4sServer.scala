56d38a3366230abd92b5af87558c1016acb30ebe
ghostbuster91 <kghost0@gmail.com>
2018-12-30 11:50:54 +0100

    [Refactor] Remove unused imports

--- server/http4s-server/src/main/scala/tapir/server/http4s/Http4sServer.scala
+++ server/http4s-server/src/main/scala/tapir/server/http4s/Http4sServer.scala
@@ -1,17 +1,9 @@
 package tapir.server.http4s
 
-import cats.Applicative
-import cats.data._
 import cats.effect.Sync
-import cats.implicits._
-import org.http4s
-import org.http4s.headers.`Content-Type`
-import org.http4s.util.CaseInsensitiveString
-import org.http4s.{Charset, EntityBody, Header, Headers, HttpRoutes, Request, Response, Status}
-import tapir.internal.{ParamsToSeq, SeqToParams}
+import org.http4s.HttpRoutes
+import tapir.Endpoint
 import tapir.typelevel.ParamsAsArgs
-import tapir.{DecodeResult, Endpoint, EndpointIO, EndpointInput, GeneralCodec, MediaType}
-import java.nio.charset.{Charset => NioCharset}
 
 trait Http4sServer {
   implicit class RichHttp4sHttpEndpoint[I, E, O](e: Endpoint[I, E, O]) {
