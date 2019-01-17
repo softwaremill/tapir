c099328a280a228d7cc1037c1897045821dda18c
ghostbuster91 <kghost0@gmail.com>
2019-01-18 00:06:14 +0100

    [Refactor] Extract duplicated method

--- docs/openapi-docs/src/test/scala/tapir/docs/openapi/VerifyYamlTest.scala
+++ docs/openapi-docs/src/test/scala/tapir/docs/openapi/VerifyYamlTest.scala
@@ -18,8 +18,6 @@ class VerifyYamlTest extends FunSuite with Matchers {
     .out(header[Int]("X-Role"))
 
   test("should match the expected yaml") {
-    def noIndentation(s: String) = s.replaceAll("[ \t]", "").trim
-
     val expectedYaml = noIndentation(Source.fromResource("expected.yml").getLines().mkString("\n"))
 
     val actualYaml = List(in_query_query_out_string, all_the_way).toOpenAPI("Fruits", "1.0").toYaml
@@ -35,8 +33,6 @@ class VerifyYamlTest extends FunSuite with Matchers {
     .out(header[Int]("X-Role"))
 
   test("should match the expected yaml when schema is recursive") {
-    def noIndentation(s: String) = s.replaceAll("[ \t]", "").trim
-
     val expectedYaml = noIndentation(Source.fromResource("expected_recursive.yml").getLines().mkString("\n"))
 
     val actualYaml = endpoint_wit_recursive_structure.toOpenAPI("Fruits", "1.0").toYaml
@@ -46,4 +42,6 @@ class VerifyYamlTest extends FunSuite with Matchers {
   }
 
   case class F1(data: List[F1])
+
+  private def noIndentation(s: String) = s.replaceAll("[ \t]", "").trim
 }
