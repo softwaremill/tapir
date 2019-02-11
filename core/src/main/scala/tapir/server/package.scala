13c64bc6de3eb31ed1b9bdb730a246e620aaaa7a
adamw <adam@warski.org>
2019-02-11 18:32:31 +0100

    Add variance to type alias

--- core/src/main/scala/tapir/server/package.scala
+++ core/src/main/scala/tapir/server/package.scala
@@ -1,6 +1,6 @@
 package tapir
 
 package object server {
-  type StatusMapper[T] = T => StatusCode
+  type StatusMapper[-T] = T => StatusCode
   type DecodeFailureHandler[R] = (R, EndpointInput.Single[_], DecodeFailure) => DecodeFailureHandling
 }
