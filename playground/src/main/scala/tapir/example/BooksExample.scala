dee201da36187c632fac8d74e780ce3913391f6c
adamw <adam@warski.org>
2019-02-18 12:57:00 +0100

    Docs

--- playground/src/main/scala/tapir/example/BooksExample.scala
+++ playground/src/main/scala/tapir/example/BooksExample.scala
@@ -155,7 +155,7 @@ class SwaggerUI(yml: String) {
           path(SwaggerYml) {
             complete(yml)
           } ~
-          getFromResourceDirectory("META-INF/resources/webjars/swagger-ui/3.20.0/")
+          getFromResourceDirectory("META-INF/resources/webjars/swagger-ui/3.20.5/")
       }
 }
 
