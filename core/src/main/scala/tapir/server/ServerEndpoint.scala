fd0cf1d98c42ea83372c5485a942192c0c26188b
adamw <adam@warski.org>
2019-07-09 10:12:08 +0200

    Add a tag method to server endpoint

--- core/src/main/scala/tapir/server/ServerEndpoint.scala
+++ core/src/main/scala/tapir/server/ServerEndpoint.scala
@@ -1,4 +1,6 @@
 package tapir.server
 import tapir.Endpoint
 
-case class ServerEndpoint[I, E, O, +S, F[_]](endpoint: Endpoint[I, E, O, S], logic: I => F[Either[E, O]])
+case class ServerEndpoint[I, E, O, +S, F[_]](endpoint: Endpoint[I, E, O, S], logic: I => F[Either[E, O]]) {
+  def tag(t: String): ServerEndpoint[I, E, O, S, F] = copy(endpoint = endpoint.tag(t))
+}
