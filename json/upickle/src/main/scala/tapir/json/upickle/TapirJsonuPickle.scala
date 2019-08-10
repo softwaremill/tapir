2257cc0346698a826f70f980e06bb9d6da774a49
Justin Heyes-Jones <justinhj@gmail.com>
2019-08-10 09:19:44 -0700

    Tidy up imports

--- json/upickle/src/main/scala/tapir/json/upickle/TapirJsonuPickle.scala
+++ json/upickle/src/main/scala/tapir/json/upickle/TapirJsonuPickle.scala
@@ -2,22 +2,21 @@ package tapir.json.upickle
 
 import java.nio.charset.StandardCharsets
 import scala.util.{Try, Success, Failure}
+import tapir._
 import tapir.Codec.JsonCodec
 import tapir.DecodeResult.{Error, Value}
-import tapir._
-import upickle.default.{ReadWriter => RW}
-import upickle.default.{read, write}
+import upickle.default.{ReadWriter, read, write}
 
 trait TapirJsonuPickle {
 
-  implicit def encoderDecoderCodec[T: RW: SchemaFor]: JsonCodec[T] = new JsonCodec[T] {
+  implicit def encoderDecoderCodec[T: ReadWriter: SchemaFor]: JsonCodec[T] = new JsonCodec[T] {
 
     def encode(t: T): String = write(t)
 
     def decode(s: String): DecodeResult[T] = {
       Try(read[T](s)) match {
         case Success(v) => Value(v)
-        case Failure(e) => Error("upickle decoder failed with:", e)
+        case Failure(e) => Error("upickle decoder failed", e)
       }
     }
 
