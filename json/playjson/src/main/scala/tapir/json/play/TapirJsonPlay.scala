2aa5154ee8376ededda2c42616aa214b2766f13d
Daniel Pfeiffer <d.pfeiffer404@gmail.com>
2019-08-29 09:05:07 +0200

    fix compilation

--- json/playjson/src/main/scala/tapir/json/play/TapirJsonPlay.scala
+++ json/playjson/src/main/scala/tapir/json/play/TapirJsonPlay.scala
@@ -10,12 +10,12 @@ import java.nio.charset.StandardCharsets
 
 trait TapirJsonPlay {
   implicit def readsWritesCodec[T: Reads: Writes: SchemaFor]: JsonCodec[T] = new JsonCodec[T] {
-    override def decode(s: String): DecodeResult[T] = implicitly[Reads[T]].reads(Json.parse(s)) match {
+    override def rawDecode(s: String): DecodeResult[T] = implicitly[Reads[T]].reads(Json.parse(s)) match {
       case JsError(errors)     => Error(s, JsResultException(errors))
       case JsSuccess(value, _) => Value(value)
     }
     override def encode(t: T): String = Json.prettyPrint(Json.toJson(t))
-    override def meta: CodecMeta[MediaType.Json, String] =
+    override def meta: CodecMeta[T, MediaType.Json, String] =
       CodecMeta(implicitly[SchemaFor[T]].schema, MediaType.Json(), StringValueType(StandardCharsets.UTF_8))
   }
 
