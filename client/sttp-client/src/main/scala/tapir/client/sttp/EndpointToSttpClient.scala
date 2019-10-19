0320d0135b0b95fd01652cbdc01e9f1fbc3907ee
ghostbuster91 <kghost0@gmail.com>
2019-10-19 08:51:00 +0200

    Simplify return type

--- client/sttp-client/src/main/scala/tapir/client/sttp/EndpointToSttpClient.scala
+++ client/sttp-client/src/main/scala/tapir/client/sttp/EndpointToSttpClient.scala
@@ -11,36 +11,35 @@ import tapir.internal._
 
 class EndpointToSttpClient(clientOptions: SttpClientOptions) {
   def toSttpRequest[I, E, O, S](e: Endpoint[I, E, O, S], baseUri: Uri): I => Request[Either[E, O], S] = { params =>
-    toSttpRequestRaw(e, baseUri)(params).mapResponse(_.bimap(getOrThrow, getOrThrow))
+    toSttpRequestRaw(e, baseUri)(params).mapResponse(getOrThrow)
   }
 
-  def toSttpRequestRaw[S, O, E, I](e: Endpoint[I, E, O, S], baseUri: Uri): I => Request[Either[DecodeResult[E], DecodeResult[O]], S] = {
-    params =>
-      val (uri, req1) =
-        setInputParams(
-          e.input.asVectorOfSingleInputs,
-          paramsTupleToParams(params),
-          0,
-          baseUri,
-          basicRequest.asInstanceOf[PartialAnyRequest]
-        )
+  def toSttpRequestRaw[S, O, E, I](e: Endpoint[I, E, O, S], baseUri: Uri): I => Request[DecodeResult[Either[E, O]], S] = { params =>
+    val (uri, req1) =
+      setInputParams(
+        e.input.asVectorOfSingleInputs,
+        paramsTupleToParams(params),
+        0,
+        baseUri,
+        basicRequest.asInstanceOf[PartialAnyRequest]
+      )
 
-      val req2 = req1.copy[Identity, Any, Any](method = sttp.model.Method(e.input.method.getOrElse(Method.GET).method), uri = uri)
+    val req2 = req1.copy[Identity, Any, Any](method = sttp.model.Method(e.input.method.getOrElse(Method.GET).method), uri = uri)
 
-      val responseAs = fromMetadata { meta =>
-        val output = if (meta.isSuccess) e.output else e.errorOutput
-        if (output == EndpointOutput.Void()) {
-          throw new IllegalStateException(s"Got response: $meta, cannot map to a void output of: $e.")
-        }
-
-        responseAsFromOutputs(meta, output)
-      }.mapWithMetadata { (body, meta) =>
-        val output = if (meta.isSuccess) e.output else e.errorOutput
-        val params = getOutputParams(output.asVectorOfSingleOutputs, body, meta)
-        if (meta.isSuccess) Right(params) else Left(params)
+    val responseAs = fromMetadata { meta =>
+      val output = if (meta.isSuccess) e.output else e.errorOutput
+      if (output == EndpointOutput.Void()) {
+        throw new IllegalStateException(s"Got response: $meta, cannot map to a void output of: $e.")
       }
 
-      req2.response(responseAs).asInstanceOf[Request[Either[DecodeResult[E], DecodeResult[O]], S]]
+      responseAsFromOutputs(meta, output)
+    }.mapWithMetadata { (body, meta) =>
+      val output = if (meta.isSuccess) e.output else e.errorOutput
+      val params = getOutputParams(output.asVectorOfSingleOutputs, body, meta)
+      params.map(p => if (meta.isSuccess) Right(p) else Left(p))
+    }
+
+    req2.response(responseAs).asInstanceOf[Request[DecodeResult[Either[E, O]], S]]
   }
 
   private def getOutputParams(outputs: Vector[EndpointOutput.Single[_]], body: Any, meta: ResponseMetadata): DecodeResult[Any] = {
@@ -247,14 +246,5 @@ class EndpointToSttpClient(clientOptions: SttpClientOptions) {
     case DecodeResult.Value(v)    => v
     case DecodeResult.Error(o, e) => throw new IllegalArgumentException(s"Cannot decode from $o", e)
     case f                        => throw new IllegalArgumentException(s"Cannot decode: $f")
-  }
-
-  private implicit class EitherBimap[L, R](either: Either[L, R]) {
-    def bimap[LL, RR](lf: L => LL, rf: R => RR): Either[LL, RR] = {
-      either match {
-        case Left(value)  => Left(lf(value))
-        case Right(value) => Right(rf(value))
-      }
-    }
   }
 }
