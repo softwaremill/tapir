acac98b002bce0eb53f50a9b23c6bf59a9d7a0fa
Richard Chuo <richard.chuo@gmail.com>
2019-11-14 01:20:33 +0800

    Support newer (after 19.8.0) Finatra
    Updated Tapir’s good Finatra support to accommodate newer Finatra version’s internal RouteDSL’s changes.

--- server/finatra-server/src/main/scala/sttp/tapir/server/finatra/TapirController.scala
+++ server/finatra-server/src/main/scala/sttp/tapir/server/finatra/TapirController.scala
@@ -1,10 +1,36 @@
 package sttp.tapir.server.finatra
 
+import com.twitter.finagle.http.Method._
 import com.twitter.finatra.http.Controller
 
 trait TapirController { self: Controller =>
   def addTapirRoute(route: FinatraRoute): Unit = {
-    any(route.path)(route.handler)
-    any(route.path + "/")(route.handler)
+    route.method match {
+      case Get =>
+        get(route.path + "/?")(route.handler)
+      case Post =>
+        post(route.path + "/?")(route.handler)
+      case Put =>
+        put(route.path + "/?")(route.handler)
+      case Head =>
+        head(route.path + "/?")(route.handler)
+      case Patch =>
+        patch(route.path + "/?")(route.handler)
+      case Delete =>
+        delete(route.path + "/?")(route.handler)
+      case Trace =>
+        // TODO - function name: trace conflicts with Logging's
+        any(route.path + "/?")(route.handler)
+      case Options =>
+        options(route.path + "/?")(route.handler)
+      case _ =>
+        get(route.path + "/?")(route.handler)
+        post(route.path + "/?")(route.handler)
+        put(route.path + "/?")(route.handler)
+        head(route.path + "/?")(route.handler)
+        patch(route.path + "/?")(route.handler)
+        delete(route.path + "/?")(route.handler)
+        options(route.path + "/?")(route.handler)
+    }
   }
 }
