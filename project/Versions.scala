fe3cc702b35c466e9a2b4159298cb1f5dd80333c
Scala Steward <me@scala-steward.org>
2019-11-18 17:28:06 +0100

    Update swagger-ui to 3.24.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val sttp = "2.0.0-RC1"
   val akkaHttp = "10.1.10"
   val akkaStreams = "2.6.0"
-  val swaggerUi = "3.24.0"
+  val swaggerUi = "3.24.3"
   val upickle = "0.8.0"
   val playJson = "2.7.4"
   val silencer = "1.4.4"
