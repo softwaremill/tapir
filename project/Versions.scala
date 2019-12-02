8ba7a26ee5d1572c900dff6f8e9e4d1055f064e7
Scala Steward <me@scala-steward.org>
2019-12-02 12:07:08 +0100

    Update akka-http to 10.1.11

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val circe = "0.12.3"
   val circeYaml = "0.12.0"
   val sttp = "2.0.0-RC3"
-  val akkaHttp = "10.1.10"
+  val akkaHttp = "10.1.11"
   val akkaStreams = "2.6.0"
   val swaggerUi = "3.24.3"
   val upickle = "0.8.0"
