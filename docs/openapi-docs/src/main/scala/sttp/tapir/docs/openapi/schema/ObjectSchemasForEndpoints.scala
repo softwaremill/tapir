515bc2e192e7277ff28b0bc42e16277124279f46
ghostbuster91 <kghost0@gmail.com>
2020-01-06 18:50:26 +0100

    Revert ObjectSchemasForEndpoints to old impl

--- docs/openapi-docs/src/main/scala/sttp/tapir/docs/openapi/schema/ObjectSchemasForEndpoints.scala
+++ docs/openapi-docs/src/main/scala/sttp/tapir/docs/openapi/schema/ObjectSchemasForEndpoints.scala
@@ -7,6 +7,7 @@ import sttp.tapir.openapi.OpenAPI.ReferenceOr
 import sttp.tapir.openapi.{Reference, Schema => OSchema}
 import sttp.tapir.{Schema => TSchema, SchemaType => TSchemaType, _}
 
+import scala.collection.immutable
 import scala.collection.immutable.ListMap
 import scala.collection.mutable.ListBuffer
 
@@ -81,7 +82,7 @@ object ObjectSchemasForEndpoints {
 
   private def objectSchemas(typeData: TypeData[_]): List[ObjectTypeData] = {
     typeData match {
-      case TypeData(s @ TSchema(st: TSchemaType.SProduct, _, _, _), validator: Validator.Product[_]) =>
+      case TypeData(s @ TSchema(st: TSchemaType.SProduct, _, _, _), validator) =>
         List(st.info -> TypeData(s, validator): ObjectTypeData) ++ fieldsSchemaWithValidator(st, validator)
           .flatMap(objectSchemas)
           .toList
@@ -99,12 +100,15 @@ object ObjectSchemasForEndpoints {
     }
   }
 
-  private def fieldsSchemaWithValidator(p: TSchemaType.SProduct, v: Validator.Product[_]): Seq[TypeData[_]] = {
-    p.fields.map { f =>
-      TypeData(f._2, v.fields(f._1).validator)
-    }.toList
+  private def fieldsSchemaWithValidator(p: TSchemaType.SProduct, v: Validator[_]): immutable.Seq[TypeData[_]] = {
+    v match {
+      case Validator.Product(validatedFields) =>
+        p.fields.map { f =>
+          TypeData(f._2, validatedFields.get(f._1).map(_.validator).getOrElse(Validator.pass))
+        }.toList
+      case _ => p.fields.map(f => TypeData(f._2, Validator.pass)).toList
+    }
   }
-
   private def subtypesSchemaWithValidator(st: TSchemaType.SCoproduct, v: Validator.Coproduct[_]): Seq[TypeData[_]] = {
     st.schemas.collect {
       case s @ TSchema(st: TSchemaType.SProduct, _, _, _) => TypeData(s, v.subtypes(st.info.fullName))
