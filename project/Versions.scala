1df90326ad2000a9f52a1b6e50f6ff4cc05ada32
Scala Steward <me@scala-steward.org>
2020-02-17 19:29:36 +0100

    Update client:akka-http-backend, ... to 2.0.0-RC10

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val catsEffect = "2.1.1"
   val circe = "0.13.0"
   val circeYaml = "0.12.0"
-  val sttp = "2.0.0-RC9"
+  val sttp = "2.0.0-RC10"
   val akkaHttp = "10.1.11"
   val akkaStreams = "2.6.3"
   val swaggerUi = "3.25.0"
