4cad3d0e291bf608a15492c1b93f3bd1dc077039
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2020-03-10 12:08:46 +0100

    Simplify the stub and remove dependencies on Cats and other stuff

--- server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpStubServer.scala
+++ server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpStubServer.scala
@@ -1,11 +1,7 @@
 package sttp.tapir.server.stub
 
-import cats.FlatMap
-import cats.effect.Effect
-import cats.effect.syntax.effect._
-import cats.syntax.applicative._
-import cats.syntax.functor._
 import sttp.client.monad.MonadError
+import sttp.client.monad.syntax._
 import sttp.client.testing.SttpBackendStub
 import sttp.client.{NothingT, Request, Response, SttpBackend}
 import sttp.model.StatusCode
@@ -15,51 +11,61 @@ import sttp.tapir.server.{DecodeFailureContext, DecodeFailureHandling, ServerDef
 import sttp.tapir.{DecodeFailure, Endpoint, EndpointInput}
 
 trait SttpStubServer {
-  implicit class RichStubServerEndpoints[F[+_]: FlatMap: Effect](
+
+  implicit class RichStubServerEndpoints[F[+_]](
       endpoints: Iterable[ServerEndpoint[_, _, _, _, F]]
   ) {
-    def toBackendStub(implicit me: MonadError[F], serverOptions: SttpServerOptions[F]): SttpBackend[F, Nothing, NothingT] = {
+    def toBackendStub(implicit me: MonadError[F]): SttpBackend[F, Nothing, NothingT] = {
 
-      def pf[I](se: ServerEndpoint[I, _, _, _, F]) = new PartialFunction[Request[_, _], Response[_]] {
-        val endpoint: Endpoint[_, _, _, _] = se.endpoint
+      def pf[I](se: ServerEndpoint[I, _, _, _, F]): PartialFunction[Request[_, _], F[Response[_]]] =
+        new PartialFunction[Request[_, _], F[Response[_]]] {
+          val endpoint: Endpoint[_, _, _, _] = se.endpoint
 
-        override def isDefinedAt(req: Request[_, _]): Boolean = {
-          val decodeInputResult = DecodeInputs(endpoint.input, new SttpDecodeInput(req))
-          decodeInputResult match {
-            case DecodeInputsResult.Failure(input, failure) =>
-              handleDecodeFailure(input, failure).code != StatusCode.NotFound
-            case DecodeInputsResult.Values(_, _) => true
+          override def isDefinedAt(req: Request[_, _]): Boolean = {
+            val decodeInputResult = DecodeInputs(endpoint.input, new SttpDecodeInput(req))
+            decodeInputResult match {
+              case DecodeInputsResult.Failure(input, failure) =>
+                handleDecodeFailure(input, failure).code != StatusCode.NotFound
+              case DecodeInputsResult.Values(_, _) => true
+            }
+          }
+
+          override def apply(req: Request[_, _]): F[Response[_]] = {
+            DecodeInputs(endpoint.input, new SttpDecodeInput(req)) match {
+              case values: DecodeInputsResult.Values =>
+                se.logic(SeqToParams(InputValues(se.endpoint.input, values)).asInstanceOf[I])
+                  .map {
+                    case Right(result) => Response(result, ServerDefaults.StatusCodes.success)
+                    case Left(err)     => Response(err, StatusCode.InternalServerError)
+                  }
+              case DecodeInputsResult.Failure(input, failure) =>
+                me.unit(handleDecodeFailure(input, failure))
+            }
           }
         }
 
-        override def apply(req: Request[_, _]): Response[_] = {
-          val decodeInputResult = DecodeInputs(endpoint.input, new SttpDecodeInput(req))
-          val effectfulResponse: F[Response[_]] = decodeInputResult match {
-            case values: DecodeInputsResult.Values =>
-              se.logic(SeqToParams(InputValues(se.endpoint.input, values)).asInstanceOf[I])
-                .map {
-                  case Right(result) => Response(result, ServerDefaults.StatusCodes.success)
-                  case Left(err)     => Response(err, StatusCode.InternalServerError)
-                }
-            case DecodeInputsResult.Failure(input, failure) =>
-              handleDecodeFailure(input, failure).pure[F]
-          }
-          effectfulResponse.toIO.unsafeRunSync()
-        }
-      }
+      val initialPf: PartialFunction[Request[_, _], F[Response[_]]] = PartialFunction.empty
 
-      endpoints.foldLeft(SttpBackendStub(me)) {
-        case (backendStub, endpoint) =>
-          backendStub.whenRequestMatchesPartial(pf(endpoint))
+      val matchers = endpoints.foldLeft(initialPf) {
+        case (currentPf, endpoint) =>
+          currentPf.orElse(pf(endpoint))
       }
+      val clz = classOf[SttpBackendStub[F, Nothing]] // TODO reflection hack until stub constructor becomes public
+      val ctor = clz.getDeclaredConstructor(
+        classOf[MonadError[F]],
+        classOf[PartialFunction[Request[_, _], F[Response[_]]]],
+        classOf[Option[SttpBackend[F, Nothing, NothingT]]]
+      )
+      ctor.setAccessible(true)
+      ctor.newInstance(me, matchers, None)
     }
 
     private def handleDecodeFailure(
         input: EndpointInput.Single[_],
         failure: DecodeFailure
-    )(implicit serverOptions: SttpServerOptions[F]): Response[_] = {
+    ): Response[_] = {
       val decodeFailureCtx = DecodeFailureContext(input, failure)
-      val handling = serverOptions.decodeFailureHandler(decodeFailureCtx)
+      val handling = ServerDefaults.decodeFailureHandler(decodeFailureCtx)
       handling match {
         case DecodeFailureHandling.NoMatch =>
           Response(failure.toString, StatusCode.NotFound)
