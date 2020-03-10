24986c2dc48e5366aee9407622b792f6908b3120
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2020-03-10 14:01:28 +0100

    Add API for calling endpoint.testUsing(input)

--- server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/package.scala
+++ server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/package.scala
@@ -1,3 +1,28 @@
 package sttp.tapir.server
 
-package object stub extends SttpStubServer
+import sttp.client.monad.MonadError
+import sttp.client.monad.syntax._
+import sttp.client.{NothingT, SttpBackend, _}
+import sttp.tapir.client.sttp._
+import sttp.tapir.server.internal.{DecodeInputs, DecodeInputsResult}
+
+package object stub extends SttpStubServer {
+
+  implicit class StubbedEndpoint[I, E, O, F[_]: MonadError](
+      serverEndpoint: ServerEndpoint[I, E, O, Nothing, F]
+  ) {
+    def testUsing(input: I): Option[F[Either[E, O]]] = {
+      implicit val stubBackend: SttpBackend[F, Nothing, NothingT] = List(serverEndpoint).toBackendStub
+      val req = serverEndpoint.endpoint.toSttpRequestUnsafe(uri"http://test.com").apply(input)
+
+      val decodeInputResult = DecodeInputs(serverEndpoint.endpoint.input, new SttpDecodeInput(req))
+      decodeInputResult match {
+        case DecodeInputsResult.Failure(_, _) =>
+          None
+        case DecodeInputsResult.Values(_, _) =>
+          Some(req.send().map(_.body))
+      }
+    }
+  }
+
+}
