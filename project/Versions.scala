f7d0d3b98bae45e45823da377533f8b54a53d94b
Scala Steward <me@scala-steward.org>
2020-03-12 02:47:04 +0100

    Update zio to 1.0.0-RC18-2

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,6 +17,6 @@ object Versions {
   val scalaTest = "3.0.8"
   val refined = "0.9.13"
   val enumeratum = "1.5.15"
-  val zio = "1.0.0-RC18"
+  val zio = "1.0.0-RC18-2"
   val zioInteropCats = "2.0.0.0-RC11"
 }
