fc046c56136b31112bb146a743e305952b5ed7a5
matwojcik <mat.wojcik@gmail.com>
2020-03-17 16:18:47 +0100

    Converter reused + removed redundant methods

--- docs/openapi-docs/src/main/scala/sttp/tapir/docs/openapi/CodecToMediaType.scala
+++ docs/openapi-docs/src/main/scala/sttp/tapir/docs/openapi/CodecToMediaType.scala
@@ -1,51 +1,21 @@
 package sttp.tapir.docs.openapi
 
 import sttp.tapir.docs.openapi.schema.{ObjectSchemas, TypeData}
-import sttp.tapir.openapi.{MediaType => OMediaType, _}
+import sttp.tapir.openapi.{MediaType => OMediaType}
 import sttp.tapir.{CodecFormat, EndpointIO, Schema => TSchema, _}
 
 import scala.collection.immutable.ListMap
 
 private[openapi] class CodecToMediaType(objectSchemas: ObjectSchemas) {
-  def apply[T, CF <: CodecFormat](o: CodecForOptional[T, CF, _], example: Option[T]): ListMap[String, OMediaType] = {
-    ListMap(
-      o.meta.format.mediaType.noCharset.toString -> OMediaType(
-        Some(objectSchemas(o)),
-        example.flatMap(exampleValue(o, _)),
-        ListMap.empty,
-        ListMap.empty
-      )
-    )
-  }
 
   def apply[T, CF <: CodecFormat](o: CodecForOptional[T, CF, _], examples: List[EndpointIO.Example[T]]): ListMap[String, OMediaType] = {
-    val (singleExample, multipleExamples) = splitExamples(examples)
+    val convertedExamples = ExampleConverter.convertExamples(o, examples)
 
     ListMap(
       o.meta.format.mediaType.noCharset.toString -> OMediaType(
         Some(objectSchemas(o)),
-        singleExample.flatMap(example => exampleValue(o, example.value)),
-        multipleExamples.zipWithIndex.map{
-          case (example, i) =>
-            example.name.getOrElse(s"Example$i") -> Right(Example(summary = example.summary, description = None, value = exampleValue(o, example.value), externalValue = None))
-        }.toListMap
-        ,
-        ListMap.empty
-      )
-    )
-  }
-
-
-  def apply[CF <: CodecFormat](
-      schema: TSchema[_],
-      format: CF,
-      example: Option[String]
-  ): ListMap[String, OMediaType] = {
-    ListMap(
-      format.mediaType.noCharset.toString -> OMediaType(
-        Some(objectSchemas(TypeData(schema, Validator.pass))),
-        example.map(ExampleSingleValue),
-        ListMap.empty,
+        convertedExamples.singleExample,
+        convertedExamples.multipleExamples,
         ListMap.empty
       )
     )
@@ -56,28 +26,15 @@ private[openapi] class CodecToMediaType(objectSchemas: ObjectSchemas) {
                                 format: CF,
                                 examples: List[EndpointIO.Example[String]]
                               ): ListMap[String, OMediaType] = {
-    val (singleExample, multipleExamples) = splitExamples(examples)
+    val convertedExamples = ExampleConverter.convertExamples(Codec.stringPlainCodecUtf8, examples)
 
     ListMap(
       format.mediaType.noCharset.toString -> OMediaType(
         Some(objectSchemas(TypeData(schema, Validator.pass))),
-        singleExample.map(example => ExampleSingleValue(example.value)),
-        multipleExamples.zipWithIndex.map{
-          case (example, i) =>
-            example.name.getOrElse(s"Example$i") -> Right(Example(summary = example.summary, description = None, value = Some(ExampleSingleValue(example.value)), externalValue = None))
-        }.toListMap,
+        convertedExamples.singleExample,
+        convertedExamples.multipleExamples,
         ListMap.empty
       )
     )
   }
-
-  private def splitExamples[T](examples: List[EndpointIO.Example[T]]): (Option[EndpointIO.Example[T]], List[EndpointIO.Example[T]]) =
-    examples match {
-      case (example@EndpointIO.Example(_, None, _)) :: Nil =>
-        (Some(example), Nil)
-      case Nil =>
-        (None, Nil)
-      case examples =>
-        (None, examples)
-    }
 }
