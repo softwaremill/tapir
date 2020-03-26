8c7a6029e4935ff950faa718bc7f781a69c1d4ec
ghostbuster91 <kghost0@gmail.com>
2020-03-26 22:13:11 +0100

    Set protocol to HTTP/1.1

--- server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpStubServerRequest.scala
+++ server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpStubServerRequest.scala
@@ -8,7 +8,7 @@ import sttp.tapir.model.{ConnectionInfo, ServerRequest}
 
 class SttpStubServerRequest[_, S](req: Request[_, S]) extends ServerRequest {
   override def method: Method = Method(req.method.method)
-  override def protocol: String = "42"
+  override def protocol: String = "HTTP/1.1"
   override def uri: URI = new URI(req.uri.toString())
   override def connectionInfo: ConnectionInfo = ConnectionInfo(None, None, None)
   override def headers: Seq[(String, String)] = req.headers.toList.map(h => (h.name, h.value))
