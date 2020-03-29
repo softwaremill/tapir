05da63fac5f37d5dc992cd5b22677da3ae2f505b
ghostbuster91 <kghost0@gmail.com>
2020-03-29 17:07:04 +0200

    Drop testusing

--- server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpStubServer.scala
+++ server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpStubServer.scala
@@ -1,72 +1,12 @@
 package sttp.tapir.server.stub
 
-import sttp.client.monad.MonadError
-import sttp.client.monad.syntax._
 import sttp.client.testing.SttpBackendStub
-import sttp.client.{NothingT, Request, Response, SttpBackend}
 import sttp.model.StatusCode
 import sttp.tapir.internal.SeqToParams
 import sttp.tapir.server.internal.{DecodeInputs, DecodeInputsResult, InputValues}
-import sttp.tapir.server.{DecodeFailureContext, DecodeFailureHandling, ServerDefaults, ServerEndpoint}
-import sttp.tapir.{DecodeFailure, Endpoint, EndpointInput}
+import sttp.tapir.{DecodeFailure, Endpoint}
 
 trait SttpStubServer {
-
-  implicit class RichStubServerEndpoints[F[_]](
-      endpoints: Iterable[ServerEndpoint[_, _, _, _, F]]
-  ) {
-    def toBackendStub(implicit me: MonadError[F]): SttpBackend[F, Nothing, NothingT] = {
-
-      def pf[I](se: ServerEndpoint[I, _, _, _, F]): PartialFunction[Request[_, _], F[Response[_]]] =
-        new PartialFunction[Request[_, _], F[Response[_]]] {
-          val endpoint: Endpoint[_, _, _, _] = se.endpoint
-
-          override def isDefinedAt(req: Request[_, _]): Boolean = {
-            val decodeInputResult = DecodeInputs(endpoint.input, new SttpDecodeInputs(req))
-            decodeInputResult match {
-              case DecodeInputsResult.Failure(input, failure) =>
-                handleDecodeFailure(input, failure).code != StatusCode.NotFound
-              case DecodeInputsResult.Values(_, _) => true
-            }
-          }
-
-          override def apply(req: Request[_, _]): F[Response[_]] = {
-            DecodeInputs(endpoint.input, new SttpDecodeInputs(req)) match {
-              case values: DecodeInputsResult.Values =>
-                se.logic(SeqToParams(InputValues(se.endpoint.input, values)).asInstanceOf[I])
-                  .map {
-                    case r @ Right(_) => Response(r, ServerDefaults.StatusCodes.success)
-                    case l @ Left(_)  => Response(l, StatusCode.InternalServerError)
-                  }
-              case DecodeInputsResult.Failure(input, failure) =>
-                me.unit(handleDecodeFailure(input, failure))
-            }
-          }
-        }
-
-      val initialPf: PartialFunction[Request[_, _], F[Response[_]]] = PartialFunction.empty
-
-      val matchers = endpoints.foldLeft(initialPf) {
-        case (currentPf, endpoint) =>
-          currentPf.orElse(pf(endpoint))
-      }
-      new SttpBackendStub(me, matchers, None)
-    }
-
-    private def handleDecodeFailure(
-        input: EndpointInput.Single[_],
-        failure: DecodeFailure
-    ): Response[_] = {
-      val decodeFailureCtx = DecodeFailureContext(input, failure)
-      val handling = ServerDefaults.decodeFailureHandler(decodeFailureCtx)
-      handling match {
-        case DecodeFailureHandling.NoMatch =>
-          Response(failure.toString, StatusCode.NotFound)
-        case DecodeFailureHandling.RespondWithResponse(_, value) =>
-          Response(value, ServerDefaults.StatusCodes.error)
-      }
-    }
-  }
 
   implicit class RichSttpBackendStub[F[_], S](val stub: SttpBackendStub[F, S]) {
     def whenRequestMatches[E, O](endpoint: Endpoint[_, E, O, _]): TypeAwareWhenRequest[E, O] = {
@@ -116,8 +56,6 @@ trait SttpStubServer {
 
       def thenError(errorResponse: E, statusCode: StatusCode): SttpBackendStub[F, S] =
         whenRequest.thenRespond(sttp.client.Response[E](errorResponse, statusCode))
-
-      def thenBadRequest(): SttpBackendStub[F, S] = whenRequest.thenRespondWithCode(StatusCode.BadRequest)
 
       def genericResponse: stub.WhenRequest = whenRequest
     }
--- server/sttp-stub-server/src/test/scala/sttp/tapir/server/stub/SttpStubServerTest.scala
+++ server/sttp-stub-server/src/test/scala/sttp/tapir/server/stub/SttpStubServerTest.scala
@@ -6,93 +6,13 @@ import sttp.client._
 import sttp.client.monad._
 import sttp.client.testing.SttpBackendStub
 import sttp.model.StatusCode
-import sttp.tapir.DecodeResult.InvalidValue
 import sttp.tapir._
 import sttp.tapir.json.circe._
-import sttp.tapir.server.ServerEndpoint
 
 class SttpStubServerTest extends FlatSpec with Matchers {
 
   behavior of "SttpStubServer"
   implicit val idMonad: MonadError[Identity] = IdMonad
-
-  it should "stub a simple endpoint with custom logic" in {
-    // given
-    val endpoint = sttp.tapir.endpoint
-      .in("api" / "sometest")
-      .get
-      .out(jsonBody[ResponseWrapper])
-
-    val endpointWithStubLogic: ServerEndpoint[Unit, Unit, ResponseWrapper, Nothing, Identity] = endpoint.serverLogic[Identity] { _ =>
-      Right(ResponseWrapper(44.414))
-    }
-
-    // when
-    val response = new StubbedEndpoint(endpointWithStubLogic).testUsing(())
-
-    // then
-    response shouldBe Some(Right(ResponseWrapper(44.414)))
-  }
-
-  it should "stub an endpoint with error response" in {
-    // given
-    val endpoint = sttp.tapir.endpoint
-      .in("api" / "sometest2")
-      .get
-      .out(jsonBody[ResponseWrapper])
-      .errorOut(statusCode and jsonBody[TestError])
-
-    val endpointWithStubLogic: ServerEndpoint[Unit, (StatusCode, TestError), ResponseWrapper, Nothing, Identity] =
-      endpoint.serverLogic[Identity] { _ => Left((StatusCode.BadRequest, ExactError("Aaargh!"))) }
-
-    // when
-    val response = new StubbedEndpoint(endpointWithStubLogic).testUsing(())
-
-    // then
-    response shouldBe Some(Left(((StatusCode.BadRequest, ExactError("Aaargh!")))))
-  }
-
-  it should "stub an endpoint with failed validation" in {
-    // given
-    val endpoint = sttp.tapir.endpoint
-      .in("api" / "sometest3")
-      .in(
-        query[Int]("amount")
-          .validate(Validator.min(0))
-      )
-      .post
-      .out(jsonBody[ResponseWrapper])
-
-    val endpointWithStubLogic: ServerEndpoint[Int, Unit, ResponseWrapper, Nothing, Identity] =
-      endpoint.serverLogic[Identity] { _ => Right(ResponseWrapper(0.3)) }
-
-    // when
-    val response = new StubbedEndpoint(endpointWithStubLogic).testUsing(-1)
-
-    // then
-    response shouldBe None
-  }
-
-  it should "stub an endpoint with passing validation" in {
-    // given
-    val endpoint = sttp.tapir.endpoint
-      .in("api" / "sometest4")
-      .in(
-        query[Int]("amount")
-          .validate(Validator.min(0))
-      )
-      .post
-      .out(jsonBody[ResponseWrapper])
-
-    val endpointWithStubLogic: ServerEndpoint[Int, Unit, ResponseWrapper, Nothing, Identity] =
-      endpoint.serverLogic[Identity] { in => Right(ResponseWrapper(0.33 * in)) }
-
-    // when
-    val response = new StubbedEndpoint(endpointWithStubLogic).testUsing(15)
-
-    // then
-    response shouldBe Some(Right(ResponseWrapper(4.95)))
-  }
 
   it should "combine tapir endpoint with sttp stub" in {
     import sttp.tapir.client.sttp._
@@ -154,7 +74,8 @@ class SttpStubServerTest extends FlatSpec with Matchers {
     implicit val backend = SttpBackendStub
       .apply(idMonad)
       .whenDecodingInputFailure(endpoint)
-      .thenBadRequest()
+      .genericResponse
+      .thenRespondWithCode(StatusCode.BadRequest)
     val response = endpoint.toSttpRequestUnsafe(uri"http://test.com").apply(-1).send()
 
     response shouldBe Response(Left(()), StatusCode.BadRequest)
