70fd7f02cda557094362014c1ae7696be0c98350
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-04-25 18:38:51 +0200

    rework blocking handlers && logging

--- server/vertx/src/test/scala/sttp/tapir/server/vertx/VertxBlockingServerTests.scala
+++ server/vertx/src/test/scala/sttp/tapir/server/vertx/VertxBlockingServerTests.scala
@@ -14,11 +14,11 @@ class VertxBlockingServerTests extends VertxServerTests with BeforeAndAfterEach 
   override def suspendResult[T](t: => T): Future[T] = vertx.executeBlocking(() => t)
 
   override def route[I, E, O](e: Endpoint[I, E, O, String], fn: I => Future[Either[E, O]], decodeFailureHandler: Option[DecodeFailureHandler]): Router => Route =
-    e.blockingRoute(fn, vertx)(options.copy(decodeFailureHandler.getOrElse(ServerDefaults.decodeFailureHandler)))
+    e.blockingRoute(fn)(options.copy(decodeFailureHandler.getOrElse(ServerDefaults.decodeFailureHandler)))
 
   override def routeRecoverErrors[I, E <: Throwable, O](e: Endpoint[I, E, O, String], fn: I => Future[O])
                                                        (implicit eClassTag: ClassTag[E]): Router => Route =
-    e.blockingRouteRecoverErrors(fn, vertx)
+    e.blockingRouteRecoverErrors(fn)
 
   override lazy val portCounter: PortCounter = new PortCounter(4000)
 
