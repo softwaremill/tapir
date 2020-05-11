78af214921775e7af9639c28688509ff5fc9a12c
adamw <adam@warski.org>
2020-05-11 10:48:26 +0200

    Param subtract implicits

--- core/src/main/scala/sttp/tapir/typelevel/ParamConcat.scala
+++ core/src/main/scala/sttp/tapir/typelevel/ParamConcat.scala
@@ -13,61 +13,66 @@ import com.github.ghik.silencer.silent
   *
   * Otherwise, the arity of a tuple.
   */
-trait ParamConcat[T, U] {
+trait ParamConcat[T, U] extends BinaryTupleOp {
   type Out
-
-  def leftArity: Int
-  def rightArity: Int
 }
 
 object ParamConcat extends LowPriorityTupleConcat4 {
-  implicit def concatUnitUnit[U]: Aux[Unit, Unit, Unit] = new ParamConcat[Unit, Unit] {
-    override type Out = Unit
-    override def leftArity = 0
-    override def rightArity = 0
-  }
-  implicit def concatNothingNothing[U]: Aux[Nothing, Nothing, Nothing] = new ParamConcat[Nothing, Nothing] {
-    override type Out = Nothing
-    override def leftArity = 0
-    override def rightArity = 0
-  }
-  implicit def concatNothingUnit[U]: Aux[Nothing, Unit, Unit] = new ParamConcat[Nothing, Unit] {
-    override type Out = Unit
-    override def leftArity = 0
-    override def rightArity = 0
-  }
-  implicit def concatUnitNothing[U]: Aux[Unit, Nothing, Unit] = new ParamConcat[Unit, Nothing] {
-    override type Out = Unit
-    override def leftArity = 0
-    override def rightArity = 0
-  }
+  implicit def concatUnitUnit[U]: Aux[Unit, Unit, Unit] =
+    new ParamConcat[Unit, Unit] {
+      override type Out = Unit
+      override def leftArity = 0
+      override def rightArity = 0
+    }
+  implicit def concatNothingNothing[U]: Aux[Nothing, Nothing, Nothing] =
+    new ParamConcat[Nothing, Nothing] {
+      override type Out = Nothing
+      override def leftArity = 0
+      override def rightArity = 0
+    }
+  implicit def concatNothingUnit[U]: Aux[Nothing, Unit, Unit] =
+    new ParamConcat[Nothing, Unit] {
+      override type Out = Unit
+      override def leftArity = 0
+      override def rightArity = 0
+    }
+  implicit def concatUnitNothing[U]: Aux[Unit, Nothing, Unit] =
+    new ParamConcat[Unit, Nothing] {
+      override type Out = Unit
+      override def leftArity = 0
+      override def rightArity = 0
+    }
 }
 
 trait LowPriorityTupleConcat4 extends LowPriorityTupleConcat3 {
-  implicit def concatUnitLeft[U](implicit ua: TupleArity[U]): Aux[Unit, U, U] = new ParamConcat[Unit, U] {
-    override type Out = U
-    override def leftArity: Int = 0
-    override def rightArity: Int = ua.arity
-  }
-  implicit def concatNothingLeft[U](implicit ua: TupleArity[U]): Aux[Nothing, U, U] = new ParamConcat[Nothing, U] {
-    override type Out = U
-    override def leftArity: Int = 0
-    override def rightArity: Int = ua.arity
-  }
+  implicit def concatUnitLeft[U](implicit ua: TupleArity[U]): Aux[Unit, U, U] =
+    new ParamConcat[Unit, U] {
+      override type Out = U
+      override def leftArity: Int = 0
+      override def rightArity: Int = ua.arity
+    }
+  implicit def concatNothingLeft[U](implicit ua: TupleArity[U]): Aux[Nothing, U, U] =
+    new ParamConcat[Nothing, U] {
+      override type Out = U
+      override def leftArity: Int = 0
+      override def rightArity: Int = ua.arity
+    }
 }
 
 trait LowPriorityTupleConcat3 extends LowPriorityTupleConcat2 {
-  implicit def concatUnitRight[T](implicit ta: TupleArity[T]): Aux[T, Unit, T] = new ParamConcat[T, Unit] {
-    override type Out = T
-    override def leftArity: Int = ta.arity
-    override def rightArity: Int = 0
-  }
+  implicit def concatUnitRight[T](implicit ta: TupleArity[T]): Aux[T, Unit, T] =
+    new ParamConcat[T, Unit] {
+      override type Out = T
+      override def leftArity: Int = ta.arity
+      override def rightArity: Int = 0
+    }
   // for void outputs
-  implicit def concatNothingRight[T](implicit ta: TupleArity[T]): Aux[T, Nothing, T] = new ParamConcat[T, Nothing] {
-    override type Out = T
-    override def leftArity: Int = ta.arity
-    override def rightArity: Int = 0
-  }
+  implicit def concatNothingRight[T](implicit ta: TupleArity[T]): Aux[T, Nothing, T] =
+    new ParamConcat[T, Nothing] {
+      override type Out = T
+      override def leftArity: Int = ta.arity
+      override def rightArity: Int = 0
+    }
 }
 
 trait LowPriorityTupleConcat2 extends LowPriorityTupleConcat1 {
