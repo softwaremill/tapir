f1847f7addf6dee7cd7b2df103924c8c87fd3137
adamw <adam@warski.org>
2020-05-13 16:18:46 +0200

    Partial server logic example

---
+++ examples/src/main/scala/sttp/tapir/examples/PartialServerLogicAkka.scala
@@ -0,0 +1,89 @@
+package sttp.tapir.examples
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import sttp.client._
+import sttp.tapir._
+import sttp.tapir.server.PartialServerEndpoint
+import sttp.tapir.server.akkahttp._
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+object PartialServerLogicAkka extends App {
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  import actorSystem.dispatcher
+
+  // authentication logic
+  case class User(name: String)
+  val AuthenticationErrorCode = 1001
+  def auth(token: String): Future[Either[Int, User]] =
+    Future {
+      if (token == "secret") Right(User("Spock"))
+      else Left(AuthenticationErrorCode)
+    }
+
+  // 1st approach: define a base endpoint, which has the authentication logic built-in
+  val secureEndpoint: PartialServerEndpoint[User, Unit, Int, Unit, Nothing, Future] = endpoint
+    .in(header[String]("X-AUTH-TOKEN"))
+    .errorOut(plainBody[Int])
+    .serverLogicForCurrent(auth)
+
+  // extend the base endpoint to define (potentially multiple) proper endpoints, define the rest of the server logic
+  val secureHelloWorld1WithLogic = secureEndpoint.get
+    .in("hello1")
+    .in(query[String]("salutation"))
+    .out(stringBody)
+    .serverLogic { case (user, salutation) => Future(Right(s"$salutation, ${user.name}!")) }
+
+  // ---
+
+  // 2nd approach: define the endpoint entirely first
+  val secureHelloWorld2: Endpoint[(String, String), Int, String, Nothing] = endpoint
+    .in(header[String]("X-AUTH-TOKEN"))
+    .errorOut(plainBody[Int])
+    .get
+    .in("hello2")
+    .in(query[String]("salutation"))
+    .out(stringBody)
+
+  // then, provide the server logic in parts
+  val secureHelloWorld2WithLogic = secureHelloWorld2
+    .serverLogicPart(auth)
+    .andThen { case (user, salutation) => Future(Right(s"$salutation, ${user.name}!")) }
+
+  // ---
+
+  // interpreting as routes
+  val helloWorld1Route: Route = secureHelloWorld1WithLogic.toRoute
+  val helloWorld2Route: Route = secureHelloWorld2WithLogic.toRoute
+
+  // starting the server
+  val bindAndCheck = Http().bindAndHandle(helloWorld1Route ~ helloWorld2Route, "localhost", 8080).map { _ =>
+    // testing
+    implicit val backend: SttpBackend[Identity, Nothing, NothingT] = HttpURLConnectionBackend()
+
+    def testWith(path: String, salutation: String, token: String): String = {
+      val result: String = basicRequest
+        .response(asStringAlways)
+        .get(uri"http://localhost:8080/$path?salutation=$salutation")
+        .header("X-AUTH-TOKEN", token)
+        .send()
+        .body
+
+      println(s"For path: $path, salutation: $salutation, token: $token, got result: $result")
+      result
+    }
+
+    assert(testWith("hello1", "Hello", "secret") == "Hello, Spock!")
+    assert(testWith("hello2", "Hello", "secret") == "Hello, Spock!")
+    assert(testWith("hello1", "Cześć", "secret") == "Cześć, Spock!")
+    assert(testWith("hello2", "Cześć", "secret") == "Cześć, Spock!")
+    assert(testWith("hello1", "Hello", "1234") == AuthenticationErrorCode.toString)
+    assert(testWith("hello2", "Hello", "1234") == AuthenticationErrorCode.toString)
+  }
+
+  Await.result(bindAndCheck.transformWith { r => actorSystem.terminate().transform(_ => r) }, 1.minute)
+}
