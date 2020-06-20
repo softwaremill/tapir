b9e49fd1be7b5e4422ad15ec434f0941f220c51b
Scala Steward <me@scala-steward.org>
2020-06-20 02:07:16 +0200

    Update zio, zio-streams to 1.0.0-RC21

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,7 @@ object Versions {
   val scalaTest = "3.0.8"
   val refined = "0.9.14"
   val enumeratum = "1.6.1"
-  val zio = "1.0.0-RC20"
+  val zio = "1.0.0-RC21"
   val zioInteropCats = "2.1.3.0-RC15"
   val playServer = "2.8.2"
   val tethys = "0.11.0"
