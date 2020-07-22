12374f78630079089199d613087194d8204cb56a
matwojcik <mat.wojcik@gmail.com>
2020-07-22 13:27:02 +0200

    Fix Eq

--- integrations/cats/src/test/scala/sttp/tapir/integ/cats/ExampleFunctorLawSpec.scala
+++ integrations/cats/src/test/scala/sttp/tapir/integ/cats/ExampleFunctorLawSpec.scala
@@ -1,7 +1,6 @@
 package sttp.tapir.integ.cats
 
 import cats.Eq
-import cats.implicits._
 import cats.laws.discipline.FunctorTests
 import org.scalacheck.Arbitrary
 import org.scalatest.funsuite.AnyFunSuite
@@ -10,8 +9,8 @@ import org.typelevel.discipline.scalatest.FunSuiteDiscipline
 import sttp.tapir.EndpointIO.Example
 import sttp.tapir.integ.cats.instances._
 
-class ExampleFunctorLawSpec  extends AnyFunSuite with FunSuiteDiscipline with Checkers {
-  implicit def eqTree[A: Eq]: Eq[Example[A]] = Eq.fromUniversalEquals
+class ExampleFunctorLawSpec extends AnyFunSuite with FunSuiteDiscipline with Checkers {
+  implicit def exampleEq[A]: Eq[Example[A]] = Eq.fromUniversalEquals
 
   implicit def exampleArbitrary[T: Arbitrary]: Arbitrary[Example[T]] = Arbitrary {
     for {
