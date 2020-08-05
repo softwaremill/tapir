3c5f8d7d99fac419516c130da6316e1eee83c801
Scala Steward <me@scala-steward.org>
2020-08-05 04:34:32 +0200

    Update zio-interop-cats to 2.1.4.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,7 +18,7 @@ object Versions {
   val refined = "0.9.15"
   val enumeratum = "1.6.1"
   val zio = "1.0.0"
-  val zioInteropCats = "2.1.4.0-RC17"
+  val zioInteropCats = "2.1.4.0"
   val playServer = "2.8.2"
   val tethys = "0.11.0"
   val vertx = "3.9.1"
