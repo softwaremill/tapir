616fde9b83a314db63a7ca5c429df80b48ca7faf
adamw <adam@warski.org>
2020-10-09 15:49:59 +0200

    Only initialise port counter if there's port in the config

--- tests/src/main/scala/sttp/tapir/tests/PortCounterFromConfig.scala
+++ tests/src/main/scala/sttp/tapir/tests/PortCounterFromConfig.scala
@@ -5,7 +5,8 @@ import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap, Suite}
 trait PortCounterFromConfig extends BeforeAndAfterAllConfigMap { this: Suite =>
   override protected def beforeAll(configMap: ConfigMap): Unit = {
     super.beforeAll(configMap)
-    val port = configMap.get("port").map(_.toString.toInt).getOrElse(50000)
-    PortCounter.startWith(port)
+    configMap.get("port").map(_.toString.toInt).foreach { port =>
+      PortCounter.startWith(port)
+    }
   }
 }
