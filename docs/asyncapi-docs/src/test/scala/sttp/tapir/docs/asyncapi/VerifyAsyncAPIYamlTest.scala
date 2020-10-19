d2800cfb6d5d11ae8af5ddc6601cac1069f27d02
adamw <adam@warski.org>
2020-10-19 14:17:16 +0200

    #91: more tests

--- docs/asyncapi-docs/src/test/scala/sttp/tapir/docs/asyncapi/VerifyAsyncAPIYamlTest.scala
+++ docs/asyncapi-docs/src/test/scala/sttp/tapir/docs/asyncapi/VerifyAsyncAPIYamlTest.scala
@@ -5,9 +5,10 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import sttp.capabilities.akka.AkkaStreams
 import sttp.model.HeaderNames
+import sttp.tapir.asyncapi.Server
 import sttp.tapir.asyncapi.circe.yaml.RichAsyncAPI
 import sttp.tapir.tests.{Fruit, FruitAmount}
-import sttp.tapir.{CodecFormat, endpoint, header, query, webSocketBody}
+import sttp.tapir.{CodecFormat, auth, endpoint, header, query, webSocketBody}
 import sttp.tapir.json.circe._
 
 import scala.io.Source
@@ -58,6 +59,21 @@ class VerifyAsyncAPIYamlTest extends AnyFunSuite with Matchers {
     val expectedYaml = loadYaml("expected_binding.yml")
 
     val actualYaml = e.toAsyncAPI("The fruit basket", "0.1").toYaml
+    val actualYamlNoIndent = noIndentation(actualYaml)
+
+    actualYamlNoIndent shouldBe expectedYaml
+  }
+
+  test("should support server security") {
+    val e = endpoint.post
+      .in("fruit")
+      .in(auth.apiKey(header[String](HeaderNames.Authorization)))
+      .out(webSocketBody[Fruit, CodecFormat.Json, Fruit, CodecFormat.Json](AkkaStreams))
+
+    val expectedYaml = loadYaml("expected_security.yml")
+
+    val actualYaml = e.toAsyncAPI("The fruit basket", "0.1", List("production" -> Server("example.org", "ws"))).toYaml
+    println(actualYaml)
     val actualYamlNoIndent = noIndentation(actualYaml)
 
     actualYamlNoIndent shouldBe expectedYaml
