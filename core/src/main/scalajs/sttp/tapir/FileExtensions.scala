25bfba32d6b84e827b4e9f56f6d7e7b9a2bab875
adamw <adam@warski.org>
2020-11-11 21:26:28 +0100

    Remove unused TapirPath

--- core/src/main/scalajs/sttp/tapir/FileExtensions.scala
+++ core/src/main/scalajs/sttp/tapir/FileExtensions.scala
@@ -2,5 +2,4 @@ package sttp.tapir
 
 object FileExtensions {
   type TapirFile = org.scalajs.dom.File
-  type TapirPath = org.scalajs.dom.File
 }
--- core/src/main/scalajvm/sttp/tapir/FileExtensions.scala
+++ core/src/main/scalajvm/sttp/tapir/FileExtensions.scala
@@ -2,5 +2,4 @@ package sttp.tapir
 
 object FileExtensions {
   type TapirFile = java.io.File
-  type TapirPath = java.nio.file.Path
 }
