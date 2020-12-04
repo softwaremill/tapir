3d7d2e6a377428d89b5916974cb8360e1c4cd517
Michał Szczygieł <1153719+mszczygiel@users.noreply.github.com>
2020-12-04 13:52:00 +0100

    add example

---
+++ examples/src/main/scala/sttp/tapir/examples/BasicAuthenticationServer.scala
@@ -0,0 +1,43 @@
+package sttp.tapir.examples
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import sttp.client3._
+import sttp.model.StatusCode
+import sttp.tapir._
+import sttp.tapir.model._
+import sttp.tapir.server.{AuthChallenge, AuthenticationFailureHandler}
+import sttp.tapir.server.akkahttp._
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+object BasicAuthenticationServer extends App {
+  private val failureHandler = AuthenticationFailureHandler.handleMissingAuthorizationHeader(AuthChallenge.basic("secret"))
+  private implicit val serverOptions: AkkaHttpServerOptions = AkkaHttpServerOptions.default.copy(decodeFailureHandler = failureHandler)
+  val secret: Endpoint[UsernamePassword, Unit, String, Any] =
+    endpoint.get.in("secret").in(auth.basic[UsernamePassword]).out(stringBody)
+
+  val secretRoute: Route = secret.toRoute(credentials => Future.successful(Right(s"Hello, ${credentials.username}!")))
+
+  // starting the server
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  import actorSystem.dispatcher
+
+  val bindAndCheck = Http().newServerAt("localhost", 8080).bindFlow(secretRoute).map { _ =>
+    // testing
+    val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
+    val unauthorized = basicRequest.get(uri"http://localhost:8080/secret").send(backend)
+    println("Got result: " + unauthorized)
+    assert(unauthorized.code == StatusCode.Unauthorized)
+    assert(unauthorized.header("WWW-Authenticate").contains("""Basic realm="secret""""))
+
+    val result = basicRequest.get(uri"http://localhost:8080/secret").header("Authorization", "Basic dXNlcjpzZWNyZXQ=").send(backend)
+    println("Got result: " + result)
+    assert(result.code == StatusCode.Ok)
+    assert(result.body == Right("Hello, user!"))
+  }
+
+  Await.result(bindAndCheck.transformWith { r => actorSystem.terminate().transform(_ => r) }, 1.minute)
+}
