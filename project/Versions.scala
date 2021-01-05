728a62dfa051a2b2efec70ff751bc7dfd8112f7d
Scala Steward <me@scala-steward.org>
2021-01-05 16:55:21 +0100

    Update model:core to 1.2.0-RC12

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val circe = "0.13.0"
   val circeYaml = "0.13.1"
   val sttp = "3.0.0-RC13"
-  val sttpModel = "1.2.0-RC9"
+  val sttpModel = "1.2.0-RC12"
   val sttpShared = "1.0.0-RC11"
   val akkaHttp = "10.2.2"
   val akkaStreams = "2.6.10"
