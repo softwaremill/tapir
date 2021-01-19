56a22c35be6294f3f1b2fd235edc3a73053ddcac
Vincent Bouzon <vincent.bouzon@live.fr>
2021-01-20 00:32:16 +0100

    add redocVersion parameter for RedocHttp4s handler

--- docs/redoc-http4s/src/main/scala/sttp/tapir/redoc/http4s/RedocHttp4s.scala
+++ docs/redoc-http4s/src/main/scala/sttp/tapir/redoc/http4s/RedocHttp4s.scala
@@ -13,15 +13,16 @@ import scala.io.Source
   * @param title       The title of the HTML page.
   * @param yaml        The yaml with the OpenAPI documentation.
   * @param yamlName    The name of the file, through which the yaml documentation will be served. Defaults to `docs.yaml`.
+  * @param redocVersion The semver version of Redoc to use. Defaults to `2.0.0-rc.23`.
   */
-class RedocHttp4s(title: String, yaml: String, yamlName: String = "docs.yaml") {
+class RedocHttp4s(title: String, yaml: String, yamlName: String = "docs.yaml", redocVersion: String = "2.0.0-rc.23") {
   private lazy val html = {
     val fileName = "redoc.html"
     val is = getClass.getClassLoader.getResourceAsStream(fileName)
     assert(Option(is).nonEmpty, s"Could not find file ${fileName} on classpath.")
     val rawHtml = Source.fromInputStream(is).mkString
     // very poor man's templating engine
-    rawHtml.replace("{{docsPath}}", yamlName).replace("{{title}}", title)
+    rawHtml.replace("{{docsPath}}", yamlName).replace("{{title}}", title).replace("{{redocVersion}}", redocVersion)
   }
 
   def routes[F[_]: ContextShift: Sync]: HttpRoutes[F] = {
