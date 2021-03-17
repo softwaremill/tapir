7a41ab841880c2c1578e1a0671a8be1b2c85836e
adamw <adam@warski.org>
2021-03-17 22:04:48 +0100

    Simplify #1072 fix

--- docs/apispec-docs/src/main/scala/sttp/tapir/docs/apispec/schema/ObjectTypeData.scala
+++ docs/apispec-docs/src/main/scala/sttp/tapir/docs/apispec/schema/ObjectTypeData.scala
@@ -28,16 +28,16 @@ object ObjectTypeData {
   def apply(typeData: TypeData[_]): List[ObjectTypeData] = {
     typeData match {
       case TypeData(TSchema(TSchemaType.SArray(o), _, _, _, _, _, _, schemaValidator), codecValidator) =>
-        apply(TypeData(o, elementValidator(schemaValidator.asInstanceOf[Validator[Any]].and(codecValidator.asInstanceOf[Validator[Any]]))))
+        apply(TypeData(o, elementValidator(schemaValidator)))
       case TypeData(s @ TSchema(st: TSchemaType.SProduct, _, _, _, _, _, _, schemaValidator), codecValidator) =>
-        productSchemas(s, st, schemaValidator.asInstanceOf[Validator[Any]].and(codecValidator.asInstanceOf[Validator[Any]]))
+        productSchemas(s, st, schemaValidator)
       case TypeData(s @ TSchema(st: TSchemaType.SCoproduct, _, _, _, _, _, _, schemaValidator), codecValidator) =>
-        coproductSchemas(s, st, schemaValidator.asInstanceOf[Validator[Any]].and(codecValidator.asInstanceOf[Validator[Any]]))
+        coproductSchemas(s, st, schemaValidator)
       case TypeData(s @ TSchema(st: TSchemaType.SOpenProduct, _, _, _, _, _, _, schemaValidator), codecValidator) =>
         (st.info -> TypeData(s, codecValidator): ObjectTypeData) +: apply(
           TypeData(
             st.valueSchema,
-            elementValidator(schemaValidator.asInstanceOf[Validator[Any]].and(codecValidator.asInstanceOf[Validator[Any]]))
+            elementValidator(schemaValidator)
           )
         )
       case _ => List.empty
