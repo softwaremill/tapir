be10dab3c01cd76b2cb5a048836ef5cbd850aedd
adamw <adam@warski.org>
2021-03-18 15:42:00 +0100

    Migrate stub server

---
+++ server/sttp-stub-server/src/main/scala/sttp/tapir/server/stub/SttpRequest.scala
@@ -0,0 +1,17 @@
+package sttp.tapir.server.stub
+
+import sttp.client3.Request
+import sttp.model.{Header, Method, QueryParams, Uri}
+import sttp.tapir.model.{ConnectionInfo, ServerRequest}
+import scala.collection.immutable.Seq
+
+class SttpRequest(r: Request[_, _]) extends ServerRequest {
+  override def method: Method = r.method
+  override def headers: Seq[Header] = r.headers
+  override def queryParameters: QueryParams = r.uri.params
+  override def protocol: String = "HTTP/1.1"
+  override def connectionInfo: ConnectionInfo = ConnectionInfo(None, None, None)
+  override def underlying: Any = r
+  override def pathSegments: List[String] = r.uri.pathSegments.segments.map(_.v).toList
+  override def uri: Uri = r.uri
+}
