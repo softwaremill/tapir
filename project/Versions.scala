9381301fac941ecc0a947e8736454301b479c620
Scala Steward <me@scala-steward.org>
2021-03-24 17:14:32 +0100

    Update client3:akka-http-backend, ... to 3.2.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val catsEffect = "2.3.3"
   val circe = "0.13.0"
   val circeYaml = "0.13.1"
-  val sttp = "3.1.9"
+  val sttp = "3.2.0"
   val sttpModel = "1.3.4"
   val sttpShared = "1.1.1"
   val akkaHttp = "10.2.4"
