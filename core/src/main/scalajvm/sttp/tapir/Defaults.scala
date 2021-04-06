4aabfd4e957f3c71ed696708e48a3186a7b6cbcf
kubinio123 <jakub.cichy@softwaremill.pl>
2021-04-06 13:38:39 +0200

    deleteOnExit for default file

--- core/src/main/scalajvm/sttp/tapir/Defaults.scala
+++ core/src/main/scalajvm/sttp/tapir/Defaults.scala
@@ -3,5 +3,9 @@ package sttp.tapir
 import java.io.File
 
 object Defaults {
-  def createTempFile: () => File = () => File.createTempFile("tapir", "tmp")
+  def createTempFile: () => File = () => {
+    val f = File.createTempFile("tapir", "tmp")
+    f.deleteOnExit()
+    f
+  }
 }
