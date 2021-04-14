d02138824bc54f1fdba3c092dc9885d2cee7b38e
Daniil Leontiev <prohormitrich53@gmail.com>
2021-04-14 20:56:54 +0300

    Fix compilation

--- docs/openapi-docs/src/main/scala/sttp/tapir/docs/openapi/OpenAPIDocsInterpreter.scala
+++ docs/openapi-docs/src/main/scala/sttp/tapir/docs/openapi/OpenAPIDocsInterpreter.scala
@@ -6,7 +6,10 @@ import sttp.tapir.server.ServerEndpoint
 
 trait OpenAPIDocsInterpreter {
   def toOpenAPI[I, E, O, S](e: Endpoint[I, E, O, S], title: String, version: String)(implicit options: OpenAPIDocsOptions): OpenAPI =
-    toOpenAPI(e, Info(title, version), List.empty)
+    toOpenAPI(e, Info(title, version))
+
+  def toOpenAPI[I, E, O, S](e: Endpoint[I, E, O, S], info: Info)(implicit options: OpenAPIDocsOptions): OpenAPI =
+    EndpointToOpenAPIDocs.toOpenAPI(info, Seq(e), options, List.empty)
 
   def toOpenAPI[I, E, O, S](e: Endpoint[I, E, O, S], info: Info, extensions: List[Extension[_]])(implicit
       options: OpenAPIDocsOptions
@@ -16,7 +19,10 @@ trait OpenAPIDocsInterpreter {
   def toOpenAPI[I, E, O, S, F[_]](se: ServerEndpoint[I, E, O, S, F], title: String, version: String)(implicit
       options: OpenAPIDocsOptions
   ): OpenAPI =
-    toOpenAPI(se.endpoint, Info(title, version), List.empty)
+    toOpenAPI(se.endpoint, Info(title, version))
+
+  def toOpenAPI[I, E, O, S, F[_]](se: ServerEndpoint[I, E, O, S, F], info: Info)(implicit options: OpenAPIDocsOptions): OpenAPI =
+    EndpointToOpenAPIDocs.toOpenAPI(info, Seq(se.endpoint), options, List.empty)
 
   def toOpenAPI[I, E, O, S, F[_]](se: ServerEndpoint[I, E, O, S, F], info: Info, extensions: List[Extension[_]])(implicit
       options: OpenAPIDocsOptions
@@ -24,7 +30,10 @@ trait OpenAPIDocsInterpreter {
     EndpointToOpenAPIDocs.toOpenAPI(info, Seq(se.endpoint), options, extensions)
 
   def toOpenAPI(es: Iterable[Endpoint[_, _, _, _]], title: String, version: String)(implicit options: OpenAPIDocsOptions): OpenAPI =
-    toOpenAPI(es, Info(title, version), List.empty)
+    toOpenAPI(es, Info(title, version))
+
+  def toOpenAPI(es: Iterable[Endpoint[_, _, _, _]], info: Info)(implicit options: OpenAPIDocsOptions): OpenAPI =
+    EndpointToOpenAPIDocs.toOpenAPI(info, es, options, List.empty)
 
   def toOpenAPI(es: Iterable[Endpoint[_, _, _, _]], info: Info, extensions: List[Extension[_]])(implicit
       options: OpenAPIDocsOptions
@@ -33,7 +42,12 @@ trait OpenAPIDocsInterpreter {
 
   def serverEndpointsToOpenAPI[F[_]](ses: Iterable[ServerEndpoint[_, _, _, _, F]], title: String, version: String)(implicit
       options: OpenAPIDocsOptions
-  ): OpenAPI = serverEndpointsToOpenAPI(ses, Info(title, version), List.empty)
+  ): OpenAPI = serverEndpointsToOpenAPI(ses, Info(title, version))
+
+  def serverEndpointsToOpenAPI[F[_]](ses: Iterable[ServerEndpoint[_, _, _, _, F]], info: Info)(implicit
+      options: OpenAPIDocsOptions
+  ): OpenAPI =
+    EndpointToOpenAPIDocs.toOpenAPI(info, ses.map(_.endpoint), options, List.empty)
 
   def serverEndpointsToOpenAPI[F[_]](ses: Iterable[ServerEndpoint[_, _, _, _, F]], info: Info, extensions: List[Extension[_]])(implicit
       options: OpenAPIDocsOptions
