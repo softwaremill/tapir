229c8f3f00f5b643f96478a0019e20067a416e6a
kubinio123 <jakub.cichy@softwaremill.pl>
2021-04-14 14:10:51 +0200

    merge

--- core/src/main/scala/sttp/tapir/server/interceptor/metrics/MetricsInterceptor.scala
+++ core/src/main/scala/sttp/tapir/server/interceptor/metrics/MetricsInterceptor.scala
@@ -3,63 +3,57 @@ package sttp.tapir.server.interceptor.metrics
 import sttp.monad.MonadError
 import sttp.monad.syntax._
 import sttp.tapir.metrics.Metric
-import sttp.tapir.model.{ServerRequest, ServerResponse}
-import sttp.tapir.server.interceptor.{EndpointInterceptor, ValuedEndpointOutput}
+import sttp.tapir.model.ServerResponse
+import sttp.tapir.server.interceptor._
 import sttp.tapir.server.interpreter.ServerResponseListener
 import sttp.tapir.server.interpreter.ServerResponseListenerSyntax._
-import sttp.tapir.{DecodeResult, Endpoint, EndpointInput}
+import sttp.tapir.{DecodeResult, Endpoint}
 
 class MetricsInterceptor[F[_], B](metrics: List[Metric[F, _]], ignoreEndpoints: Seq[Endpoint[_, _, _, _]])(implicit
     monad: MonadError[F],
     listener: ServerResponseListener[F, B]
 ) extends EndpointInterceptor[F, B] {
 
-  override def onDecodeSuccess[I](
-      request: ServerRequest,
-      endpoint: Endpoint[I, _, _, _],
-      i: I,
-      next: Option[ValuedEndpointOutput[_]] => F[ServerResponse[B]]
-  )(implicit monad: MonadError[F]): F[ServerResponse[B]] = {
-    if (ignoreEndpoints.contains(endpoint)) next(None)
-    else {
-      for {
-        _ <- collectMetrics { case Metric(m, Some(onRequest), _) => onRequest(endpoint, request, m) }
-        response <- next(None).map(r =>
-          r.listen {
-            collectMetrics { case Metric(m, _, Some(onResponse)) => onResponse(endpoint, request, r, m) }
-          }
-        )
-      } yield response
-    }
-  }
-
-  /** If there's some `ServerResponse` collects `onResponse` as well as `onRequest` metric which was not collected in `onDecodeSuccess` stage.
-    */
-  override def onDecodeFailure(
-      request: ServerRequest,
-      endpoint: Endpoint[_, _, _, _],
-      failure: DecodeResult.Failure,
-      failingInput: EndpointInput[_],
-      next: Option[ValuedEndpointOutput[_]] => F[Option[ServerResponse[B]]]
-  )(implicit monad: MonadError[F]): F[Option[ServerResponse[B]]] = {
-    if (ignoreEndpoints.contains(endpoint)) next(None)
-    else {
-      failure match {
-        case _: DecodeResult.Mismatch => next(None)
-        case _ =>
+  override def apply(responder: Responder[F, B], endpointHandler: EndpointHandler[F, B]): EndpointHandler[F, B] =
+    new EndpointHandler[F, B] {
+      override def onDecodeSuccess[I](ctx: DecodeSuccessContext[F, I])(implicit monad: MonadError[F]): F[ServerResponse[B]] =
+        if (ignoreEndpoints.contains(ctx.endpoint)) endpointHandler.onDecodeSuccess(ctx)
+        else {
           for {
-            response <- next(None).map(_.map { r =>
-              r.listen {
-                for {
-                  _ <- collectMetrics { case Metric(m, Some(onRequest), _) => onRequest(endpoint, request, m) }
-                  _ <- collectMetrics { case Metric(m, _, Some(onResponse)) => onResponse(endpoint, request, r, m) }
-                } yield ()
-              }
-            })
+            _ <- collectMetrics { case Metric(m, Some(onRequest), _) => onRequest(ctx.endpoint, ctx.request, m) }
+            response <- endpointHandler
+              .onDecodeSuccess(ctx)
+              .map(r =>
+                r.listen {
+                  collectMetrics { case Metric(m, _, Some(onResponse)) => onResponse(ctx.endpoint, ctx.request, r, m) }
+                }
+              )
           } yield response
-      }
+        }
+
+      /** If there's some `ServerResponse` collects `onResponse` as well as `onRequest` metric which was not collected in `onDecodeSuccess` stage.
+        */
+      override def onDecodeFailure(ctx: DecodeFailureContext)(implicit monad: MonadError[F]): F[Option[ServerResponse[B]]] =
+        if (ignoreEndpoints.contains(ctx.endpoint)) endpointHandler.onDecodeFailure(ctx)
+        else {
+          ctx.failure match {
+            case _: DecodeResult.Mismatch => endpointHandler.onDecodeFailure(ctx)
+            case _ =>
+              for {
+                response <- endpointHandler
+                  .onDecodeFailure(ctx)
+                  .map(_.map { r =>
+                    r.listen {
+                      for {
+                        _ <- collectMetrics { case Metric(m, Some(onRequest), _) => onRequest(ctx.endpoint, ctx.request, m) }
+                        _ <- collectMetrics { case Metric(m, _, Some(onResponse)) => onResponse(ctx.endpoint, ctx.request, r, m) }
+                      } yield ()
+                    }
+                  })
+              } yield response
+          }
+        }
     }
-  }
 
   private def collectMetrics(pf: PartialFunction[Metric[F, _], F[Unit]]): F[Unit] = {
     def collect(metrics: List[Metric[F, _]]): F[Unit] = {
--- core/src/test/scala/sttp/tapir/server/interpreter/ServerInterpreterTest.scala
+++ core/src/test/scala/sttp/tapir/server/interpreter/ServerInterpreterTest.scala
@@ -2,60 +2,17 @@ package sttp.tapir.server.interpreter
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import sttp.capabilities.Streams
-import sttp.monad.MonadError
-import sttp.tapir._
-import sttp.tapir.internal.NoStreams
-import sttp.tapir.model.{ConnectionInfo, ServerRequest, ServerResponse}
-import sttp.tapir.server.interceptor.{
-  DecodeFailureContext,
-  DecodeSuccessContext,
-  EndpointHandler,
-  EndpointInterceptor,
-  RequestHandler,
-  RequestInterceptor,
-  Responder
-}
-import sttp.model._
 import sttp.model.Uri._
+import sttp.model._
+import sttp.monad.MonadError
+import sttp.tapir.TestUtil._
+import sttp.tapir._
+import sttp.tapir.model.{ConnectionInfo, ServerRequest, ServerResponse}
+import sttp.tapir.server.interceptor._
 
-import java.nio.charset.Charset
 import scala.collection.immutable
 
 class ServerInterpreterTest extends AnyFlatSpec with Matchers {
-  type Id[X] = X
-
-  object TestRequestBody extends RequestBody[Id, Nothing] {
-    override val streams: Streams[Nothing] = NoStreams
-    override def toRaw[R](bodyType: RawBodyType[R]): Id[R] = ???
-    override def toStream(): streams.BinaryStream = ???
-  }
-
-  object TestToResponseBody extends ToResponseBody[Unit, Nothing] {
-    override val streams: Streams[Nothing] = NoStreams
-    override def fromRawValue[R](v: R, headers: HasHeaders, format: CodecFormat, bodyType: RawBodyType[R]): Unit = ???
-    override def fromStreamValue(
-        v: streams.BinaryStream,
-        headers: HasHeaders,
-        format: CodecFormat,
-        charset: Option[Charset]
-    ): Unit = ???
-    override def fromWebSocketPipe[REQ, RESP](
-        pipe: streams.Pipe[REQ, RESP],
-        o: WebSocketBodyOutput[streams.Pipe[REQ, RESP], REQ, RESP, _, Nothing]
-    ): Unit = ???
-  }
-
-  implicit object IdMonadError extends MonadError[Id] {
-    override def unit[T](t: T): Id[T] = t
-    override def map[T, T2](fa: Id[T])(f: T => T2): Id[T2] = f(fa)
-    override def flatMap[T, T2](fa: Id[T])(f: T => Id[T2]): Id[T2] = f(fa)
-    override def error[T](t: Throwable): Id[T] = throw t
-    override protected def handleWrappedError[T](rt: Id[T])(h: PartialFunction[Throwable, Id[T]]): Id[T] = rt
-    override def ensure[T](f: Id[T], e: => Id[Unit]): Id[T] = try f
-    finally e
-  }
-
   it should "call the interceptors in the correct order" in {
     var callTrail: List[String] = Nil
 
@@ -64,12 +21,12 @@ class ServerInterpreterTest extends AnyFlatSpec with Matchers {
         new EndpointHandler[Id, Unit] {
           override def onDecodeSuccess[I](ctx: DecodeSuccessContext[Id, I])(implicit monad: MonadError[Id]): Id[ServerResponse[Unit]] = {
             callTrail ::= s"$prefix success"
-            endpointHandler.onDecodeSuccess(ctx)
+            endpointHandler.onDecodeSuccess(ctx)(TestUtil.idMonadError)
           }
 
           override def onDecodeFailure(ctx: DecodeFailureContext)(implicit monad: MonadError[Id]): Id[Option[ServerResponse[Unit]]] = {
             callTrail ::= s"$prefix failure"
-            endpointHandler.onDecodeFailure(ctx)
+            endpointHandler.onDecodeFailure(ctx)(TestUtil.idMonadError)
           }
         }
     }
