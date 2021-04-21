681b4d3b0069222315e1d4319f9286f2a818c0dd
Scala Steward <me@scala-steward.org>
2021-04-21 23:22:25 +0200

    Update model:core to 1.4.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val circe = "0.13.0"
   val circeYaml = "0.13.1"
   val sttp = "3.3.0-RC4"
-  val sttpModel = "1.4.3"
+  val sttpModel = "1.4.4"
   val sttpShared = "1.2.1"
   val akkaHttp = "10.2.4"
   val akkaStreams = "2.6.14"
