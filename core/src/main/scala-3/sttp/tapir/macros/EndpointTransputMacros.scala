1d5c62c4c4a44d988630ccb4c1df9c20be7c3eb2
Mateusz Borek <mateusz.borek@softwaremill.com>
2021-05-07 17:32:17 +0200

    mapTo for Scala 3

--- core/src/main/scala-3/sttp/tapir/macros/EndpointTransputMacros.scala
+++ core/src/main/scala-3/sttp/tapir/macros/EndpointTransputMacros.scala
@@ -2,6 +2,28 @@ package sttp.tapir.macros
 
 import sttp.tapir.EndpointTransput
 
+import sttp.tapir.{Codec, CodecFormat, DecodeResult, Schema}
+import scala.compiletime.constValue
+import scala.compiletime.erasedValue
+import scala.deriving.Mirror
+
 trait EndpointTransputMacros[T] { this: EndpointTransput[T] =>
-  def mapTo[CASE_CLASS]: ThisType[CASE_CLASS] = ??? // TODO
+  inline def mapTo[CASE_CLASS <: Product](using mc: Mirror.ProductOf[CASE_CLASS]): ThisType[CASE_CLASS] = {
+    checkFields[CASE_CLASS, T]
+
+    val to: T => CASE_CLASS = {
+      case t: Tuple => mc.fromProduct(t)
+      case t =>   mc.fromProduct(Tuple1(t))
+    }
+    val from: CASE_CLASS => T = Tuple.fromProduct(_).asInstanceOf[T]
+
+    this.map[CASE_CLASS](to)(from)
+  }
+
+  inline def checkFields[A, B](using m: Mirror.ProductOf[A]): Unit =
+    inline (erasedValue[m.MirroredElemTypes], erasedValue[B]) match {
+      case _: (B *: EmptyTuple, B) => ()
+      case _: (B, B) => ()
+      case _ => scala.compiletime.error("Given case class does not match the endpoint transput.")//TODO improve message
+    }
 }
