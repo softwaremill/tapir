d498361019923db72f3446420de60573fb40535c
Mateusz Borek <mateusz.borek@softwaremill.com>
2021-05-11 12:47:12 +0200

    Add validator for coproduct enum.

--- core/src/main/scala-3/sttp/tapir/macros/ValidatorMacros.scala
+++ core/src/main/scala-3/sttp/tapir/macros/ValidatorMacros.scala
@@ -1,6 +1,12 @@
 package sttp.tapir.macros
 
 import sttp.tapir.Validator
+import sttp.tapir.SchemaType.SObjectInfo
+
+import scala.quoted.{Expr, Quotes, Type}
+import scala.compiletime
+
+import scala.quoted.*
 
 trait ValidatorMacros {
 
@@ -8,5 +14,36 @@ trait ValidatorMacros {
     * This enumeration will only be used for documentation, as a value outside of the allowed values will not be
     * decoded in the first place (the decoder has no other option than to fail).
     */
-  def derivedEnumeration[T]: Validator.Enumeration[T] = ??? // TODO
+  inline def derivedEnumeration[T]: Validator.Enumeration[T] = ${ValidatorMacros.derivedEnumerationImpl[T]}
+}
+
+object ValidatorMacros {
+  def derivedEnumerationImpl[T: Type](using q: Quotes): Expr[Validator.Enumeration[T]] = {
+    import quotes.reflect.*
+
+    val tpe = TypeRepr.of[T]
+    val symbol = tpe.typeSymbol
+
+    if (!symbol.isClassDef || !(symbol.flags is Flags.Sealed)) {
+      report.throwError("Can only enumerate values of a sealed trait or class.")
+    } else {
+      val children = symbol.children.toList.sortBy(_.name)
+      
+      if (!children.forall(_.flags is Flags.Module)) {
+        report.throwError("All children must be objects.")
+      } else {
+        val instances = children.map(x => tpe.memberType(x).asType match { 
+          case '[f] => Ref(x).asExprOf[f]
+        })
+
+        '{
+          Validator.Enumeration[T](
+            List(${Varargs(instances)}: _*).asInstanceOf[List[T]],
+            None,
+            None//TODO
+          )
+        }
+      }
+    }
+  }
 }
