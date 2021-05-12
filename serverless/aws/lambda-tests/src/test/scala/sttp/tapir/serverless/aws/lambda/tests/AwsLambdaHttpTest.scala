e4444664b62d2c16ee9d0ea0a7c5149d56cfb13a
kubinio123 <jakub.cichy@softwaremill.pl>
2021-05-12 09:50:13 +0200

    some it tests

--- serverless/aws/lambda-tests/src/test/scala/sttp/tapir/serverless/aws/lambda/tests/AwsLambdaHttpTest.scala
+++ serverless/aws/lambda-tests/src/test/scala/sttp/tapir/serverless/aws/lambda/tests/AwsLambdaHttpTest.scala
@@ -1,7 +1,8 @@
 package sttp.tapir.serverless.aws.lambda.tests
 
 import cats.effect.IO
-import org.scalatest.Assertion
+import org.scalatest.Assertions
+import org.scalatest.compatible.Assertion
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers._
 import sttp.capabilities.WebSockets
@@ -11,36 +12,44 @@ import sttp.model.{Header, Uri}
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.tests.backendResource
 
+import java.util.Base64
+
 class AwsLambdaHttpTest extends AnyFunSuite {
 
   private val baseUri: Uri = uri"http://localhost:3000"
 
-//  testServer(empty_endpoint, "GET empty endpoint") { backend =>
-//    basicRequest.get(baseUri).send(backend).map(_.body shouldBe Right(""))
-//  }
-//
-//  testServer(empty_endpoint, "POST empty endpoint") { backend =>
-//    basicRequest.post(baseUri).send(backend).map(_.body shouldBe Right(""))
-//  }
-//
-//  testServer(empty_get_endpoint, "GET a GET endpoint") { backend =>
-//    basicRequest.get(baseUri).send(backend).map(_.body shouldBe Right(""))
-//  }
-//
-//  testServer(empty_get_endpoint, "POST a GET endpoint") { backend =>
-//    basicRequest.post(baseUri).send(backend).map(_.body shouldBe Right(""))
-//  }
-//
-//  testServer(in_path_path_out_string_endpoint) { backend =>
-//    basicRequest.get(uri"$baseUri/fruit/orange/amount/20").send(backend).map(_.body shouldBe Right("orange 20"))
-//  }
-//
-//  testServer(in_path_path_out_string_endpoint, "with URL encoding") { backend =>
-//    basicRequest.get(uri"$baseUri/fruit/apple%2Fred/amount/20").send(backend).map(_.body shouldBe Right("apple/red 20"))
-//  }
+  testServer(empty_endpoint, "GET empty endpoint") { backend =>
+    basicRequest.get(baseUri).send(backend).map(_.body shouldBe Right(""))
+  }
+
+  testServer(empty_endpoint, "POST empty endpoint") { backend =>
+    basicRequest.post(baseUri).send(backend).map(_.body shouldBe Right(""))
+  }
+
+  testServer(empty_get_endpoint, "GET a GET endpoint") { backend =>
+    basicRequest.get(baseUri).send(backend).map(_.body shouldBe Right(""))
+  }
+
+  testServer(empty_get_endpoint, "POST a GET endpoint") { backend =>
+    basicRequest.post(baseUri).send(backend).map(_.body shouldBe Right(""))
+  }
+
+  testServer(in_path_path_out_string_endpoint) { backend =>
+    basicRequest.get(uri"$baseUri/fruit/orange/amount/20").send(backend).map { req =>
+      req.body.map(b => decode(b) shouldBe "orange 20").getOrElse(Assertions.fail())
+    }
+  }
+
+  testServer(in_path_path_out_string_endpoint, "with URL encoding") { backend =>
+    basicRequest.get(uri"$baseUri/fruit/apple%2Fred/amount/20").send(backend).map { req =>
+      req.body.map(b => decode(b) shouldBe "apple/red 20").getOrElse(Assertions.fail())
+    }
+  }
 
   testServer(in_string_out_string_endpoint) { backend =>
-    basicRequest.post(uri"$baseUri/api/echo/string").body("Sweet").send(backend).map(_.body shouldBe Right("Sweet"))
+    basicRequest.post(uri"$baseUri/api/echo/string").body("Sweet").send(backend).map { req =>
+      req.body.map(b => decode(b) shouldBe "Sweet").getOrElse(Assertions.fail())
+    }
   }
 
   testServer(in_json_out_json_endpoint) { backend =>
@@ -48,7 +57,11 @@ class AwsLambdaHttpTest extends AnyFunSuite {
       .post(uri"$baseUri/api/echo/json")
       .body("""{"fruit":"orange","amount":11}""")
       .send(backend)
-      .map(_.body shouldBe Right("""{"fruit":"orange","amount":11}"""))
+      .map { req =>
+        req.body
+          .map(b => decode(b) shouldBe """{"fruit":"orange","amount":11}""")
+          .getOrElse(Assertions.fail())
+      }
   }
 
   testServer(in_headers_out_headers_endpoint) { backend =>
@@ -58,6 +71,8 @@ class AwsLambdaHttpTest extends AnyFunSuite {
       .send(backend)
       .map(_.headers should contain allOf (Header.unsafeApply("X-Fruit", "apple"), Header.unsafeApply("Y-Fruit", "Orange")))
   }
+
+  private def decode(enc: String): String = new String(Base64.getDecoder.decode(enc))
 
   private def testServer(t: ServerEndpoint[_, _, _, Any, IO], suffix: String = "")(
       f: SttpBackend[IO, Fs2Streams[IO] with WebSockets] => IO[Assertion]
