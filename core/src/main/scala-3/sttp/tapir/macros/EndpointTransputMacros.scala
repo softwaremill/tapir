9939e4065dc4d6d3713319db8e27ce9a7b9578f7
Mateusz Borek <mateusz.borek@softwaremill.com>
2021-05-12 11:29:06 +0200

    Fix MappingMacros name

--- core/src/main/scala-3/sttp/tapir/macros/EndpointTransputMacros.scala
+++ core/src/main/scala-3/sttp/tapir/macros/EndpointTransputMacros.scala
@@ -1,13 +1,13 @@
 package sttp.tapir.macros
 
 import sttp.tapir.EndpointTransput
-import sttp.tapir.internal.EndpointMapMacros
+import sttp.tapir.internal.MappingMacros
 
 import scala.compiletime.erasedValue
 import scala.deriving.Mirror
 
 trait EndpointTransputMacros[T] { this: EndpointTransput[T] =>
   inline def mapTo[CASE_CLASS <: Product](using mc: Mirror.ProductOf[CASE_CLASS]): ThisType[CASE_CLASS] = 
-    this.map(EndpointMapMacros.mappingImpl[T, CASE_CLASS])
+    this.map(MappingMacros.mappingImpl[T, CASE_CLASS])
 
 }
