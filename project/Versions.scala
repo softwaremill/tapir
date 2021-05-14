51ba930a4966115e418c516b229a040ddb946119
Scala Steward <me@scala-steward.org>
2021-05-14 20:39:15 +0200

    Update scalatest to 3.2.9

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,7 @@ object Versions {
   val json4s = "3.6.11"
   val sprayJson = "1.3.6"
   val scalaCheck = "1.15.4"
-  val scalaTest = "3.2.8"
+  val scalaTest = "3.2.9"
   val scalaTestPlusScalaCheck = "3.2.2.0"
   val refined = "0.9.25"
   val enumeratum = "1.6.1"
