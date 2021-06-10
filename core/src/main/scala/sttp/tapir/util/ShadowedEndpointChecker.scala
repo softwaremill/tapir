a77044f29d128e00dc7a53df84381992f84e5ac1
slabiakt <tomasz.slabiak@softwaremill.pl>
2021-06-10 10:33:52 +0200

    Improve logic in ShadowedEndpointChecker

--- core/src/main/scala/sttp/tapir/util/ShadowedEndpointChecker.scala
+++ core/src/main/scala/sttp/tapir/util/ShadowedEndpointChecker.scala
@@ -5,26 +5,28 @@ import sttp.tapir.{Endpoint, ShadowedEndpoint}
 object ShadowedEndpointChecker {
 
   def findShadowedEndpoints(endpoints: List[Endpoint[_, _, _, _]]): List[ShadowedEndpoint] = {
-    val duplicates = endpoints
-      .groupBy(endpoint => extractNormalizedPath(endpoint))
-      .filter(p =>
-        p match {
-          case (_, endpointsWithCommonPath) => endpointsWithCommonPath.size > 1
-        }
-      )
-    if (duplicates.isEmpty) List() else buildShadowedEndpoints(duplicates)
+    def findEndpoints(endpoints: List[Endpoint[_, _, _, _]], acc: List[ShadowedEndpoint]): List[ShadowedEndpoint] =
+      endpoints match {
+        case endpoint :: endpoints => findEndpoints(endpoints, acc ::: findAllShadowedByEndpoint(endpoint, endpoints))
+        case Nil => acc
+      }
+
+    def findAllShadowedByEndpoint(endpoint: Endpoint[_, _, _, _], endpoints: List[Endpoint[_, _, _, _]]): List[ShadowedEndpoint] = {
+      if (hasWildcard(endpoint))
+        endpoints.filter(e => normalizePath(e).startsWith(normalizePath(endpoint))).map(e => ShadowedEndpoint(e, endpoint))
+      else
+        endpoints.filter(e => normalizePath(e).equals(normalizePath(endpoint))).map(e => ShadowedEndpoint(e, endpoint))
+    }
+
+    findEndpoints(endpoints, List()).distinctBy(_.e)
   }
 
-  def buildShadowedEndpoints(duplicates: Map[String, List[Endpoint[_, _, _, _]]]): List[ShadowedEndpoint] = {
-    duplicates.flatMap({
-      case (_, overlappingEndpoint :: overlappedEndpoints) =>
-        overlappedEndpoints.map(e => ShadowedEndpoint(e, overlappingEndpoint))
-      case (_, Nil) => Nil
-    }).toList
+  def hasWildcard(e: Endpoint[_, _, _, _]): Boolean = {
+    e.input.show.endsWith("/...")
   }
 
-  def extractNormalizedPath(i: Endpoint[_, _, _, _]): String = {
-    val pathWithoutWildcard = i.input.show.replace("/*", "")
+  def normalizePath(e: Endpoint[_, _, _, _]): String = {
+    val pathWithoutWildcard = e.input.show.replace(" /...", "")
     if (pathWithoutWildcard.endsWith("/")) pathWithoutWildcard.dropRight(1) else pathWithoutWildcard
   }
 }
