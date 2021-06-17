c120016a3f98c56bac69b7cf2c1b23d3a9999859
adamw <adam@warski.org>
2021-06-17 16:58:42 +0200

    Adapt schema derivation to Scala3 in OpenAPI tests, especially for recursive values.

---
+++ docs/openapi-docs/src/test/scala/sttp/tapir/docs/openapi/dtos/VerifyYamlValidatorTestData.scala
@@ -0,0 +1,10 @@
+package sttp.tapir.docs.openapi.dtos
+
+import sttp.tapir.tests.FruitAmount
+
+// TODO: move back to VerifyYamlTest companion after https://github.com/lampepfl/dotty/issues/12849 is fixed
+object VerifyYamlValidatorTestData {
+  case class ObjectWithList(data: List[FruitAmount])
+  case class ObjectWithStrings(data: List[String])
+  case class MyClass(myAttribute: Int)
+}
