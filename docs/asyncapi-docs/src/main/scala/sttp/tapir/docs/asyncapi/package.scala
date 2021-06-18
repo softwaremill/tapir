b12b3cf91ca2a7c372b0234d181d1117b84ccbc8
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-06-18 09:38:31 +0200

    Setting options for AsyncAPIInterpreter as explicit parameter

--- docs/asyncapi-docs/src/main/scala/sttp/tapir/docs/asyncapi/package.scala
+++ docs/asyncapi-docs/src/main/scala/sttp/tapir/docs/asyncapi/package.scala
@@ -1,5 +1,11 @@
 package sttp.tapir.docs
 
-package object asyncapi extends TapirAsyncAPIDocs {
+package object asyncapi {
   private[docs] type MessageKey = String
+
+  def apply(docsOptions: AsyncAPIDocsOptions = AsyncAPIDocsOptions.default): TapirAsyncAPIDocs = {
+    new TapirAsyncAPIDocs {
+      override def asyncAPIDocsOptions: AsyncAPIDocsOptions = docsOptions
+    }
+  }
 }
