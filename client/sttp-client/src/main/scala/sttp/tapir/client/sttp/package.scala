f489684b93a0ab0ff10189c51d833d18400f40c0
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-06-21 10:00:50 +0200

    Removed depreacted traits

--- client/sttp-client/src/main/scala/sttp/tapir/client/sttp/package.scala
+++
@@ -1,9 +0,0 @@
-package sttp.tapir.client
-
-package object sttp {
-  def apply(clientOptions: SttpClientOptions = SttpClientOptions.default): TapirSttpClient = {
-    new TapirSttpClient {
-      override def sttpClientOptions: SttpClientOptions = clientOptions
-    }
-  }
-}
--- docs/asyncapi-docs/src/main/scala/sttp/tapir/docs/asyncapi/package.scala
+++ docs/asyncapi-docs/src/main/scala/sttp/tapir/docs/asyncapi/package.scala
@@ -2,10 +2,4 @@ package sttp.tapir.docs
 
 package object asyncapi {
   private[docs] type MessageKey = String
-
-  def apply(docsOptions: AsyncAPIDocsOptions = AsyncAPIDocsOptions.default): TapirAsyncAPIDocs = {
-    new TapirAsyncAPIDocs {
-      override def asyncAPIDocsOptions: AsyncAPIDocsOptions = docsOptions
-    }
-  }
 }
