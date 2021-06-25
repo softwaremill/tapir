f0a46e4811937b92aaabd345b1c79c08180f88a9
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-06-25 11:57:14 +0200

    Resolve TODO for streaming

--- server/zio-http/src/main/scala/sttp/tapir/server/zhttp/ZHttpToResponseBody.scala
+++ server/zio-http/src/main/scala/sttp/tapir/server/zhttp/ZHttpToResponseBody.scala
@@ -1,6 +1,5 @@
 package sttp.tapir.server.zhttp
 
-import sttp.capabilities
 import sttp.capabilities.zio.ZioStreams
 import sttp.model.HasHeaders
 import sttp.tapir.server.interpreter.ToResponseBody
@@ -12,13 +11,13 @@ import zio.stream.{Stream, ZStream}
 import java.nio.charset.Charset
 
 class ZHttpToResponseBody extends ToResponseBody[ZStream[Blocking, Throwable, Byte], ZioStreams] {
-  val streams: capabilities.Streams[ZioStreams] = ZioStreams
+  val streams: ZioStreams = ZioStreams
 
   override def fromRawValue[R](v: R, headers: HasHeaders, format: CodecFormat, bodyType: RawBodyType[R]): ZStream[Blocking, Throwable, Byte] =
     rawValueToEntity(bodyType, v)
 
   override def fromStreamValue(v: streams.BinaryStream, headers: HasHeaders, format: CodecFormat, charset: Option[Charset]): ZStream[Blocking, Throwable, Byte] =
-    v.asInstanceOf[Stream[Throwable, Byte]].refineOrDie(PartialFunction.empty) //TODO
+    v
 
   override def fromWebSocketPipe[REQ, RESP](pipe: streams.Pipe[REQ, RESP], o: WebSocketBodyOutput[streams.Pipe[REQ, RESP], REQ, RESP, _, ZioStreams]): ZStream[Blocking, Throwable, Byte] =
     Stream.empty //TODO
