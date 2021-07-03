57af54c73d91d9efe26e55343b3de0a1bba70afb
David van Geest <dvangeest@hopper.com>
2021-07-02 22:44:26 -0400

    Use decode failures in AkkaHttpServerInterpreter.

--- server/akka-http-server/src/main/scala/sttp/tapir/server/akkahttp/AkkaHttpServerInterpreter.scala
+++ server/akka-http-server/src/main/scala/sttp/tapir/server/akkahttp/AkkaHttpServerInterpreter.scala
@@ -1,6 +1,6 @@
 package sttp.tapir.server.akkahttp
 
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.{HttpMethod, HttpMethods, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives.{
   complete,
   extractExecutionContext,
@@ -11,15 +11,18 @@ import akka.http.scaladsl.server.Directives.{
   reject,
   respondWithHeaders
 }
+import akka.http.scaladsl.server.MethodRejection
 import akka.http.scaladsl.server.Route
 import sttp.capabilities.WebSockets
 import sttp.capabilities.akka.AkkaStreams
+import sttp.model.Method
 import sttp.monad.FutureMonad
 import sttp.tapir.Endpoint
+import sttp.tapir.EndpointInput
 import sttp.tapir.model.ServerResponse
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.akkahttp.AkkaModel.parseHeadersOrThrowWithoutContentHeaders
-import sttp.tapir.server.interpreter.{BodyListener, ServerInterpreter}
+import sttp.tapir.server.interpreter.{BodyListener, DecodeBasicInputsResult, ServerInterpreter}
 
 import scala.concurrent.Future
 import scala.reflect.ClassTag
@@ -32,7 +35,7 @@ trait AkkaHttpServerInterpreter {
     toRoute(e.serverLogic(logic))
 
   def toRouteRecoverErrors[I, E, O](
-    e: Endpoint[I, E, O, AkkaStreams with WebSockets]
+      e: Endpoint[I, E, O, AkkaStreams with WebSockets]
   )(logic: I => Future[O])(implicit eIsThrowable: E <:< Throwable, eClassTag: ClassTag[E]): Route =
     toRoute(e.serverLogicRecoverErrors(logic))
 
@@ -53,8 +56,13 @@ trait AkkaHttpServerInterpreter {
           )
 
           onSuccess(interpreter(serverRequest, ses)) {
-            case None => reject
-            case Some(response) => serverResponseToAkka(response)
+            case Left(decodeFailures) =>
+              val rejections = decodeFailures.collect { case DecodeBasicInputsResult.Failure(EndpointInput.FixedMethod(m, _, _), _) =>
+                MethodRejection(methodToAkkaHttp(m))
+              }
+              reject(rejections.toSeq: _*)
+            case Right(response) =>
+              serverResponseToAkka(response)
           }
         }
       }
@@ -74,6 +82,19 @@ trait AkkaHttpServerInterpreter {
         complete(HttpResponse(entity = entity, status = statusCode, headers = akkaHeaders))
       case None => complete(HttpResponse(statusCode, headers = akkaHeaders))
     }
+  }
+
+  private def methodToAkkaHttp(m: Method): HttpMethod = m match {
+    case Method.CONNECT => HttpMethods.CONNECT
+    case Method.DELETE  => HttpMethods.DELETE
+    case Method.GET     => HttpMethods.GET
+    case Method.HEAD    => HttpMethods.HEAD
+    case Method.OPTIONS => HttpMethods.OPTIONS
+    case Method.PATCH   => HttpMethods.PATCH
+    case Method.POST    => HttpMethods.POST
+    case Method.PUT     => HttpMethods.PUT
+    case Method.TRACE   => HttpMethods.TRACE
+    case Method(s)      => HttpMethod.custom(s)
   }
 }
 
