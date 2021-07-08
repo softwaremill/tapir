0a6817656bb17d26fde8c2f1d71e09f3a00c8bca
adamw <adam@warski.org>
2021-07-08 09:45:33 +0200

    Simpler example

--- examples/src/main/scala/sttp/tapir/examples/HelloWorldZioHttpServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/HelloWorldZioHttpServer.scala
@@ -8,24 +8,22 @@ import zio._
 import zio.blocking.Blocking
 
 object HelloWorldZioHttpServer extends App {
-
-  val helloWorld: Endpoint[String, Throwable, String, Any] =
+  val helloWorld: Endpoint[String, Unit, String, Any] =
     endpoint.get
       .in("hello")
       .in(path[String]("name"))
-      .errorOut(plainBody[String].map(err => new Throwable(err))(_.getMessage))
       .out(stringBody)
 
-  val add: Endpoint[(Int, Int), Throwable, String, Any] =
+  val add: Endpoint[(Int, Int), Unit, String, Any] =
     endpoint.get
       .in("add")
       .in(path[Int]("x"))
       .in(path[Int]("y"))
-      .errorOut(plainBody[String].map(err => new Throwable(err))(_.getMessage))
       .out(stringBody)
 
-  val app: HttpApp[Blocking, Throwable] = ZioHttpInterpreter().toRoutes(helloWorld)(name => ZIO.succeed(Right(s"Hello $name"))) <>
-    ZioHttpInterpreter().toRoutes(add) { case (x, y) => ZIO.succeed(Right(s"Adding up ${x + y}")) }
+  val app: HttpApp[Blocking, Throwable] =
+    ZioHttpInterpreter().toRoutes(helloWorld)(name => ZIO.succeed(Right(s"Hello, $name!"))) <>
+      ZioHttpInterpreter().toRoutes(add) { case (x, y) => ZIO.succeed(Right(s"$x + $y equals: ${x + y}")) }
 
   // starting the server
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
