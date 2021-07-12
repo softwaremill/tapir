4a36a4cd578dfb00a0e2d5f0fd333797d09ffb27
slabiakt <tomasz.slabiak@softwaremill.pl>
2021-07-12 08:47:12 +0200

    Add info about remote and local in connectionInfo for Play server

--- server/play-server/src/main/scala/sttp/tapir/server/play/PlayServerRequest.scala
+++ server/play-server/src/main/scala/sttp/tapir/server/play/PlayServerRequest.scala
@@ -5,6 +5,7 @@ import play.utils.UriEncoding
 import sttp.model.{Header, Method, QueryParams, Uri}
 import sttp.tapir.model.{ConnectionInfo, ServerRequest}
 
+import java.net.InetSocketAddress
 import java.nio.charset.StandardCharsets
 import scala.collection.immutable._
 
@@ -12,7 +13,9 @@ private[play] class PlayServerRequest(request: RequestHeader) extends ServerRequ
   override lazy val method: Method = Method(request.method.toUpperCase)
   override def protocol: String = request.version
   override lazy val uri: Uri = Uri.unsafeParse(request.uri)
-  override lazy val connectionInfo: ConnectionInfo = ConnectionInfo(None, None, Some(request.secure))
+  private val splitHost = request.host.split(":")
+  private val local = if (splitHost.size > 1) Some(new InetSocketAddress(splitHost.head, splitHost.last.toInt)) else None
+  override lazy val connectionInfo: ConnectionInfo = ConnectionInfo(local, None, Some(request.secure))
   override lazy val headers: Seq[Header] = request.headers.headers.map { case (k, v) => Header(k, v) }.toList
   override lazy val queryParameters: QueryParams = QueryParams.fromMultiMap(request.queryString)
   override lazy val pathSegments: List[String] =
