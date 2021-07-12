c865d840f103eb94d89381f2004d407dcdd6dd1f
adamw <adam@warski.org>
2021-07-12 14:05:08 +0200

    Fix WS tests

--- client/sttp-client/src/test/scalajvm/sttp/tapir/client/sttp/SttpClientTests.scala
+++ client/sttp-client/src/test/scalajvm/sttp/tapir/client/sttp/SttpClientTests.scala
@@ -1,6 +1,8 @@
 package sttp.tapir.client.sttp
 
-import cats.effect.{IO, Resource}
+import cats.effect.IO
+import cats.effect.std.Dispatcher
+import cats.effect.unsafe.implicits.global
 import sttp.capabilities.WebSockets
 import sttp.capabilities.fs2.Fs2Streams
 import sttp.client3._
@@ -9,15 +11,13 @@ import sttp.tapir.client.tests.ClientTests
 import sttp.tapir.{DecodeResult, Endpoint}
 
 abstract class SttpClientTests[R >: WebSockets with Fs2Streams[IO]] extends ClientTests[R] {
-
-  val backend: Resource[IO, SttpBackend[IO, Fs2Streams[IO] with WebSockets]] = HttpClientFs2Backend.resource[IO]()
+  val (dispatcher, closeDispatcher) = Dispatcher[IO].allocated.unsafeRunSync()
+  val backend: SttpBackend[IO, R] = HttpClientFs2Backend[IO](dispatcher).unsafeRunSync()
   def wsToPipe: WebSocketToPipe[R]
 
   override def send[I, E, O](e: Endpoint[I, E, O, R], port: Port, args: I, scheme: String = "http"): IO[Either[E, O]] = {
-    backend.use { b =>
-      implicit val wst: WebSocketToPipe[R] = wsToPipe
-      SttpClientInterpreter().toRequestThrowDecodeFailures(e, Some(uri"$scheme://localhost:$port")).apply(args).send(b).map(_.body)
-    }
+    implicit val wst: WebSocketToPipe[R] = wsToPipe
+    SttpClientInterpreter().toRequestThrowDecodeFailures(e, Some(uri"$scheme://localhost:$port")).apply(args).send(backend).map(_.body)
   }
 
   override def safeSend[I, E, O](
@@ -25,9 +25,13 @@ abstract class SttpClientTests[R >: WebSockets with Fs2Streams[IO]] extends Clie
       port: Port,
       args: I
   ): IO[DecodeResult[Either[E, O]]] = {
-    backend.use { b =>
-      implicit val wst: WebSocketToPipe[R] = wsToPipe
-      SttpClientInterpreter.toRequest(e, Some(uri"http://localhost:$port")).apply(args).send(b).map(_.body)
-    }
+    implicit val wst: WebSocketToPipe[R] = wsToPipe
+    SttpClientInterpreter().toRequest(e, Some(uri"http://localhost:$port")).apply(args).send(backend).map(_.body)
+  }
+
+  override protected def afterAll(): Unit = {
+    backend.close().unsafeRunSync()
+    closeDispatcher.unsafeRunSync()
+    super.afterAll()
   }
 }
