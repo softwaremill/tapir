6a55375c255db5a25e1c36bdbe006abd5380bfad
adamw <adam@warski.org>
2021-09-15 16:19:38 +0200

    Remove some code duplication

--- server/netty-server/src/main/scala/sttp/tapir/server/netty/NettyCatsServerInterpreter.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/NettyCatsServerInterpreter.scala
@@ -2,39 +2,18 @@ package sttp.tapir.server.netty
 
 import cats.effect.Async
 import cats.effect.std.Dispatcher
-import io.netty.buffer.ByteBuf
-import sttp.monad.syntax._
 import sttp.monad.MonadError
-import sttp.tapir.internal.NoStreams
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.server.interceptor.RequestResult
-import sttp.tapir.server.interpreter.{BodyListener, ServerInterpreter}
 import sttp.tapir.server.netty.internal.CatsUtil.CatsMonadError
-import sttp.tapir.server.netty.internal.{NettyBodyListener, NettyRequestBody, NettyToResponseBody}
+import sttp.tapir.server.netty.internal.NettyServerInterpreter
 
 trait NettyCatsServerInterpreter[F[_]] {
   implicit def async: Async[F]
   def nettyServerOptions: NettyCatsServerOptions[F]
 
   def toRoute(ses: List[ServerEndpoint[_, _, _, Any, F]]): Route[F] = {
-    val handler: Route[F] = { (request: NettyServerRequest) =>
-      implicit val monad: MonadError[F] = new CatsMonadError[F]
-      implicit val bodyListener: BodyListener[F, ByteBuf] = new NettyBodyListener
-      val serverInterpreter = new ServerInterpreter[Any, F, ByteBuf, NoStreams](
-        new NettyRequestBody(request, request, nettyServerOptions.createFile),
-        new NettyToResponseBody,
-        nettyServerOptions.interceptors,
-        nettyServerOptions.deleteFile
-      )
-
-      serverInterpreter(request, ses)
-        .map {
-          case RequestResult.Response(response) => Some(response)
-          case RequestResult.Failure(_)         => None
-        }
-    }
-
-    handler
+    implicit val monad: MonadError[F] = new CatsMonadError[F]
+    NettyServerInterpreter.toRoute(ses, nettyServerOptions.interceptors, nettyServerOptions.createFile, nettyServerOptions.deleteFile)
   }
 }
 
--- server/netty-server/src/main/scala/sttp/tapir/server/netty/NettyFutureServerInterpreter.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/NettyFutureServerInterpreter.scala
@@ -1,13 +1,10 @@
 package sttp.tapir.server.netty
 
-import scala.concurrent.{ExecutionContext, Future}
-import io.netty.buffer.ByteBuf
 import sttp.monad.FutureMonad
-import sttp.tapir.internal.NoStreams
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.server.interceptor.RequestResult
-import sttp.tapir.server.interpreter.{BodyListener, ServerInterpreter}
-import sttp.tapir.server.netty.internal.{NettyBodyListener, NettyRequestBody, NettyToResponseBody}
+import sttp.tapir.server.netty.internal.NettyServerInterpreter
+
+import scala.concurrent.{ExecutionContext, Future}
 
 trait NettyFutureServerInterpreter {
   def nettyServerOptions: NettyFutureServerOptions
@@ -15,24 +12,8 @@ trait NettyFutureServerInterpreter {
   def toRoute(
       ses: List[ServerEndpoint[_, _, _, Any, Future]]
   )(implicit ec: ExecutionContext): FutureRoute = {
-    val handler: FutureRoute = { (request: NettyServerRequest) =>
-      implicit val monad: FutureMonad = new FutureMonad()
-      implicit val bodyListener: BodyListener[Future, ByteBuf] = new NettyBodyListener
-      val serverInterpreter = new ServerInterpreter[Any, Future, ByteBuf, NoStreams](
-        new NettyRequestBody(request, request, nettyServerOptions.createFile),
-        new NettyToResponseBody,
-        nettyServerOptions.interceptors,
-        nettyServerOptions.deleteFile
-      )
-
-      serverInterpreter(request, ses)
-        .map {
-          case RequestResult.Response(response) => Some(response)
-          case RequestResult.Failure(_)         => None
-        }
-    }
-
-    handler
+    implicit val monad: FutureMonad = new FutureMonad()
+    NettyServerInterpreter.toRoute(ses, nettyServerOptions.interceptors, nettyServerOptions.createFile, nettyServerOptions.deleteFile)
   }
 }
 
