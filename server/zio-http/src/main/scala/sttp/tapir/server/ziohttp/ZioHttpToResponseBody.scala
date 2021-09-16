24cb4d3444135765295fd9221c6d29a9a4fbd4ca
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-09-16 11:08:29 +0200

    Added file streaming for ziohttp-based server

--- server/zio-http/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpToResponseBody.scala
+++ server/zio-http/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpToResponseBody.scala
@@ -2,12 +2,14 @@ package sttp.tapir.server.ziohttp
 
 import sttp.capabilities.zio.ZioStreams
 import sttp.model.HasHeaders
+import sttp.tapir.internal.TapirFile
 import sttp.tapir.server.interpreter.ToResponseBody
-import sttp.tapir.{CodecFormat, RawBodyType, WebSocketBodyOutput}
-import zio.Chunk
+import sttp.tapir.{CodecFormat, RangeValue, RawBodyType, WebSocketBodyOutput}
+import zio.{Chunk, stream}
 import zio.blocking.Blocking
 import zio.stream.{Stream, ZStream}
 
+import java.io.RandomAccessFile
 import java.nio.charset.Charset
 
 class ZioHttpToResponseBody extends ToResponseBody[ZStream[Any, Throwable, Byte], ZioStreams] {
@@ -35,8 +37,23 @@ class ZioHttpToResponseBody extends ToResponseBody[ZStream[Any, Throwable, Byte]
       case RawBodyType.ByteArrayBody       => Stream.fromChunk(Chunk.fromArray(r))
       case RawBodyType.ByteBufferBody      => Stream.fromChunk(Chunk.fromByteBuffer(r))
       case RawBodyType.InputStreamBody     => ZStream.fromInputStream(r).provideLayer(Blocking.live)
-      case RawBodyType.FileBody            => ZStream.fromFile(r.toPath).provideLayer(Blocking.live)
+      case RawBodyType.FileBody            =>
+        val tapirFile = r.asInstanceOf[TapirFile]
+        tapirFile.range
+          .map(rangeValue => ZStream.fromChunk(Chunk.fromArray(prepareChunk(tapirFile, rangeValue))))
+          .getOrElse(ZStream.fromFile(tapirFile.toPath).provideLayer(Blocking.live))
+
       case RawBodyType.MultipartBody(_, _) => Stream.empty
     }
+  }
+
+  private def prepareChunk[R, CF <: CodecFormat](tapirFile: TapirFile, rangeValue: RangeValue): Array[Byte] = {
+    val raf = new RandomAccessFile(tapirFile.toFile, "r")
+    val chunkSize = rangeValue.end - rangeValue.start
+    val dataArray = Array.ofDim[Byte](chunkSize)
+    raf.seek(rangeValue.start)
+    val bytesRead = raf.read(dataArray, 0, chunkSize)
+    val readChunk = dataArray.take(bytesRead)
+    readChunk
   }
 }
