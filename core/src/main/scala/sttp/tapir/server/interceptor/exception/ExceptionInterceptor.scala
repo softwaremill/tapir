0e8ff47c605f299b094614b546ef6eeed89a8cdc
adamw <adam@warski.org>
2021-10-04 17:25:00 +0200

    In the exception interceptor, catch all non-fatal exceptions

--- core/src/main/scala/sttp/tapir/server/interceptor/exception/ExceptionInterceptor.scala
+++ core/src/main/scala/sttp/tapir/server/interceptor/exception/ExceptionInterceptor.scala
@@ -6,13 +6,15 @@ import sttp.tapir.model.ServerResponse
 import sttp.tapir.server.interceptor._
 import sttp.tapir.server.interpreter.BodyListener
 
+import scala.util.control.NonFatal
+
 class ExceptionInterceptor[F[_]](handler: ExceptionHandler) extends EndpointInterceptor[F] {
   override def apply[B](responder: Responder[F, B], decodeHandler: EndpointHandler[F, B]): EndpointHandler[F, B] =
     new EndpointHandler[F, B] {
       override def onDecodeSuccess[I](
           ctx: DecodeSuccessContext[F, I]
       )(implicit monad: MonadError[F], bodyListener: BodyListener[F, B]): F[ServerResponse[B]] = {
-        monad.handleError(decodeHandler.onDecodeSuccess(ctx)) { case e: Exception =>
+        monad.handleError(decodeHandler.onDecodeSuccess(ctx)) { case NonFatal(e) =>
           handler(ExceptionContext(e, ctx.endpoint, ctx.request)) match {
             case Some(value) => responder(ctx.request, value)
             case None        => monad.error(e)
@@ -23,7 +25,7 @@ class ExceptionInterceptor[F[_]](handler: ExceptionHandler) extends EndpointInte
       override def onDecodeFailure(
           ctx: DecodeFailureContext
       )(implicit monad: MonadError[F], bodyListener: BodyListener[F, B]): F[Option[ServerResponse[B]]] = {
-        monad.handleError(decodeHandler.onDecodeFailure(ctx)) { case e: Exception =>
+        monad.handleError(decodeHandler.onDecodeFailure(ctx)) { case NonFatal(e) =>
           handler(ExceptionContext(e, ctx.endpoint, ctx.request)) match {
             case Some(value) => responder(ctx.request, value).map(Some(_))
             case None        => monad.error(e)
