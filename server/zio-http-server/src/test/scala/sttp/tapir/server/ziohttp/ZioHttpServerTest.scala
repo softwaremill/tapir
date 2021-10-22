937f6da933ea6094db4704f49982f426c531411f
Michał Ossowski <michal.ossowski@softwaremill.com>
2021-10-22 10:52:05 +0200

    ZioHttpServerTest - fix test not being executed

--- server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpServerTest.scala
+++ server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpServerTest.scala
@@ -34,7 +34,10 @@ class ZioHttpServerTest extends TestSuite {
         ).tests() ++
           // TODO: re-enable static content once a newer zio http is available. Currently these tests often fail with:
           // Cause: java.io.IOException: parsing HTTP/1.1 status line, receiving [f2 content], parser state [STATUS_LINE]
-          new AllServerTests(createServerTest, interpreter, backend, basic = false, staticContent = false).tests() ++
+          new AllServerTests(
+            createServerTest, interpreter, backend,
+            basic = false, file = false, multipart = false, staticContent = false
+          ).tests() ++
           new ServerStreamingTests(createServerTest, ZioStreams).tests()
     }
   }
