9a070a17bd169270ea018f8cfb08287969042383
adamw <adam@warski.org>
2021-10-27 11:31:33 +0200

    Adjust exception handling

--- server/vertx/src/main/scala/sttp/tapir/server/vertx/encoders/VertxOutputEncoders.scala
+++ server/vertx/src/main/scala/sttp/tapir/server/vertx/encoders/VertxOutputEncoders.scala
@@ -4,21 +4,20 @@ import io.vertx.core.Future
 import io.vertx.ext.web.RoutingContext
 import sttp.tapir.model.ServerResponse
 
-import scala.util.{Failure, Success, Try}
+import scala.util.control.NonFatal
 
 object VertxOutputEncoders {
   private[vertx] def apply(serverResponse: ServerResponse[RoutingContext => Future[Void]]): RoutingContext => Future[Void] = { rc =>
     val resp = rc.response
-    Try {
+    try {
       resp.setStatusCode(serverResponse.code.code)
       serverResponse.headers.foreach { h => resp.headers.add(h.name, h.value) }
       serverResponse.body match {
         case Some(responseHandler) => responseHandler(rc)
-        case None => resp.end()
+        case None                  => resp.end()
       }
-    } match {
-      case Success(r) => r
-      case Failure(ex) => Future.failedFuture(ex)
+    } catch {
+      case NonFatal(e) => Future.failedFuture(e)
     }
   }
 }
