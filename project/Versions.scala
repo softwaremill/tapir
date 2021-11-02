e3e1253b3ea93a6c90cfb5513aefca22ef9575ef
Scala Steward <me@scala-steward.org>
2021-11-02 15:44:19 +0100

    Update akka-http to 10.2.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val sttp = "3.3.16"
   val sttpModel = "1.4.16"
   val sttpShared = "1.2.7"
-  val akkaHttp = "10.2.6"
+  val akkaHttp = "10.2.7"
   val akkaStreams = "2.6.17"
   val swaggerUi = "3.52.5"
   val upickle = "1.4.2"
