017b299ed4071d34ffa69f002522eb86e5116a15
Scala Steward <me@scala-steward.org>
2021-11-10 17:04:26 +0100

    Update zio-json to 0.2.0-M2

--- project/Versions.scala
+++ project/Versions.scala
@@ -22,7 +22,7 @@ object Versions {
   val enumeratum = "1.7.0"
   val zio = "1.0.12"
   val zioInteropCats = "3.1.1.0"
-  val zioJson = "0.2.0-M1"
+  val zioJson = "0.2.0-M2"
   val playClient = "2.1.3"
   val playServer = "2.8.7"
   val tethys = "0.25.0"
