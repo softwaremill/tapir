5384fa9664bf4b90999043d99c7799b9aa4f684f
Scala Steward <me@scala-steward.org>
2021-12-11 04:11:39 +0100

    Update swagger-ui to 4.1.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val sttpShared = "1.2.7"
   val akkaHttp = "10.2.7"
   val akkaStreams = "2.6.17"
-  val swaggerUi = "4.1.2"
+  val swaggerUi = "4.1.3"
   val upickle = "1.4.2"
   val playJson = "2.9.2"
   val finatra = "21.2.0"
