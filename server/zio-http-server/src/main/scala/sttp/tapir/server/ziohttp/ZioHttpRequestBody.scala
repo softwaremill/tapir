c257f08c32f39f44a81e330053ff1abdfdc50ac1
jules Ivanic <jules.ivanic@gmail.com>
2021-12-12 11:06:47 +0800

    format

--- server/zio-http-server/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpRequestBody.scala
+++ server/zio-http-server/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpRequestBody.scala
@@ -2,16 +2,20 @@ package sttp.tapir.server.ziohttp
 
 import sttp.capabilities
 import sttp.capabilities.zio.ZioStreams
-import sttp.tapir.{FileRange, RawBodyType}
+import sttp.tapir.FileRange
+import sttp.tapir.RawBodyType
 import sttp.tapir.model.ServerRequest
-import sttp.tapir.server.interpreter.{RawValue, RequestBody}
-import zio.stream.{Stream, ZStream}
-import zio.{RIO, Task}
+import sttp.tapir.server.interpreter.RawValue
+import sttp.tapir.server.interpreter.RequestBody
+import zhttp.http.Request
+import zio.Chunk
+import zio.RIO
+import zio.Task
+import zio.stream.Stream
+import zio.stream.ZStream
 
 import java.io.ByteArrayInputStream
 import java.nio.ByteBuffer
-import zio.Chunk
-import zhttp.http.Request
 
 class ZioHttpRequestBody[R](request: Request, serverRequest: ServerRequest, serverOptions: ZioHttpServerOptions[R])
     extends RequestBody[RIO[R, *], ZioStreams] {
