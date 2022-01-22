27799b506bbf5f4abeaf70c83e2183e148472bc5
Pierre Ricadat <ghostdogpr@gmail.com>
2022-01-22 22:38:55 +0900

    Remove deprecation warnings

--- integrations/zio/src/test/scala/sttp/tapir/ztapir/ZTapirTest.scala
+++ integrations/zio/src/test/scala/sttp/tapir/ztapir/ZTapirTest.scala
@@ -74,7 +74,7 @@ object ZTapirTest extends DefaultRunnableSpec with ZTapir {
 
   private def failedAutLogic(userName: String): UIO[User] = ZIO(10 / 0).orDie.as(User(userName))
 
-  private val testZServerLogicErrorHandling = testM("zServerLogic error handling") {
+  private val testZServerLogicErrorHandling = test("zServerLogic error handling") {
     val testEndpoint: PublicEndpoint[Unit, TestError, String, Any] =
       endpoint.in("foo" / "bar").errorOut(plainBody[TestError]).out(stringBody)
 
@@ -97,7 +97,7 @@ object ZTapirTest extends DefaultRunnableSpec with ZTapir {
       }
   }
 
-  private val testZServerSecurityLogicErrorHandling = testM("zServerLogicForCurrent error handling") {
+  private val testZServerSecurityLogicErrorHandling = test("zServerLogicForCurrent error handling") {
     val securedEndpoint: ZPartialServerEndpoint[Any, String, User, Unit, TestError, Unit, Any] =
       endpoint.securityIn(header[String]("X-User-Name")).errorOut(plainBody[TestError]).zServerSecurityLogic[Any, User](failedAutLogic)
 
