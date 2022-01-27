89646269803a1bb9be2cd8b084571075e18d2368
Scala Steward <me@scala-steward.org>
2022-01-27 15:45:47 +0100

    Update upickle to 1.5.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val akkaHttp = "10.2.7"
   val akkaStreams = "2.6.18"
   val swaggerUi = "4.2.1"
-  val upickle = "1.4.4"
+  val upickle = "1.5.0"
   val playJson = "2.9.2"
   val finatra = "22.1.0"
   val catbird = "21.12.0"
