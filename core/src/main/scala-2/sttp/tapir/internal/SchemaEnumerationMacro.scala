fff9202740a8c4bd3b0ba165cc6b652eb6b3c650
kubinio123 <jakub.cichy@softwaremill.com>
2022-02-22 15:09:58 +0100

    derive enumeration schema with macro scala 2

--- core/src/main/scala-2/sttp/tapir/internal/SchemaEnumerationMacro.scala
+++ core/src/main/scala-2/sttp/tapir/internal/SchemaEnumerationMacro.scala
@@ -1,6 +1,8 @@
 package sttp.tapir.internal
 
+import sttp.tapir.Schema
 import sttp.tapir.macros.CreateDerivedEnumerationSchema
+
 import scala.reflect.macros.blackbox
 
 object SchemaEnumerationMacro {
@@ -11,5 +13,26 @@ object SchemaEnumerationMacro {
     c.Expr[CreateDerivedEnumerationSchema[T]](q"""
       new sttp.tapir.macros.CreateDerivedEnumerationSchema(Validator.derivedEnumeration)
     """)
+  }
+
+  def derivedEnumerationValue[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[Schema[T]] = {
+    import c.universe._
+
+    val Enumeration = typeOf[scala.Enumeration]
+    val SchemaAnnotations = typeOf[sttp.tapir.internal.SchemaAnnotations[_]]
+
+    val weakTypeT = weakTypeOf[T]
+    val owner = weakTypeT.typeSymbol.owner
+
+    if (!(owner.asClass.toType <:< Enumeration)) {
+      c.abort(c.enclosingPosition, s"Can only derive Schema for values owned by scala.Enumeration")
+    } else {
+
+      val enumeration = TermName(weakTypeT.toString.split("\\.").dropRight(1).last)
+      val validator = q"sttp.tapir.Validator.enumeration($enumeration.values.toList)"
+      val schemaAnnotations = c.inferImplicitValue(appliedType(SchemaAnnotations, weakTypeT))
+
+      c.Expr[Schema[T]](q"$schemaAnnotations.enrich(Schema.string[$weakTypeT].validate($validator))")
+    }
   }
 }
