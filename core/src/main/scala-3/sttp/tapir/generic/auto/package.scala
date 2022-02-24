72daca30d91614b5b83378b0cd928e124df46ea4
kubinio123 <jakub.cichy@softwaremill.com>
2022-02-24 12:59:04 +0100

    clean up imports

--- core/src/main/scala-3/sttp/tapir/generic/auto/package.scala
+++ core/src/main/scala-3/sttp/tapir/generic/auto/package.scala
@@ -1,10 +1,6 @@
 package sttp.tapir.generic
 
 import sttp.tapir.Schema
-import sttp.tapir.SchemaType.SString
-import sttp.tapir.internal.SchemaMagnoliaDerivation
-
-import scala.deriving.Mirror
 
 package object auto extends SchemaDerivation
 
