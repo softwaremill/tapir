93b7d7aa8f05cecad7858ea287ad78e78b7e534a
adamw <adam@warski.org>
2022-03-16 15:40:17 +0100

    Fix comment

--- core/src/main/scala/sttp/tapir/attribute.scala
+++ core/src/main/scala/sttp/tapir/attribute.scala
@@ -21,7 +21,7 @@ object AttributeKey extends AttributeKeyMacros
 /** An attribute is arbitrary data that is attached to an endpoint or endpoint input/output. The data is not interpreted by tapir's core in
   * any way, but might be used by interpreters.
   *
-  * Typically, you'll add attributes using [[Endpoint.attribute]] and [[EndpointTransput.Basic.attribute]]. The attribute keys should be
+  * Typically, you'll add attributes using [[Endpoint.attribute]] and [[EndpointTransput.Atom.attribute]]. The attribute keys should be
   * defined by the interpreters which are using them, and made available for import.
   */
 case class AttributeMap private (private val storage: Map[String, Any]) {
