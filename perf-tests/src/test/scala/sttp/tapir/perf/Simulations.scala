802c300a289e88b7fe0736c6b9244a279b280bbe
adamw <adam@warski.org>
2022-03-18 10:10:30 +0100

    Remove warmup

--- perf-tests/src/test/scala/sttp/tapir/perf/Simulations.scala
+++ perf-tests/src/test/scala/sttp/tapir/perf/Simulations.scala
@@ -4,28 +4,27 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.PopulationBuilder
 import io.gatling.http.Predef._
 
-import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object CommonSimulations {
   private val userCount = 100
   private val baseUrl = "http://127.0.0.1:8080"
 
-  def testScenario(warmupDuration: Duration, duration: Duration, routeNumber: Int): PopulationBuilder = {
+  def testScenario(duration: FiniteDuration, routeNumber: Int): PopulationBuilder = {
     val httpProtocol = http.baseUrl(baseUrl)
-    def execHttpGet(suffix: String) = exec(http(s"HTTP GET /path$routeNumber/4 $suffix").get(s"/path$routeNumber/4"))
+    val execHttpGet = exec(http(s"HTTP GET /path$routeNumber/4").get(s"/path$routeNumber/4"))
+
     scenario(s"Repeatedly invoke GET of route number $routeNumber")
-      .during(warmupDuration.toSeconds.toInt)(execHttpGet("warmup"))
-      .pause(5.seconds)
-      .during(duration.toSeconds.toInt)(execHttpGet("test"))
+      .during(duration.toSeconds.toInt)(execHttpGet)
       .inject(atOnceUsers(userCount))
       .protocols(httpProtocol)
   }
 }
 
 class OneRouteSimulation extends Simulation {
-  setUp(CommonSimulations.testScenario(10.seconds, 1.minute, 0))
+  setUp(CommonSimulations.testScenario(1.minute, 0))
 }
 
 class MultiRouteSimulation extends Simulation {
-  setUp(CommonSimulations.testScenario(10.seconds, 1.minute, 127))
+  setUp(CommonSimulations.testScenario(1.minute, 127))
 }
