3b544ee140e4a553b74c2c5b401ed42ad1278fc4
adamw <adam@warski.org>
2022-03-18 09:41:51 +0100

    Add warmup, simplify simulation

--- perf-tests/src/test/scala/sttp/tapir/perf/Simulations.scala
+++ perf-tests/src/test/scala/sttp/tapir/perf/Simulations.scala
@@ -1,34 +1,31 @@
 package sttp.tapir.perf
 
 import io.gatling.core.Predef._
-import io.gatling.core.structure.{PopulationBuilder, ScenarioBuilder}
+import io.gatling.core.structure.PopulationBuilder
 import io.gatling.http.Predef._
 
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{Duration, DurationInt}
 
 object CommonSimulations {
-  private val duration = 1.minute
   private val userCount = 100
   private val baseUrl = "http://127.0.0.1:8080"
 
-  val scn: ScenarioBuilder = scenario("get plaintext")
-    .during(duration.toSeconds.toInt) {
-      exec(
-        http("first plaintext")
-          .get("/4")
-      )
-    }
-
-  def genericInjection(n: Int): PopulationBuilder = {
-    val httpProtocol = http.baseUrl(baseUrl + "/path" + n.toString)
-    scn.inject(atOnceUsers(userCount)).protocols(httpProtocol)
+  def testScenario(warmupDuration: Duration, duration: Duration, routeNumber: Int): PopulationBuilder = {
+    val httpProtocol = http.baseUrl(baseUrl)
+    def execHttpGet(suffix: String) = exec(http(s"HTTP GET /path$routeNumber/4 $suffix").get(s"/path$routeNumber/4"))
+    scenario(s"Repeatedly invoke GET of route number $routeNumber")
+      .during(warmupDuration.toSeconds.toInt)(execHttpGet("warmup"))
+      .pause(5.seconds)
+      .during(duration.toSeconds.toInt)(execHttpGet("test"))
+      .inject(atOnceUsers(userCount))
+      .protocols(httpProtocol)
   }
 }
 
 class OneRouteSimulation extends Simulation {
-  setUp(CommonSimulations.genericInjection(0))
+  setUp(CommonSimulations.testScenario(10.seconds, 1.minute, 0))
 }
 
 class MultiRouteSimulation extends Simulation {
-  setUp(CommonSimulations.genericInjection(127))
+  setUp(CommonSimulations.testScenario(10.seconds, 1.minute, 127))
 }
