ddb688cf64e4dfac05caca92059ab2e9500e0f39
João Ferreira <jtjeferreira@gmail.com>
2022-04-28 13:16:21 +0100

    revert changes to ServerBasicTests, but increase maxMemoryBuffer conf

--- server/play-server/src/test/scala/sttp/tapir/server/play/PlayTestServerInterpreter.scala
+++ server/play-server/src/test/scala/sttp/tapir/server/play/PlayTestServerInterpreter.scala
@@ -4,7 +4,8 @@ import akka.actor.ActorSystem
 import cats.data.NonEmptyList
 import cats.effect.{IO, Resource}
 import play.api.Mode
-import play.api.mvc.{Handler, RequestHeader}
+import play.api.http.ParserConfiguration
+import play.api.mvc.{Handler, PlayBodyParsers, RequestHeader}
 import play.api.routing.Router
 import play.api.routing.Router.Routes
 import play.core.server.{DefaultAkkaHttpServerComponents, ServerConfig}
@@ -22,6 +23,8 @@ class PlayTestServerInterpreter(implicit actorSystem: ActorSystem)
 
   override def route(es: List[ServerEndpoint[AkkaStreams with WebSockets, Future]], interceptors: Interceptors): Routes = {
     val serverOptions: PlayServerOptions = interceptors(PlayServerOptions.customiseInterceptors).options
+      //increase the default maxMemoryBuffer so that tests pass
+      .copy(playBodyParsers = PlayBodyParsers(conf = ParserConfiguration(maxMemoryBuffer = 1024000)))
     PlayServerInterpreter(serverOptions).toRoutes(es)
   }
 
