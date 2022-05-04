155995f85e8b6bc2fdc75c9b6be444dde081f0fc
Scala Steward <me@scala-steward.org>
2022-05-04 13:13:01 +0200

    Update zio-interop-cats to 3.3.0-RC7

--- project/Versions.scala
+++ project/Versions.scala
@@ -24,7 +24,7 @@ object Versions {
   val zio1InteropCats = "3.2.9.1"
   val zio1Json = "0.2.0-M4"
   val zio = "2.0.0-RC5"
-  val zioInteropCats = "3.3.0-RC6"
+  val zioInteropCats = "3.3.0-RC7"
   val zioInteropReactiveStreams = "2.0.0-RC6"
   val zioJson = "0.3.0-RC7"
   val playClient = "2.1.7"
