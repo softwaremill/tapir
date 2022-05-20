0b7bd6579ba709b491915cca7b2039baa6f2e8a5
adamw <adam@warski.org>
2022-05-20 15:02:39 +0200

    Properly set content-length header if known in the zio-http interpreter

--- server/zio-http-server/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpInterpreter.scala
+++ server/zio-http-server/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpInterpreter.scala
@@ -2,7 +2,7 @@ package sttp.tapir.server.ziohttp
 
 import io.netty.handler.codec.http.HttpResponseStatus
 import sttp.capabilities.zio.ZioStreams
-import sttp.model.{Header => SttpHeader}
+import sttp.model.{HeaderNames, Header => SttpHeader}
 import sttp.monad.MonadError
 import sttp.tapir.server.interceptor.RequestResult
 import sttp.tapir.server.interceptor.reject.RejectInterceptor
@@ -10,7 +10,6 @@ import sttp.tapir.server.interpreter.{FilterServerEndpoints, ServerInterpreter}
 import sttp.tapir.ztapir._
 import zhttp.http.{Http, HttpData, Request, Response, Status, Header => ZioHttpHeader, Headers => ZioHttpHeaders}
 import zio._
-import zio.stream.Stream
 
 trait ZioHttpInterpreter[R] {
 
@@ -22,7 +21,7 @@ trait ZioHttpInterpreter[R] {
   def toHttp(ses: List[ZServerEndpoint[R, ZioStreams]]): Http[R, Throwable, Request, Response] = {
     implicit val bodyListener: ZioHttpBodyListener[R] = new ZioHttpBodyListener[R]
     implicit val monadError: MonadError[RIO[R, *]] = new RIOMonadError[R]
-    val interpreter = new ServerInterpreter[ZioStreams, RIO[R, *], Stream[Throwable, Byte], ZioStreams](
+    val interpreter = new ServerInterpreter[ZioStreams, RIO[R, *], ZioHttpResponseBody, ZioStreams](
       FilterServerEndpoints(ses),
       new ZioHttpRequestBody(zioHttpServerOptions),
       new ZioHttpToResponseBody,
@@ -36,11 +35,18 @@ trait ZioHttpInterpreter[R] {
           .apply(ZioHttpServerRequest(req))
           .map {
             case RequestResult.Response(resp) =>
+              val baseHeaders = resp.headers.groupBy(_.name).map(sttpToZioHttpHeader).toList
+              val allHeaders = resp.body match {
+                case Some((_, Some(contentLength))) if resp.contentLength.isEmpty =>
+                  (HeaderNames.ContentLength, contentLength.toString) :: baseHeaders
+                case _ => baseHeaders
+              }
+
               Http.succeed(
                 Response(
                   status = Status.fromHttpResponseStatus(HttpResponseStatus.valueOf(resp.code.code)),
-                  headers = ZioHttpHeaders(resp.headers.groupBy(_.name).map(sttpToZioHttpHeader).toList),
-                  data = resp.body.map(stream => HttpData.fromStream(stream)).getOrElse(HttpData.empty)
+                  headers = ZioHttpHeaders(allHeaders),
+                  data = resp.body.map { case (stream, _) => HttpData.fromStream(stream) }.getOrElse(HttpData.empty)
                 )
               )
             case RequestResult.Failure(_) => Http.empty
@@ -51,7 +57,6 @@ trait ZioHttpInterpreter[R] {
 
   private def sttpToZioHttpHeader(hl: (String, Seq[SttpHeader])): ZioHttpHeader =
     (hl._1, hl._2.map(f => f.value).mkString(", "))
-
 }
 
 object ZioHttpInterpreter {
