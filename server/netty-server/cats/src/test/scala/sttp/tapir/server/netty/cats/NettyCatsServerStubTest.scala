62e8f47068cb0e63ab400af11ab54dbb2133aeca
adamw <adam@warski.org>
2022-05-31 17:17:31 +0200

    Fix netty

---
+++ server/netty-server/cats/src/test/scala/sttp/tapir/server/netty/cats/NettyCatsServerStubTest.scala
@@ -0,0 +1,26 @@
+package sttp.tapir.server.netty.cats
+
+import cats.effect.IO
+import cats.effect.std.Dispatcher
+import cats.effect.unsafe.implicits.global
+import sttp.client3.testing.SttpBackendStub
+import sttp.tapir.integ.cats.CatsMonadError
+import sttp.tapir.server.interceptor.CustomiseInterceptors
+import sttp.tapir.server.netty.NettyOptionsBuilder
+import sttp.tapir.server.tests.{CreateServerStubTest, ServerStubTest}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+class NettyCatsCreateServerStubTest extends CreateServerStubTest[IO, NettyCatsServerOptions[IO]] {
+  val (dispatcher, shutdownDispatcher) = Dispatcher[IO].allocated.unsafeRunSync()
+
+  override def customiseInterceptors: CustomiseInterceptors[IO, NettyCatsServerOptions[IO]] =
+    NettyCatsServerOptions.customiseInterceptors[IO](dispatcher, NettyOptionsBuilder.make().tcp().build)
+  override def stub[R]: SttpBackendStub[IO, R] = SttpBackendStub(new CatsMonadError[IO]())
+  override def asFuture[A]: IO[A] => Future[A] = io => io.unsafeToFuture()
+
+  override def cleanUp(): Unit = Await.ready(shutdownDispatcher.unsafeToFuture(), Duration.Inf)
+}
+
+class NettyCatsServerStubTest extends ServerStubTest(new NettyCatsCreateServerStubTest)
