ccfa5b284089b429ee1a3d2ea0e3d7c892a00c90
scala-steward <scala-steward>
2022-07-02 00:06:32 +0000

    Update fs2-reactive-streams to 3.2.9

--- project/Versions.scala
+++ project/Versions.scala
@@ -42,7 +42,7 @@ object Versions {
   val armeria = "1.16.0"
   val scalaJava8Compat = "1.0.2"
   val scalaCollectionCompat = "2.7.0"
-  val fs2 = "3.2.8"
+  val fs2 = "3.2.9"
   val decline = "2.3.0"
   val quicklens = "1.8.8"
   val openTelemetry = "1.15.0"
