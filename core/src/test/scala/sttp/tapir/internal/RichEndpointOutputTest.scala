ae237e84a5adfe1551eb41fd114e4c1ee5d39f90
adamw <adam@warski.org>
2022-08-08 12:58:57 +0200

    Move test to shared sources as the sttp-model issue is fixed

--- core/src/test/scala/sttp/tapir/internal/RichEndpointOutputTest.scala
+++ core/src/test/scala/sttp/tapir/internal/RichEndpointOutputTest.scala
@@ -5,7 +5,6 @@ import org.scalatest.matchers.should.Matchers
 import sttp.model.MediaType
 import sttp.tapir._
 
-// TODO: move to shared test sources when https://github.com/softwaremill/sttp-model/issues/188 is fixed
 class RichEndpointOutputTest extends AnyFlatSpec with Matchers {
   "output media type" should "match content type with lower and upper case charset" in {
     val o = endpoint.put
