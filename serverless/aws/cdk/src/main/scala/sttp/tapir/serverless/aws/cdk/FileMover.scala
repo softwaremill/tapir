de88adaf4622a815e0dd1f33b77f6c0197504d7c
ayeo <me@ayeo.pl>
2022-08-24 16:08:30 +0200

    Dev checkpoint

--- serverless/aws/cdk/src/main/scala/sttp/tapir/serverless/aws/cdk/FileMover.scala
+++ serverless/aws/cdk/src/main/scala/sttp/tapir/serverless/aws/cdk/FileMover.scala
@@ -3,12 +3,12 @@ package sttp.tapir.serverless.aws.cdk
 import cats.effect.IO
 import cats.implicits.toTraverseOps
 
+import java.io.File
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 import scala.io.Source
-import scala.reflect.io.Directory
 
-class FileMover(sourceDir: String, destDir: String) { //fixme use F[_]
+class FileMover(sourceDir: String, destDir: String) { // fixme use F[_]
 
   def move(files: Map[String, String]): IO[Unit] =
     files.toList.map { case (source, destination) => move(source, destination) }.sequence.void
@@ -20,7 +20,7 @@ class FileMover(sourceDir: String, destDir: String) { //fixme use F[_]
   }
 
   def clear: IO[Unit] =
-    IO.blocking(new Directory(Paths.get(sourceDir).toFile).deleteRecursively())
+    deleteRecursively(Paths.get(destDir).toFile)
 
   def put(content: IO[String], destination: String): IO[Unit] = {
     createDirectories(destination) >> content.flatMap(c => save(c, destination))
@@ -28,6 +28,17 @@ class FileMover(sourceDir: String, destDir: String) { //fixme use F[_]
 
   private def save(content: String, destination: String): IO[Unit] = IO.blocking {
     Files.write(Paths.get(destination), content.getBytes(StandardCharsets.UTF_8))
+  }
+
+  private def deleteRecursively(file: File): IO[Unit] = {
+    val deleteFiles = Option(file.listFiles)
+      .map(_.toList)
+      .getOrElse(List.empty)
+      .map(deleteRecursively)
+      .sequence
+      .void
+
+    deleteFiles >> IO.blocking(file.delete()).void
   }
 
   private def createDirectories(destination: String): IO[Unit] = IO.blocking {
