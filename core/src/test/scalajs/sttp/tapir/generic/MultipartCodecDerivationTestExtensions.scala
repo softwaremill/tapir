e24de182558ae8969afd82823273aac273291453
adamw <adam@warski.org>
2022-09-07 15:43:28 +0200

    Fix JS file usage, update scalajs-dom

--- core/src/test/scalajs/sttp/tapir/generic/MultipartCodecDerivationTestExtensions.scala
+++ core/src/test/scalajs/sttp/tapir/generic/MultipartCodecDerivationTestExtensions.scala
@@ -4,8 +4,8 @@ import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
 import scala.scalajs.js.typedarray.AB2TA
 
+import org.scalajs.dom.BlobPart
 import org.scalajs.dom.File
-import sttp.tapir.dom.experimental.{File => DomFileWithBody}
 
 trait MultipartCodecDerivationTestExtensions {
 
@@ -15,8 +15,8 @@ trait MultipartCodecDerivationTestExtensions {
     def delete(): Unit = {}
   }
 
-  def createTempFile() = new DomFileWithBody(
-    Array(Array.empty[Byte].toTypedArray.asInstanceOf[js.Any]).toJSArray,
+  def createTempFile() = new File(
+    Iterable(Array.empty[Byte].toTypedArray.asInstanceOf[BlobPart]).toJSIterable,
     "temp.txt"
   )
 }
