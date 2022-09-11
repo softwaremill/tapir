2a6edd47f4e5e2e444e85cc69cc50a73e7c5d178
Massimo Siani <massimosiani@users.noreply.github.com>
2022-09-11 21:15:39 +0200

    add an aws js handler for cats Resource

--- serverless/aws/lambda/src/main/scalajs/sttp/tapir/serverless/aws/lambda/js/AwsJsRouteHandler.scala
+++ serverless/aws/lambda/src/main/scalajs/sttp/tapir/serverless/aws/lambda/js/AwsJsRouteHandler.scala
@@ -1,6 +1,6 @@
 package sttp.tapir.serverless.aws.lambda.js
 
-import cats.effect.IO
+import cats.effect.{IO, Resource}
 import cats.effect.unsafe.implicits.global
 import sttp.client3.impl.cats.CatsMonadAsyncError
 import sttp.monad.{FutureMonad, MonadError}
@@ -11,7 +11,7 @@ import scala.scalajs.js.JSConverters._
 
 object AwsJsRouteHandler {
 
-  private def toJsRoute[F[_]: MonadError](route: Route[F])(implicit monadError: MonadError[F]): JsRoute[F] = { awsJsRequest: AwsJsRequest =>
+  private def toJsRoute[F[_]](route: Route[F])(implicit monadError: MonadError[F]): JsRoute[F] = { awsJsRequest: AwsJsRequest =>
     monadError.map(route.apply(AwsJsRequest.toAwsRequest(awsJsRequest)))(AwsJsResponse.fromAwsResponse)
   }
 
@@ -25,5 +25,12 @@ object AwsJsRouteHandler {
     implicit val monadError: MonadError[IO] = new CatsMonadAsyncError[IO]()
     val jsRoute = toJsRoute(route)
     jsRoute(event).unsafeToFuture().toJSPromise
+  }
+
+  def catsResourceHandler(event: AwsJsRequest, routeR: Resource[IO, Route[IO]])(implicit
+      ec: ExecutionContext
+  ): scala.scalajs.js.Promise[AwsJsResponse] = {
+    implicit val monadError: MonadError[IO] = new CatsMonadAsyncError[IO]()
+    routeR.use(route => toJsRoute(route).apply(event)).unsafeToFuture().toJSPromise
   }
 }
