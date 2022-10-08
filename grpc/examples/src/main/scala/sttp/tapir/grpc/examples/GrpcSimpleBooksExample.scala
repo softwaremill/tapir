fe6b5ba2b089f6fce3dc331b5cae465606438844
Mateusz Borek <mateusz.borek@softwaremill.com>
2022-10-08 07:38:44 +0200

    Polish grpc example

--- grpc/examples/src/main/scala/sttp/tapir/grpc/examples/GrpcSimpleBooksExample.scala
+++ grpc/examples/src/main/scala/sttp/tapir/grpc/examples/GrpcSimpleBooksExample.scala
@@ -8,38 +8,41 @@ import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.StrictLogging
 import sttp.tapir._
 import sttp.tapir.grpc.protobuf._
-import sttp.tapir.grpc.protobuf.model._
 import sttp.tapir.grpc.protobuf.pbdirect._
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.akkagrpc.AkkaGrpcServerInterpreter
 import sttp.tapir.generic.auto._
-import sttp.tapir.grpc.examples.gen.{SimpleBook => GenSimpleBook, Library => GenLibrary, LibraryClient => GenLibraryClient}
+import sttp.tapir.grpc.examples.gen.{Library => GenLibrary, LibraryClient => GenLibraryClient, AddBookMsg => GenAddBookMsg}
 
-import java.io.ByteArrayOutputStream
+import java.util.concurrent.atomic.AtomicLong
 import scala.concurrent.{Await, ExecutionContext, Future}
 
-case class SimpleBook(title: String)
+case class SimpleBook(id: Long, title: String, description: String)
+case class AddBookMsg(title: String, description: String)
+
 
 /** Descriptions of endpoints used in the example.
-  */
+ */
 object Endpoints {
   val addBook = endpoint
     .in("Library" / "AddBook")
-    .in(grpcBody[SimpleBook])
+    .in(grpcBody[AddBookMsg])
     .out(grpcBody[SimpleBook])
 
-  val es = List(addBook)
+  val endpoints = List(addBook)
 }
 
-object SimpleBooksExample extends StrictLogging {
+object SimpleBooksExampleServer extends StrictLogging {
 
   import Endpoints._
+
+  private val counter = new AtomicLong(0)
 
   def booksServerEndpoints: List[ServerEndpoint[Any, Future]] =
     List(
       addBook.serverLogic { book =>
-        println(book)
-        Future.successful(book.asRight[Unit])
+        logger.info(s"Adding a new book [$book]")
+        Future.successful(SimpleBook(counter.getAndIncrement(), book.title, book.description).asRight[Unit])
       }
     )
 
@@ -49,45 +52,44 @@ object SimpleBooksExample extends StrictLogging {
       .withFallback(ConfigFactory.defaultApplication())
     val system = ActorSystem("HelloWorld", conf)
 
-    new TestServer(system).run()
+    new ExampleGrpcServer(system).run()
   }
 }
 
-class TestServer(system: ActorSystem) {
+class ExampleGrpcServer(system: ActorSystem) extends StrictLogging {
   def run(): Future[Http.ServerBinding] = {
     // Akka boot up code
     implicit val sys: ActorSystem = system
     implicit val ec: ExecutionContext = sys.dispatcher
 
-    val route = new AkkaGrpcServerInterpreter {
-      override implicit def executionContext: ExecutionContext = ec
-    }.toRoute(SimpleBooksExample.booksServerEndpoints)
+    val route = AkkaGrpcServerInterpreter(ec).toRoute(SimpleBooksExampleServer.booksServerEndpoints)
 
     val binding = Http().newServerAt("127.0.0.1", 8080).bind(route)
 
     // report successful binding
-    binding.foreach { binding => println(s"gRPC server bound to: ${binding.localAddress}") }
+    binding.foreach { binding => logger.info(s"gRPC server bound to: ${binding.localAddress}") }
 
     binding
   }
 }
 
-object MainGenerator extends ProtoSchemaGenerator {
-  val path: String = "/Users/mborek/OSS/tapir/grpc/examples/src/main/protobuf/main.proto"
-  val endpoints = Endpoints.es
-  val packageName = "sttp.tapir.grpc.examples.gen"
-
-  generate()
+object SimpleBookExampleProtoGenerator extends App {
+  ProtoSchemaGenerator.generate(
+    path = "grpc/examples/src/main/protobuf/main.proto",
+    packageName = "sttp.tapir.grpc.examples.gen",
+    endpoints = Endpoints.endpoints
+  )
 }
 
-object ClientMain extends App with StrictLogging {
+object SimpleBookExampleClient extends App with StrictLogging {
+
   import scala.concurrent.duration._
 
   implicit val sys = ActorSystem("HelloWorldClient")
   implicit val ec = sys.dispatcher
 
-  val client = GenLibraryClient(GrpcClientSettings.connectToServiceAt("localhost", 8080).withTls(false))  
-  val result = Await.result(client.addBook(GenSimpleBook("TEST_BOOK")), 10.second)
+  val client = GenLibraryClient(GrpcClientSettings.connectToServiceAt("localhost", 8080).withTls(false))
+  val result = Await.result(client.addBook(GenAddBookMsg("TEST_BOOK", "TEST")), 10.second)
 
-  println(s"Result: [$result]")
+  logger.info(s"Result: [$result]")
 }
