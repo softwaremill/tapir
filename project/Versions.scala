debe4d1acec857e5bae809b4da9244a84e468268
scala-steward <scala-steward>
2022-10-28 00:06:40 +0000

    Update circe-yaml to 0.14.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val catsEffect = "3.3.14"
   val circe = "0.14.3"
   val circeGenericExtras = "0.14.2"
-  val circeYaml = "0.14.1"
+  val circeYaml = "0.14.2"
   val sttp = "3.8.3"
   val sttpModel = "1.5.2"
   val sttpShared = "1.3.10"
