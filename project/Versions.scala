6a9fd0113386bf34cc98382a047cd4d6f5e4418f
scala-steward <scala-steward>
2022-11-02 00:06:25 +0000

    Update zio, zio-streams, zio-test, ... to 2.0.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,7 +28,7 @@ object Versions {
   val zio1InteropCats = "3.2.9.1"
   val zio1Json = "0.2.0"
   val zio1InteropReactiveStreams = "1.3.12"
-  val zio = "2.0.2"
+  val zio = "2.0.3"
   val zioInteropCats = "3.3.0"
   val zioInteropReactiveStreams = "2.0.0"
   val zioJson = "0.3.0"
