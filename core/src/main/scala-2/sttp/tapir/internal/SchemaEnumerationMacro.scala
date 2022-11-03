0867f85987a4d19be7421a29e04405e6eef54a2e
adamw <adam@warski.org>
2022-11-03 18:06:19 +0100

    Add derivation of enumeration codecs

--- core/src/main/scala-2/sttp/tapir/internal/SchemaEnumerationMacro.scala
+++ core/src/main/scala-2/sttp/tapir/internal/SchemaEnumerationMacro.scala
@@ -13,7 +13,7 @@ private[tapir] object SchemaEnumerationMacro {
     val weakTypeT = weakTypeOf[T]
     val schemaAnnotations = c.inferImplicitValue(appliedType(SchemaAnnotations, weakTypeT))
 
-    // this needs to be a macro so that we can call another macro - Validator.derivedEnumeration
+    // calling another macro - Validator.derivedEnumeration
     c.Expr[CreateDerivedEnumerationSchema[T]](q"""
       new _root_.sttp.tapir.macros.CreateDerivedEnumerationSchema(_root_.sttp.tapir.Validator.derivedEnumeration, $schemaAnnotations)
     """)
@@ -24,7 +24,7 @@ private[tapir] object SchemaEnumerationMacro {
 
     val SchemaAnnotations = typeOf[SchemaAnnotations[_]]
     val weakTypeT = weakTypeOf[T]
-    val validator = enumerationValueValidator[T](c)
+    val validator = ValidatorEnumerationMacro.enumerationValueValidator[T](c)
     val schemaAnnotations = c.inferImplicitValue(appliedType(SchemaAnnotations, weakTypeT))
 
     c.Expr[CreateDerivedEnumerationSchema[T]](q"""
@@ -37,31 +37,9 @@ private[tapir] object SchemaEnumerationMacro {
 
     val SchemaAnnotations = typeOf[SchemaAnnotations[_]]
     val weakTypeT = weakTypeOf[T]
-    val validator = enumerationValueValidator[T](c)
+    val validator = ValidatorEnumerationMacro.enumerationValueValidator[T](c)
     val schemaAnnotations = c.inferImplicitValue(appliedType(SchemaAnnotations, weakTypeT))
 
     c.Expr[Schema[T]](q"$schemaAnnotations.enrich(Schema.string[$weakTypeT].validate($validator))")
-  }
-
-  private def enumerationValueValidator[T: c.WeakTypeTag](c: blackbox.Context) = {
-    import c.universe._
-
-    val Enumeration = typeOf[scala.Enumeration]
-
-    val weakTypeT = weakTypeOf[T]
-    val owner = weakTypeT.typeSymbol.owner
-
-    if (!(owner.asClass.toType <:< Enumeration)) {
-      c.abort(c.enclosingPosition, "Can only derive Schema for values owned by scala.Enumeration")
-    } else {
-      val enumNameComponents = weakTypeT.toString.split("\\.").dropRight(1)
-      val enumeration = enumNameComponents.toList match {
-        case head :: tail => tail.foldLeft[Tree](Ident(TermName(head))) { case (tree, nextName) => Select(tree, TermName(nextName)) }
-        case Nil          => c.abort(c.enclosingPosition, s"Invalid enum name: ${weakTypeT.toString}")
-      }
-
-      q"_root_.sttp.tapir.Validator.enumeration($enumeration.values.toList, v => Option(v), Some(sttp.tapir.Schema.SName(${enumNameComponents
-          .mkString(".")})))"
-    }
   }
 }
