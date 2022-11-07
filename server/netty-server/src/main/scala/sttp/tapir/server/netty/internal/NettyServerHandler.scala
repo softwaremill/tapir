f2bc24436860f523a11e43fdc3cf840a302d0be6
Rafal Ambrozewicz <rafal.ambrozewicz@softwaremill.com>
2022-11-07 09:28:34 +0100

    2504 support for file ranges in the netty based backend (#2507)
    * Introduced reuse of `contentLengthHeader` and `acceptRangesHeader` in the `TapirStaticContentEndpoints` (as `def`s so they do not break binary compatibility)
    * Added support for file ranges in netty-based backends by using chunk responses for file requests. 
    * Ensured, that streams and files in netty-based backends are not loaded into memory at once, but rather chunked by chunk.
    * Fixed `BodyListener` logic, so that success/failure callback gets called after processing of the request.
    * Number of changes in `NettyServerHandler` to make it all work.
    * Enabled `staticContent` tests for `NettyFutureServerTest`, so that file ranges requests are tested.

--- server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/NettyServerHandler.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/NettyServerHandler.scala
@@ -2,38 +2,25 @@ package sttp.tapir.server.netty.internal
 
 import com.typesafe.scalalogging.Logger
 import io.netty.buffer.{ByteBuf, Unpooled}
-import io.netty.channel.{ChannelFutureListener, ChannelHandlerContext, SimpleChannelInboundHandler}
+import io.netty.channel._
 import io.netty.handler.codec.http._
+import io.netty.handler.stream.{ChunkedFile, ChunkedStream}
 import sttp.monad.MonadError
 import sttp.monad.syntax._
 import sttp.tapir.server.model.ServerResponse
-import sttp.tapir.server.netty.{NettyServerRequest, Route}
+import sttp.tapir.server.netty.{NettyResponse, NettyServerRequest, Route}
+import sttp.tapir.server.netty.NettyResponseContent.{
+  ByteBufNettyResponseContent,
+  ChunkedFileNettyResponseContent,
+  ChunkedStreamNettyResponseContent
+}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.Future
 
 class NettyServerHandler[F[_]](route: Route[F], unsafeRunAsync: (() => F[Unit]) => Unit)(implicit me: MonadError[F])
     extends SimpleChannelInboundHandler[FullHttpRequest] {
 
   private val logger = Logger[NettyServerHandler[F]]
-
-  private def toHttpResponse(interpreterResponse: ServerResponse[ByteBuf], req: FullHttpRequest): FullHttpResponse = {
-    val res = new DefaultFullHttpResponse(
-      req.protocolVersion(),
-      HttpResponseStatus.valueOf(interpreterResponse.code.code),
-      interpreterResponse.body.getOrElse(Unpooled.EMPTY_BUFFER)
-    )
-
-    interpreterResponse.headers
-      .groupBy(_.name)
-      .foreach { case (k, v) =>
-        res.headers().set(k, v.map(_.value).asJava)
-      }
-
-    res.headers().set(HttpHeaderNames.CONTENT_LENGTH, res.content().readableBytes())
-
-    res
-  }
 
   override def channelRead0(ctx: ChannelHandlerContext, request: FullHttpRequest): Unit = {
     if (HttpUtil.is100ContinueExpected(request)) {
@@ -48,31 +35,125 @@ class NettyServerHandler[F[_]](route: Route[F], unsafeRunAsync: (() => F[Unit]) 
             case Some(response) => response
             case None           => ServerResponse.notFound
           }
-          .map(toHttpResponse(_, req))
-          .map(flushResponse(ctx, req, _))
+          .map((serverResponse: ServerResponse[NettyResponse]) => {
+            serverResponse.handle(
+              ctx = ctx,
+              byteBufHandler = (channelPromise, byteBuf) => {
+                val res = new DefaultFullHttpResponse(req.protocolVersion(), HttpResponseStatus.valueOf(serverResponse.code.code), byteBuf)
+
+                res.setHeadersFrom(serverResponse)
+                res.handleContentLengthAndChunkedHeaders(Option(byteBuf.readableBytes()))
+                res.handleCloseAndKeepAliveHeaders(req)
+
+                ctx.writeAndFlush(res, channelPromise).closeIfNeeded(req)
+              },
+              chunkedStreamHandler = (channelPromise, chunkedStream) => {
+                val resHeader: DefaultHttpResponse =
+                  new DefaultHttpResponse(req.protocolVersion(), HttpResponseStatus.valueOf(serverResponse.code.code))
+
+                resHeader.setHeadersFrom(serverResponse)
+                resHeader.handleContentLengthAndChunkedHeaders(None)
+                resHeader.handleCloseAndKeepAliveHeaders(req)
+
+                ctx.write(resHeader)
+                ctx.writeAndFlush(new HttpChunkedInput(chunkedStream), channelPromise).closeIfNeeded(req)
+              },
+              chunkedFileHandler = (channelPromise, chunkedFile) => {
+                val resHeader: DefaultHttpResponse =
+                  new DefaultHttpResponse(req.protocolVersion(), HttpResponseStatus.valueOf(serverResponse.code.code))
+
+                resHeader.setHeadersFrom(serverResponse)
+                resHeader.handleContentLengthAndChunkedHeaders(Option(chunkedFile.length()))
+                resHeader.handleCloseAndKeepAliveHeaders(req)
+
+                ctx.write(resHeader)
+                // HttpChunkedInput will write the end marker (LastHttpContent) for us.
+                ctx.writeAndFlush(new HttpChunkedInput(chunkedFile), channelPromise).closeIfNeeded(req)
+              },
+              noBodyHandler = () => {
+                val res = new DefaultFullHttpResponse(
+                  req.protocolVersion(),
+                  HttpResponseStatus.valueOf(serverResponse.code.code),
+                  Unpooled.EMPTY_BUFFER
+                )
+
+                res.setHeadersFrom(serverResponse)
+                res.handleContentLengthAndChunkedHeaders(Option(Unpooled.EMPTY_BUFFER.readableBytes()))
+                res.handleCloseAndKeepAliveHeaders(req)
+
+                ctx.writeAndFlush(res).closeIfNeeded(req)
+              }
+            )
+          })
           .handleError { case ex: Exception =>
             logger.error("Error while processing the request", ex)
             // send 500
             val res = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR)
             res.headers().set(HttpHeaderNames.CONTENT_LENGTH, 0)
-            flushResponse(ctx, req, res)
+            res.handleCloseAndKeepAliveHeaders(req)
+
+            ctx.writeAndFlush(res).closeIfNeeded(req)
             me.unit(())
           }
           .ensure(me.eval(req.release()))
       } // exceptions should be handled
-
       ()
     }
   }
 
-  def flushResponse(ctx: ChannelHandlerContext, req: HttpRequest, res: HttpResponse): Unit = {
-    if (!HttpUtil.isKeepAlive(req)) {
-      ctx.writeAndFlush(res).addListener(ChannelFutureListener.CLOSE)
-      ()
-    } else {
-      res.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE)
-      ctx.writeAndFlush(res)
-      ()
+  private implicit class RichServerNettyResponse(val r: ServerResponse[NettyResponse]) {
+    def handle(
+        ctx: ChannelHandlerContext,
+        byteBufHandler: (ChannelPromise, ByteBuf) => Unit,
+        chunkedStreamHandler: (ChannelPromise, ChunkedStream) => Unit,
+        chunkedFileHandler: (ChannelPromise, ChunkedFile) => Unit,
+        noBodyHandler: () => Unit
+    ): Unit = {
+      r.body match {
+        case Some(function) => {
+          val values = function(ctx)
+
+          values match {
+            case r: ByteBufNettyResponseContent       => byteBufHandler(r.channelPromise, r.byteBuf)
+            case r: ChunkedStreamNettyResponseContent => chunkedStreamHandler(r.channelPromise, r.chunkedStream)
+            case r: ChunkedFileNettyResponseContent   => chunkedFileHandler(r.channelPromise, r.chunkedFile)
+          }
+        }
+        case None => noBodyHandler()
+      }
+    }
+  }
+
+  private implicit class RichHttpMessage(val m: HttpMessage) {
+    def setHeadersFrom(response: ServerResponse[_]): Unit = {
+      response.headers
+        .groupBy(_.name)
+        .foreach { case (k, v) =>
+          m.headers().set(k, v.map(_.value).asJava)
+        }
+    }
+
+    def handleContentLengthAndChunkedHeaders(length: Option[Long]): Unit = {
+      val lengthKnownAndShouldBeSet = !m.headers().contains(HttpHeaderNames.CONTENT_LENGTH) && length.nonEmpty
+      val lengthUnknownAndChunkedShouldBeUsed = !m.headers().contains(HttpHeaderNames.CONTENT_LENGTH) && length.isEmpty
+
+      if (lengthKnownAndShouldBeSet) { length.map { l => m.headers().set(HttpHeaderNames.CONTENT_LENGTH, l) } }
+      if (lengthUnknownAndChunkedShouldBeUsed) { m.headers().add(HttpHeaderNames.TRANSFER_ENCODING, HttpHeaderValues.CHUNKED) }
+    }
+
+    def handleCloseAndKeepAliveHeaders(request: FullHttpRequest): Unit = {
+      if (!HttpUtil.isKeepAlive(request))
+        m.headers.set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE)
+      else if (request.protocolVersion.equals(HttpVersion.HTTP_1_0))
+        m.headers.set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE)
+    }
+  }
+
+  private implicit class RichChannelFuture(val cf: ChannelFuture) {
+    def closeIfNeeded(request: FullHttpRequest): Unit = {
+      if (!HttpUtil.isKeepAlive(request)) {
+        cf.addListener(ChannelFutureListener.CLOSE)
+      }
     }
   }
 }
