df0cd76008c3523ae6388e8a1689d9ebc8e6c46d
scala-steward <scala-steward>
2022-11-08 00:17:14 +0000

    Update armeria to 1.20.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -44,7 +44,7 @@ object Versions {
   val monixNewtype = "0.2.3"
   val zioPrelude = "1.0.0-RC16"
   val awsLambdaInterface = "2.1.1"
-  val armeria = "1.20.1"
+  val armeria = "1.20.2"
   val scalaJava8Compat = "1.0.2"
   val scalaCollectionCompat = "2.8.1"
   val fs2 = "3.3.0"
