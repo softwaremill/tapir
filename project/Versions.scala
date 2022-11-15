a46a5c4deda5c0214e3709a8f197bb71b0e486b3
scala-steward <scala-steward>
2022-11-15 00:15:59 +0000

    Revert commit(s) 6e8150b34

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val circeYaml = "0.14.1"
   val sttp = "3.8.3"
   val sttpModel = "1.5.2"
-  val sttpShared = "1.3.11"
+  val sttpShared = "1.3.10"
   val sttpApispec = "0.3.1"
   val akkaHttp = "10.2.10"
   val akkaStreams = "2.6.20"
