0006106769ea148a8fc45bae931880fdb296f288
frekw <fredrik@warnsberg.se>
2022-11-18 01:32:07 +0100

    scope start

--- server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpTestServerInterpreter.scala
+++ server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpTestServerInterpreter.scala
@@ -8,11 +8,12 @@ import sttp.tapir.server.tests.TestServerInterpreter
 import sttp.tapir.tests.Port
 import zio._
 import zio.http._
+import zio.http.netty.server.NettyDriver
 import zio.interop.catz._
 
 import java.net.InetSocketAddress
 
-class ZioHttpTestServerInterpreter()
+class ZioHttpTestServerInterpreter(implicit trace: Trace)
     extends TestServerInterpreter[Task, ZioStreams, ZioHttpServerOptions[Any], Http[Any, Throwable, Request, Response]] {
 
   override def route(es: List[ServerEndpoint[ZioStreams, Task]], interceptors: Interceptors): Http[Any, Throwable, Request, Response] = {
@@ -23,10 +24,12 @@ class ZioHttpTestServerInterpreter()
   override def server(routes: NonEmptyList[Http[Any, Throwable, Request, Response]]): Resource[IO, Port] = {
     implicit val r: Runtime[Any] = Runtime.default
 
-    val io: ZIO[Scope, Throwable, Nothing] =
-      Server
-        .serve(routes.toList.reduce(_ ++ _))
-        .provide(ServerConfig.live(ServerConfig(address = new InetSocketAddress(0))), Server.live)
+    val io: ZIO[Scope, Throwable, Int] =
+      (for {
+        driver <- ZIO.service[Driver]
+        _ <- driver.start(trace)
+        _ <- Server.serve(routes.toList.reduce(_ ++ _)).provide(ZLayer.succeed(driver) >>> Server.base).forkScoped
+      } yield 0).provide(Scope.default, ServerConfig.live(ServerConfig(address = new InetSocketAddress(0))), NettyDriver.default)
 
     Resource.scoped[IO, Any, Int](io)
   }
