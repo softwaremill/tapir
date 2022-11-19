0d69d4a115ff0e9a91c146906eb0e0673c7b0f3f
scala-steward <scala-steward>
2022-11-19 00:14:27 +0000

    Update vertx-web to 4.3.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -35,7 +35,7 @@ object Versions {
   val playClient = "2.1.10"
   val playServer = "2.8.18"
   val tethys = "0.26.0"
-  val vertx = "4.3.4"
+  val vertx = "4.3.5"
   val jsScalaJavaTime = "2.4.0"
   val nativeScalaJavaTime = "2.4.0-M3"
   val jwtScala = "5.0.0"
