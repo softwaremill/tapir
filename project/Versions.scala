1e7b10629374735706fc77f5b921c3274f23a1d0
scala-steward <scala-steward>
2022-11-20 00:13:43 +0000

    Update zio-interop-cats to 22.0.0.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -29,7 +29,7 @@ object Versions {
   val zio1Json = "0.2.0"
   val zio1InteropReactiveStreams = "1.3.12"
   val zio = "2.0.4"
-  val zioInteropCats = "3.3.0"
+  val zioInteropCats = "22.0.0.0"
   val zioInteropReactiveStreams = "2.0.0"
   val zioJson = "0.3.0"
   val playClient = "2.1.10"
