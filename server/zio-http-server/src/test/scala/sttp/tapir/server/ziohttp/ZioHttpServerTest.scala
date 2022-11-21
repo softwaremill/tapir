ba4e3abf8e6c15270964579286bc4c82d73b4d9a
frekw <fredrik@warnsberg.se>
2022-11-21 10:27:24 +0100

    wip reuse netty resources

--- server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpServerTest.scala
+++ server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpServerTest.scala
@@ -10,46 +10,67 @@ import sttp.tapir.server.tests._
 import sttp.tapir.tests.{Test, TestSuite}
 import sttp.tapir.ztapir.{RIOMonadError, RichZEndpoint}
 import zio.http.{Path, Request, URL}
+import zio.http.netty.{ChannelType, EventLoopGroups, ChannelFactories}
 import zio.interop.catz._
-import zio.{Runtime, Task, UIO, Unsafe, ZIO}
+import zio.{Runtime, Task, UIO, Unsafe, Scope, ZIO, ZEnvironment, ZLayer}
 
 class ZioHttpServerTest extends TestSuite {
 
   override def tests: Resource[IO, List[Test]] = backendResource.map { backend =>
     implicit val r: Runtime[Any] = Runtime.default
     // creating the netty dependencies once, to speed up tests
-    val interpreter = new ZioHttpTestServerInterpreter()
-    val createServerTest = new DefaultCreateServerTest(backend, interpreter)
+    Resource
+      .scoped[IO, Any, ZEnvironment[zio.http.service.EventLoopGroup with zio.http.service.ServerChannelFactory]]({
+        val config = ZLayer.succeed(new ChannelType.Config {
+          def channelType = ChannelType.AUTO
+        })
+        val eventLoop: ZLayer[Any, Nothing, io.netty.channel.EventLoopGroup] =
+          ZLayer.succeed(new io.netty.channel.DefaultEventLoopGroup())
+        config >>> ChannelFactories.Server.fromConfig ++ eventLoop
+      }.build)
+      .map { nettyDeps =>
+        val eventLoopGroup = nettyDeps.get[zio.http.service.EventLoopGroup]
+        val channelFactory = nettyDeps.get[zio.http.service.ServerChannelFactory]
+        val interpreter = new ZioHttpTestServerInterpreter(eventLoopGroup, channelFactory)
+        val createServerTest = new DefaultCreateServerTest(backend, interpreter)
 
-    def additionalTests(): List[Test] = List(
-      // https://github.com/softwaremill/tapir/issues/1914
-      Test("zio http route can be used as a function") {
-        val ep = endpoint.get.in("p1").out(stringBody).zServerLogic[Any](_ => ZIO.succeed("response"))
-        val route = ZioHttpInterpreter().toHttp(ep)
-        val test: UIO[Assertion] = route(Request.get(url = URL.apply(Path.empty / "p1")))
-          .flatMap(response => response.body.asString)
-          .map(_ shouldBe "response")
-          .catchAll(_ => ZIO.succeed(fail("Unable to extract body from Http response")))
-        Unsafe.unsafe(implicit u => r.unsafe.runToFuture(test))
+        def additionalTests(): List[Test] = List(
+          // https://github.com/softwaremill/tapir/issues/1914
+          Test("zio http route can be used as a function") {
+            val ep = endpoint.get.in("p1").out(stringBody).zServerLogic[Any](_ => ZIO.succeed("response"))
+            val route = ZioHttpInterpreter().toHttp(ep)
+            val test: UIO[Assertion] = route(Request.get(url = URL.apply(Path.empty / "p1")))
+              .flatMap(response => response.body.asString)
+              .map(_ shouldBe "response")
+              .catchAll(_ => ZIO.succeed(fail("Unable to extract body from Http response")))
+            Unsafe.unsafe(implicit u => r.unsafe.runToFuture(test))
+          }
+        )
+
+        implicit val m: MonadError[Task] = new RIOMonadError[Any]
+
+        new ServerBasicTests(
+          createServerTest,
+          interpreter,
+          multipleValueHeaderSupport = false,
+          supportsUrlEncodedPathSegments = false,
+          supportsMultipleSetCookieHeaders = false,
+          invulnerableToUnsanitizedHeaders = false
+        ).tests() ++
+          // TODO: re-enable static content once a newer zio http is available. Currently these tests often fail with:
+          // Cause: java.io.IOException: parsing HTTP/1.1 status line, receiving [f2 content], parser state [STATUS_LINE]
+          // new AllServerTests(
+          //   createServerTest,
+          //   interpreter,
+          //   backend,
+          //   basic = false,
+          //   staticContent = false,
+          //   multipart = false,
+          //   file = false
+          // ).tests() ++ new ServerStreamingTests(createServerTest, ZioStreams).tests()
+          // ++
+          // new ZioHttpCompositionTest(createServerTest).tests() ++
+          additionalTests()
       }
-    )
-
-    implicit val m: MonadError[Task] = new RIOMonadError[Any]
-
-    new ServerBasicTests(
-      createServerTest,
-      interpreter,
-      multipleValueHeaderSupport = false,
-      supportsUrlEncodedPathSegments = false,
-      supportsMultipleSetCookieHeaders = false,
-      invulnerableToUnsanitizedHeaders = false
-    ).tests() ++
-      // TODO: re-enable static content once a newer zio http is available. Currently these tests often fail with:
-      // Cause: java.io.IOException: parsing HTTP/1.1 status line, receiving [f2 content], parser state [STATUS_LINE]
-      new AllServerTests(createServerTest, interpreter, backend, basic = false, staticContent = false, multipart = false, file = false)
-        .tests() ++
-      new ServerStreamingTests(createServerTest, ZioStreams).tests() ++
-      new ZioHttpCompositionTest(createServerTest).tests() ++
-      additionalTests()
   }
 }
--- server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpTestServerInterpreter.scala
+++ server/zio-http-server/src/test/scala/sttp/tapir/server/ziohttp/ZioHttpTestServerInterpreter.scala
@@ -7,12 +7,14 @@ import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.tests.TestServerInterpreter
 import sttp.tapir.tests.Port
 import zio._
-import zio.http._
 import zio.http.netty.server.NettyDriver
+import zio.http.service.ServerChannelFactory
+import zio.http._
 import zio.interop.catz._
 
-class ZioHttpTestServerInterpreter(implicit trace: Trace)
-    extends TestServerInterpreter[Task, ZioStreams, ZioHttpServerOptions[Any], Http[Any, Throwable, Request, Response]] {
+class ZioHttpTestServerInterpreter(eventLoopGroup: zio.http.service.EventLoopGroup, channelFactory: zio.http.service.ServerChannelFactory)(
+    implicit trace: Trace
+) extends TestServerInterpreter[Task, ZioStreams, ZioHttpServerOptions[Any], Http[Any, Throwable, Request, Response]] {
 
   override def route(es: List[ServerEndpoint[ZioStreams, Task]], interceptors: Interceptors): Http[Any, Throwable, Request, Response] = {
     val serverOptions: ZioHttpServerOptions[Any] = interceptors(ZioHttpServerOptions.customiseInterceptors).options
@@ -22,16 +24,56 @@ class ZioHttpTestServerInterpreter(implicit trace: Trace)
   override def server(routes: NonEmptyList[Http[Any, Throwable, Request, Response]]): Resource[IO, Port] = {
     implicit val r: Runtime[Any] = Runtime.default
 
-    val io: ZIO[Scope, Throwable, Int] =
+    val makeNettyDriver = {
+      import zio.http.netty.server._
+      import io.netty.bootstrap.ServerBootstrap
+      import io.netty.channel._
+      import io.netty.util.ResourceLeakDetector
+      import zio._
+      import zio.http.netty._
+      import zio.http.service.ServerTime
+      import zio.http.{Driver, Http, HttpApp, Server, ServerConfig}
+
+      import java.net.InetSocketAddress
+      import java.util.concurrent.atomic.AtomicReference
+
+      type ErrorCallbackRef = AtomicReference[Option[Server.ErrorCallback]]
+      type AppRef = AtomicReference[(HttpApp[Any, Throwable], ZEnvironment[Any])]
+      type EnvRef = AtomicReference[ZEnvironment[Any]]
+
+      val app = ZLayer.succeed(
+        new AtomicReference[(HttpApp[Any, Throwable], ZEnvironment[Any])]((Http.empty, ZEnvironment.empty))
+      )
+      val ecb = ZLayer.succeed(new AtomicReference[Option[Server.ErrorCallback]](Option.empty))
+      val time = ZLayer.succeed(ServerTime.make(1000.millis))
+
+      // val serverChannelFactory = ChannelFactories.Server.fromConfig
+      // val eventLoopGroup = EventLoopGroups.fromConfig
+      val nettyRuntime = NettyRuntime.usingSharedThreadPool
+      val serverChannelInitializer = ServerChannelInitializer.layer
+      val serverInboundHandler = ServerInboundHandler.layer
+
+      val serverLayers = app ++
+        ZLayer.succeed(channelFactory) ++
+        (
+          (
+            (time ++ app ++ ecb) ++
+              (ZLayer.succeed(eventLoopGroup) >>> nettyRuntime) >>> serverInboundHandler
+          ) >>> serverChannelInitializer
+        ) ++
+        ecb ++
+        ZLayer.succeed(eventLoopGroup)
+
+      NettyDriver.make.provideSomeLayer[ServerConfig with Scope](serverLayers)
+    }
+
+    val effect: ZIO[Scope, Throwable, Int] =
       (for {
-        driver <- ZIO.service[Driver]
+        driver <- makeNettyDriver
         port <- driver.start(trace)
         _ <- driver.addApp[Any](routes.toList.reduce(_ ++ _), ZEnvironment())
-      } yield port).provideSome[Scope](
-        ServerConfig.live(ServerConfig.default.port(0)),
-        NettyDriver.default
-      )
+      } yield port).provideSome[Scope](ServerConfig.live(ServerConfig.default.port(0)))
 
-    Resource.scoped[IO, Any, Int](io)
+    Resource.scoped[IO, Any, Int](effect)
   }
 }
