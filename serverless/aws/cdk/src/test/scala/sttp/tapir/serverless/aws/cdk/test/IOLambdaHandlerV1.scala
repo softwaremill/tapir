a69efc120a97583678c24e8796cde985af646438
Grzegorz Hejduk <grzegorz.hejduk@softwaremill.com>
2022-12-08 08:50:42 +0100

    Fix awsCdk tests

--- serverless/aws/cdk/src/test/scala/sttp/tapir/serverless/aws/cdk/test/IOLambdaHandlerV1.scala
+++ serverless/aws/cdk/src/test/scala/sttp/tapir/serverless/aws/cdk/test/IOLambdaHandlerV1.scala
@@ -5,7 +5,7 @@ import cats.effect.unsafe.implicits.global
 import com.amazonaws.services.lambda.runtime.Context
 import io.circe.generic.auto._
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.serverless.aws.lambda.{AwsRequestV1, LambdaHandler, lambdaRequestMapperV1}
+import sttp.tapir.serverless.aws.lambda.{AwsRequestV1, LambdaHandler}
 
 import java.io.{InputStream, OutputStream}
 
