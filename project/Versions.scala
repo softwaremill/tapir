b38abc5beed6014620f729e37490b2517b6bd3a0
scala-steward <scala-steward>
2023-03-04 00:05:42 +0000

    Update circe-core, circe-generic, ... to 0.14.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val http4sBlazeServer = "0.23.13"
   val http4sBlazeClient = "0.23.13"
   val catsEffect = "3.4.8"
-  val circe = "0.14.3"
+  val circe = "0.14.5"
   val circeGenericExtras = "0.14.3"
   val circeYaml = "0.14.2"
   val sttp = "3.8.9"
