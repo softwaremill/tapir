755cab28c03f30ab6cd7fa59276c2326f6392870
scala-steward <scala-steward>
2023-04-18 00:15:07 +0000

    Update quicklens to 1.9.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -49,7 +49,7 @@ object Versions {
   val scalaCollectionCompat = "2.9.0"
   val fs2 = "3.6.1"
   val decline = "2.4.1"
-  val quicklens = "1.9.0"
+  val quicklens = "1.9.2"
   val openTelemetry = "1.24.0"
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.2.0"
