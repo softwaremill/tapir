6463e1cf9485f90d6bb1a175e2cd773f4e9dc290
kciesielski <kciesielski@users.noreply.github.com>
2023-04-25 18:05:02 +0200

    [aws-lambda] Fix ScalaJS compatibility

--- serverless/aws/lambda/src/main/scalajs/sttp/tapir/serverless/aws/lambda/AwsToResponseBody.scala
+++ serverless/aws/lambda/src/main/scalajs/sttp/tapir/serverless/aws/lambda/AwsToResponseBody.scala
@@ -24,10 +24,11 @@ private[lambda] class AwsToResponseBody[F[_]](options: AwsServerOptions[F]) exte
         val r = if (options.encodeResponseBody) Base64.getEncoder.encodeToString(bytes) else new String(bytes)
         (r, Some(bytes.length.toLong))
 
-      case RawBodyType.ByteBufferBody   => throw new UnsupportedOperationException
-      case RawBodyType.InputStreamBody  => throw new UnsupportedOperationException
-      case RawBodyType.FileBody         => throw new UnsupportedOperationException
-      case _: RawBodyType.MultipartBody => throw new UnsupportedOperationException
+      case RawBodyType.ByteBufferBody       => throw new UnsupportedOperationException
+      case RawBodyType.InputStreamBody      => throw new UnsupportedOperationException
+      case RawBodyType.InputStreamRangeBody => throw new UnsupportedOperationException
+      case RawBodyType.FileBody             => throw new UnsupportedOperationException
+      case _: RawBodyType.MultipartBody     => throw new UnsupportedOperationException
     }
 
   override def fromStreamValue(
