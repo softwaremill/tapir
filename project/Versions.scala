8c9b345c93000ea62c1f84797193a4c8ecf52ee4
scala-steward <scala-steward>
2023-04-29 00:06:07 +0000

    Update cats-effect to 3.4.10

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val http4sBlazeServer = "0.23.14"
   val http4sBlazeClient = "0.23.14"
   val catsCore = "2.9.0"
-  val catsEffect = "3.4.9"
+  val catsEffect = "3.4.10"
   val circe = "0.14.3"
   val circeGenericExtras = "0.14.3"
   val circeYaml = "0.14.2"
