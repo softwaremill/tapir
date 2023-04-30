e120fc0af59c25949d0c5f655f17b139e1ca46ed
scala-steward <scala-steward>
2023-04-30 00:14:35 +0000

    Revert commit(s) fec789d23

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val http4sBlazeServer = "0.23.14"
   val http4sBlazeClient = "0.23.14"
   val catsEffect = "3.4.9"
-  val circe = "0.14.5"
+  val circe = "0.14.3"
   val circeGenericExtras = "0.14.3"
   val circeYaml = "0.14.2"
   val sttp = "3.8.15"
