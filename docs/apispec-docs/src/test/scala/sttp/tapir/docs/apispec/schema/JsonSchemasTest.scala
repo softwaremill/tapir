589bfd572e69c3c04a92c843d9440055ddafb464
kciesielski <kciesielski@users.noreply.github.com>
2023-05-15 12:31:58 +0200

    Update tests

--- docs/apispec-docs/src/test/scala/sttp/tapir/docs/apispec/schema/JsonSchemasTest.scala
+++ docs/apispec-docs/src/test/scala/sttp/tapir/docs/apispec/schema/JsonSchemasTest.scala
@@ -23,10 +23,10 @@ class JsonSchemasTest extends AnyFlatSpec with Matchers with OptionValues with E
     val tSchema = implicitly[Schema[Parent]]
 
     // when
-    val result: ASchema = JsonSchemas(tSchema.copy(description = Some("desc!")), markOptionsAsNullable = true)
+    val result: ASchema = JsonSchemas(tSchema, markOptionsAsNullable = true)
 
     // then
-    println(Printer.noSpaces.print(result.asJson.deepDropNullValues))
+    println(Printer.spaces2.print(result.asJson.deepDropNullValues))
     result.asJson.deepDropNullValues shouldBe json"""{"$$schema":"https://json-schema.org/draft-04/schema#","required":["innerChildField"],"type":"object","properties":{"innerChildField":{"$$ref":"#/$$defs/Child"}},"description":"desc!","$$defs":{"Child":{"required":["childId"],"type":"object","properties":{"childId":{"type":"string"},"childNames":{"type":"array","items":{"type":"string"}}}}}}"""
 
   }
@@ -45,16 +45,17 @@ class JsonSchemasTest extends AnyFlatSpec with Matchers with OptionValues with E
   it should "handle repeated type names" in {
     // given
     object Childhood {
-      case class Child(age: Int, height: Int)
+      case class Child(age: Int, height: Option[Int])
     }
     case class Parent(innerChildField: Child, childDetails: Childhood.Child)
     case class Child(childName: String)
     val tSchema = implicitly[Schema[Parent]]
 
     // when
-    val result = JsonSchemas(tSchema, markOptionsAsNullable = false)
+    val result = JsonSchemas(tSchema, markOptionsAsNullable = true)
 
     // then
+    println(Printer.spaces2.print(result.asJson.deepDropNullValues))
     result.asJson.deepDropNullValues shouldBe json"""{"$$schema":"https://json-schema.org/draft-04/schema#","required":["innerChildField","childDetails"],"type":"object","properties":{"innerChildField":{"$$ref":"#/$$defs/Child"},"childDetails":{"$$ref":"#/$$defs/Child1"}},"$$defs":{"Child":{"required":["childName"],"type":"object","properties":{"childName":{"type":"string"}}},"Child1":{"required":["age","height"],"type":"object","properties":{"age":{"type":"integer","format":"int32"},"height":{"type":"integer","format":"int32"}}}}}"""
   }
 
