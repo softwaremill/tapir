d749535228e9da66538adc4c14fef543ee8bf06a
kciesielski <kciesielski@users.noreply.github.com>
2023-06-15 10:04:42 +0200

    wip

---
+++ json/upickle/src/main/scala-3/sttp/tapir/json/upickle/auto/package.scala
@@ -0,0 +1,55 @@
+package sttp.tapir.json.upickle
+
+import upickle.AttributeTagged
+import scala.deriving.Mirror
+import scala.reflect.ClassTag
+
+package object auto {
+
+  sealed trait AnimalKind
+  case class Mammal(mammalParam: Int) extends AnimalKind
+  case class Bird(birdParam: String) extends AnimalKind
+
+  case class DogAge(years: Int)
+  case class Dog(name: String, dogAge: DogAge)
+
+  object TapirPickle extends AttributeTagged {
+    def camelToSnake(s: String) = {
+      s.replaceAll("([A-Z])", "#$1").split('#').map(_.toLowerCase).mkString("_")
+    }
+    def snakeToCamel(s: String) = {
+      val res = s.split("_", -1).map(x => s"${x(0).toUpper}${x.drop(1)}").mkString
+      s"${s(0).toLower}${res.drop(1)}"
+    }
+
+    override def objectAttributeKeyReadMap(s: CharSequence): CharSequence =
+      snakeToCamel(s.toString)
+    override def objectAttributeKeyWriteMap(s: CharSequence): CharSequence =
+      camelToSnake(s.toString)
+
+    override def objectTypeKeyReadMap(s: CharSequence): CharSequence =
+      snakeToCamel(s.toString)
+    override def objectTypeKeyWriteMap(s: CharSequence): CharSequence =
+      camelToSnake(s.toString)
+
+  }
+
+  // given animalKindRw: TapirPickle.ReadWriter[AnimalKind] = TapirPickle.ReadWriter.derived[AnimalKind]
+  given dogAgeR: TapirPickle.ReadWriter[DogAge] = TapirPickle.ReadWriter.derived[DogAge]
+  given dogRw: TapirPickle.ReadWriter[Dog] = TapirPickle.ReadWriter.derived[Dog]
+ 
+  import magnolia1.*
+
+  object AutoPickleWriter extends AutoDerivation[TapirPickle.Writer]:
+    def join[T](ctx: CaseClass[TapirPickle.Writer, T]): TapirPickle.Writer[T] =
+      ctx.annotations
+      TapirPickle.macroW[T]
+
+  val autoIntCodec = implicitly[TapirPickle.ReadWriter[Int]]
+
+  def testMe =
+    val dogJsonStr = TapirPickle.write(Dog("Bob", DogAge(3)))
+    val intJsonStr = TapirPickle.write(3)
+
+    println(s"Hello, $dogJsonStr, $intJsonStr")
+}
---
+++ json/upickle/src/test/scala-3/sttp/tapir/json/upickle/AutoTest.scala
@@ -0,0 +1,13 @@
+package sttp.tapir.json.upickle
+
+import org.scalatest.Assertion
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.flatspec.AnyFlatSpecLike
+import auto._
+
+class AutoTest extends AnyFlatSpecLike with Matchers {
+
+  it should "work" in {
+    testMe
+  }
+}
