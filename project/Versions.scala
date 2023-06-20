9574d11c34f1123f2dd78a2971ecc7f3bc236537
softwaremill-ci <ci@softwaremill.pl>
2023-06-20 08:16:03 +0200

    Update scala-collection-compat to 2.11.0 (#2968)
    Co-authored-by: scala-steward <scala-steward>

--- project/Versions.scala
+++ project/Versions.scala
@@ -47,7 +47,7 @@ object Versions {
   val awsLambdaInterface = "2.3.2"
   val armeria = "1.23.1"
   val scalaJava8Compat = "1.0.2"
-  val scalaCollectionCompat = "2.10.0"
+  val scalaCollectionCompat = "2.11.0"
   val fs2 = "3.7.0"
   val decline = "2.4.1"
   val quicklens = "1.9.4"
