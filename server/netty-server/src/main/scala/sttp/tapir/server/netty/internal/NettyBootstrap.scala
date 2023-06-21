12ca0df76128335d46b976384a81cb6e99d4c48f
wydra98 <wydra.adrian98@gmail.com>
2023-06-21 08:40:02 +0200

    Extend netty configuration with timeouts

--- server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/NettyBootstrap.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/NettyBootstrap.scala
@@ -2,6 +2,7 @@ package sttp.tapir.server.netty.internal
 
 import io.netty.bootstrap.ServerBootstrap
 import io.netty.channel.{Channel, ChannelFuture, ChannelInitializer, ChannelOption, EventLoopGroup}
+import io.netty.handler.timeout.ReadTimeoutHandler
 import sttp.tapir.server.netty.NettyConfig
 
 import java.net.{InetSocketAddress, SocketAddress}
@@ -19,10 +20,13 @@ object NettyBootstrap {
       .group(eventLoopGroup)
       .channel(nettyConfig.eventLoopConfig.serverChannel)
       .childHandler(new ChannelInitializer[Channel] {
-        override def initChannel(ch: Channel): Unit = nettyConfig.initPipeline(nettyConfig)(ch.pipeline(), handler)
+        override def initChannel(ch: Channel): Unit = nettyConfig.initPipeline(nettyConfig)(ch.pipeline().addLast(new ReadTimeoutHandler(nettyConfig.requestTimeout)), handler)
       })
       .option[java.lang.Integer](ChannelOption.SO_BACKLOG, nettyConfig.socketBacklog) // https://github.com/netty/netty/issues/1692
       .childOption[java.lang.Boolean](ChannelOption.SO_KEEPALIVE, nettyConfig.socketKeepAlive) // https://github.com/netty/netty/issues/1692
+      .childOption[java.lang.Integer](ChannelOption.SO_TIMEOUT, nettyConfig.socketTimeout)
+      .childOption[java.lang.Integer](ChannelOption.SO_LINGER, nettyConfig.lingerTimeout)
+      .childOption[java.lang.Integer](ChannelOption.CONNECT_TIMEOUT_MILLIS, nettyConfig.connectionTimeout * 1000)
 
     httpBootstrap.bind(overrideSocketAddress.getOrElse(new InetSocketAddress(nettyConfig.host, nettyConfig.port)))
   }
