66a98ca37a972dfa5eb55e13ebb3db360c9b7949
Tigran Ohanyan <32593716+TigranOhanyan@users.noreply.github.com>
2023-06-26 11:04:43 +0400

    Feature/serverless zio (#2975)

---
+++ serverless/aws/lambda-cats-effect-tests/src/main/scala/sttp/tapir/serverless/aws/lambda/tests/LambdaSamTemplate.scala
@@ -0,0 +1,24 @@
+package sttp.tapir.serverless.aws.lambda.tests
+
+import sttp.tapir.serverless.aws.sam._
+
+import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.{Files, Paths}
+
+object LambdaSamTemplate extends App {
+
+  val jarPath =
+    Paths.get("serverless/aws/lambda-cats-effect-tests/target/jvm-2.13/tapir-aws-lambda-cats-effect-tests.jar").toAbsolutePath.toString
+
+  val samOptions: AwsSamOptions = AwsSamOptions(
+    "Tests",
+    source = CodeSource(
+      "java11",
+      jarPath,
+      "sttp.tapir.serverless.aws.lambda.tests.IOLambdaHandlerV2::handleRequest"
+    ),
+    memorySize = 1024
+  )
+  val yaml = AwsSamInterpreter(samOptions).toSamTemplate(allEndpoints.map(_.endpoint).toList).toYaml
+  Files.write(Paths.get("aws-lambda-cats-effect-template.yaml"), yaml.getBytes(UTF_8))
+}
---
+++ serverless/aws/lambda-core/src/main/scala/sttp/tapir/serverless/aws/lambda/AwsServerInterpreter.scala
@@ -0,0 +1,52 @@
+package sttp.tapir.serverless.aws.lambda
+
+import sttp.model.{HeaderNames, StatusCode}
+import sttp.monad.MonadError
+import sttp.monad.syntax._
+import sttp.tapir.capabilities.NoStreams
+import sttp.tapir.server.ServerEndpoint
+import sttp.tapir.server.interceptor.RequestResult
+import sttp.tapir.server.interceptor.reject.RejectInterceptor
+import sttp.tapir.server.interpreter.{BodyListener, FilterServerEndpoints, ServerInterpreter}
+
+private[aws] abstract class AwsServerInterpreter[F[_]: MonadError] {
+
+  def awsServerOptions: AwsServerOptions[F]
+
+  def toRoute(se: ServerEndpoint[Any, F]): Route[F] =
+    toRoute(List(se))
+
+  def toRoute(ses: List[ServerEndpoint[Any, F]]): Route[F] = {
+    implicit val bodyListener: BodyListener[F, LambdaResponseBody] = new AwsBodyListener[F]
+
+    val interpreter = new ServerInterpreter[Any, F, LambdaResponseBody, NoStreams](
+      FilterServerEndpoints(ses),
+      new AwsRequestBody[F](),
+      new AwsToResponseBody(awsServerOptions),
+      RejectInterceptor.disableWhenSingleEndpoint(awsServerOptions.interceptors, ses),
+      deleteFile = _ => ().unit // no file support
+    )
+
+    { (request: AwsRequest) =>
+      val serverRequest = AwsServerRequest(request)
+
+      interpreter.apply(serverRequest).map {
+        case RequestResult.Failure(_) =>
+          AwsResponse(isBase64Encoded = awsServerOptions.encodeResponseBody, StatusCode.NotFound.code, Map.empty, "")
+        case RequestResult.Response(res) =>
+          val baseHeaders = res.headers.groupBy(_.name).map { case (n, v) => n -> v.map(_.value).mkString(",") }
+          val allHeaders = res.body match {
+            case Some((_, Some(contentLength))) if res.contentLength.isEmpty =>
+              baseHeaders + (HeaderNames.ContentLength -> contentLength.toString)
+            case _ => baseHeaders
+          }
+          AwsResponse(
+            isBase64Encoded = awsServerOptions.encodeResponseBody,
+            res.code.code,
+            allHeaders,
+            res.body.map(_._1).getOrElse("")
+          )
+      }
+    }
+  }
+}
