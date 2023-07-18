7dcdc6db47ca47fea302c7aba2fe0b0de7732bb1
kciesielski <kciesielski@users.noreply.github.com>
2023-07-18 15:35:03 +0200

    Always expect content-length in streaming response if set by user

--- server/tests/src/main/scala/sttp/tapir/server/tests/ServerStreamingTests.scala
+++ server/tests/src/main/scala/sttp/tapir/server/tests/ServerStreamingTests.scala
@@ -4,8 +4,9 @@ import cats.syntax.all._
 import org.scalatest.matchers.should.Matchers._
 import sttp.capabilities.Streams
 import sttp.client3._
-import sttp.model.{Header, MediaType}
+import sttp.model.{Header, HeaderNames, MediaType}
 import sttp.monad.MonadError
+import sttp.tapir.tests.Test
 import sttp.tapir.tests.Streaming.{
   in_stream_out_either_json_xml_stream,
   in_stream_out_stream,
@@ -13,13 +14,8 @@ import sttp.tapir.tests.Streaming.{
   in_string_stream_out_either_stream_string,
   out_custom_content_type_stream_body
 }
-import sttp.tapir.tests.Test
 
-class ServerStreamingTests[F[_], S, OPTIONS, ROUTE](
-    createServerTest: CreateServerTest[F, S, OPTIONS, ROUTE],
-    streams: Streams[S],
-    streamingWithContentLengthSupport: Boolean = true
-)(implicit
+class ServerStreamingTests[F[_], S, OPTIONS, ROUTE](createServerTest: CreateServerTest[F, S, OPTIONS, ROUTE], streams: Streams[S])(implicit
     m: MonadError[F]
 ) {
 
@@ -27,89 +23,81 @@ class ServerStreamingTests[F[_], S, OPTIONS, ROUTE](
     import createServerTest._
 
     val penPineapple = "pen pineapple apple pen"
-    if (streamingWithContentLengthSupport)
-      List(
-        testServer(
-          in_stream_out_stream_with_content_length(streams)
-        )((in: (Long, streams.BinaryStream)) => pureResult(in.asRight[Unit])) { (backend, baseUri) =>
-          {
-            basicRequest
-              .post(uri"$baseUri/api/echo")
-              .contentLength(penPineapple.length.toLong)
-              .body(penPineapple)
-              .send(backend)
-              .map { response =>
-                response.body shouldBe Right(penPineapple)
-                if (streamingWithContentLengthSupport) {
-                  response.contentLength shouldBe Some(penPineapple.length)
-                } else {
-                  response.contentLength shouldBe None
-                }
-              }
-          }
+
+    List(
+      testServer(in_stream_out_stream(streams))((s: streams.BinaryStream) => pureResult(s.asRight[Unit])) { (backend, baseUri) =>
+        basicRequest.post(uri"$baseUri/api/echo").body(penPineapple).send(backend).map(_.body shouldBe Right(penPineapple))
+      },
+      testServer(
+        in_stream_out_stream_with_content_length(streams)
+      )((in: (Long, streams.BinaryStream)) => pureResult(in.asRight[Unit])) { (backend, baseUri) =>
+        {
+          basicRequest
+            .post(uri"$baseUri/api/echo")
+            .contentLength(penPineapple.length.toLong)
+            .body(penPineapple)
+            .send(backend)
+            .map { response =>
+              response.body shouldBe Right(penPineapple)
+              response.contentLength shouldBe Some(penPineapple.length)
+            }
         }
-      )
-    else
-      Nil ++
-        List(
-          testServer(in_stream_out_stream(streams))((s: streams.BinaryStream) => pureResult(s.asRight[Unit])) { (backend, baseUri) =>
-            basicRequest.post(uri"$baseUri/api/echo").body(penPineapple).send(backend).map(_.body shouldBe Right(penPineapple))
-          },
-          testServer(out_custom_content_type_stream_body(streams)) { case (k, s) =>
-            pureResult((if (k < 0) (MediaType.ApplicationJson.toString(), s) else (MediaType.ApplicationXml.toString(), s)).asRight[Unit])
-          } { (backend, baseUri) =>
-            basicRequest
-              .post(uri"$baseUri?kind=-1")
-              .body(penPineapple)
-              .send(backend)
-              .map { r =>
-                r.body shouldBe Right(penPineapple)
-                r.contentType shouldBe Some(MediaType.ApplicationJson.toString())
-              } >>
-              basicRequest
-                .post(uri"$baseUri?kind=1")
-                .body(penPineapple)
-                .send(backend)
-                .map { r =>
-                  r.body shouldBe Right(penPineapple)
-                  r.contentType shouldBe Some(MediaType.ApplicationXml.toString())
-                }
-          },
-          testServer(in_string_stream_out_either_stream_string(streams)) {
-            case ("left", s) => pureResult((Left(s): Either[streams.BinaryStream, String]).asRight[Unit])
-            case _           => pureResult((Right("was not left"): Either[streams.BinaryStream, String]).asRight[Unit])
-          } { (backend, baseUri) =>
-            basicRequest
-              .post(uri"$baseUri?which=left")
-              .body(penPineapple)
-              .send(backend)
-              .map(_.body shouldBe Right(penPineapple)) >>
-              basicRequest
-                .post(uri"$baseUri?which=right")
-                .body(penPineapple)
-                .send(backend)
-                .map(_.body shouldBe Right("was not left"))
-          },
-          testServer(in_stream_out_either_json_xml_stream(streams)) { s => pureResult(s.asRight[Unit]) } { (backend, baseUri) =>
-            basicRequest
-              .post(uri"$baseUri")
-              .body(penPineapple)
-              .header(Header.accept(MediaType.ApplicationXml, MediaType.ApplicationJson))
-              .send(backend)
-              .map { r =>
-                r.contentType shouldBe Some(MediaType.ApplicationXml.toString())
-                r.body shouldBe Right(penPineapple)
-              } >>
-              basicRequest
-                .post(uri"$baseUri")
-                .body(penPineapple)
-                .header(Header.accept(MediaType.ApplicationJson, MediaType.ApplicationXml))
-                .send(backend)
-                .map { r =>
-                  r.contentType shouldBe Some(MediaType.ApplicationJson.toString())
-                  r.body shouldBe Right(penPineapple)
-                }
-          }
-        )
+      },
+      testServer(out_custom_content_type_stream_body(streams)) { case (k, s) =>
+        pureResult((if (k < 0) (MediaType.ApplicationJson.toString(), s) else (MediaType.ApplicationXml.toString(), s)).asRight[Unit])
+      } { (backend, baseUri) =>
+        basicRequest
+          .post(uri"$baseUri?kind=-1")
+          .body(penPineapple)
+          .send(backend)
+          .map { r =>
+            r.body shouldBe Right(penPineapple)
+            r.contentType shouldBe Some(MediaType.ApplicationJson.toString())
+          } >>
+          basicRequest
+            .post(uri"$baseUri?kind=1")
+            .body(penPineapple)
+            .send(backend)
+            .map { r =>
+              r.body shouldBe Right(penPineapple)
+              r.contentType shouldBe Some(MediaType.ApplicationXml.toString())
+            }
+      },
+      testServer(in_string_stream_out_either_stream_string(streams)) {
+        case ("left", s) => pureResult((Left(s): Either[streams.BinaryStream, String]).asRight[Unit])
+        case _           => pureResult((Right("was not left"): Either[streams.BinaryStream, String]).asRight[Unit])
+      } { (backend, baseUri) =>
+        basicRequest
+          .post(uri"$baseUri?which=left")
+          .body(penPineapple)
+          .send(backend)
+          .map(_.body shouldBe Right(penPineapple)) >>
+          basicRequest
+            .post(uri"$baseUri?which=right")
+            .body(penPineapple)
+            .send(backend)
+            .map(_.body shouldBe Right("was not left"))
+      },
+      testServer(in_stream_out_either_json_xml_stream(streams)) { s => pureResult(s.asRight[Unit]) } { (backend, baseUri) =>
+        basicRequest
+          .post(uri"$baseUri")
+          .body(penPineapple)
+          .header(Header.accept(MediaType.ApplicationXml, MediaType.ApplicationJson))
+          .send(backend)
+          .map { r =>
+            r.contentType shouldBe Some(MediaType.ApplicationXml.toString())
+            r.body shouldBe Right(penPineapple)
+          } >>
+          basicRequest
+            .post(uri"$baseUri")
+            .body(penPineapple)
+            .header(Header.accept(MediaType.ApplicationJson, MediaType.ApplicationXml))
+            .send(backend)
+            .map { r =>
+              r.contentType shouldBe Some(MediaType.ApplicationJson.toString())
+              r.body shouldBe Right(penPineapple)
+            }
+      }
+    )
   }
 }
