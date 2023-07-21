2cdd666c235763609cbefe7f9ae17b2057b54c3d
wydra98 <wydra.adrian98@gmail.com>
2023-07-21 08:48:45 +0200

    Add logs

--- server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/NettyConnectionCounter.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/NettyConnectionCounter.scala
@@ -2,15 +2,21 @@ package sttp.tapir.server.netty.internal
 
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import io.netty.util.internal.logging.InternalLoggerFactory
 
 import java.util.concurrent.atomic.AtomicInteger
 
 @Sharable case class NettyConnectionCounter(maxConnections: Int) extends ChannelInboundHandlerAdapter {
   private val connections = new AtomicInteger()
+  private lazy val logger = InternalLoggerFactory.getInstance(getClass)
 
   override def channelActive(ctx: ChannelHandlerContext): Unit = {
     val counter = connections.incrementAndGet
-    if (counter <= maxConnections) super.channelActive(ctx) else ctx.close
+    if (counter <= maxConnections) super.channelActive(ctx)
+    else {
+      logger.warn(s"Max connections exceeded: $maxConnections")
+      ctx.close
+    }
   }
 
   override def channelInactive(ctx: ChannelHandlerContext): Unit = {
