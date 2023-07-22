e83623b8183ddd6187843dfcd06425a9337ab551
scala-steward <scala-steward>
2023-07-22 00:15:17 +0000

    Update enumeratum to 1.7.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -25,7 +25,7 @@ object Versions {
   val scalaTest = "3.2.16"
   val scalaTestPlusScalaCheck = "3.2.16.0"
   val refined = "0.11.0"
-  val enumeratum = "1.7.2"
+  val enumeratum = "1.7.3"
   val zio1 = "1.0.18"
   val zio1InteropCats = "13.0.0.2"
   val zio1Json = "0.2.0"
