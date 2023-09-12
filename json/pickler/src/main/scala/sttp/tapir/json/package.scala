cc4be0d16edd6485e66fa17ad59b0b905ad23524
kciesielski <kciesielski@users.noreply.github.com>
2023-09-12 21:05:35 +0200

    Add API for jsonBody

---
+++ json/pickler/src/main/scala/sttp/tapir/json/package.scala
@@ -0,0 +1,12 @@
+package sttp.tapir.json
+
+import sttp.tapir._
+
+def jsonBody[T: Pickler]: EndpointIO.Body[String, T] = stringBodyUtf8AnyFormat(summon[Pickler[T]].toCodec)
+
+def jsonBodyWithRaw[T: Pickler]: EndpointIO.Body[String, (String, T)] = stringBodyUtf8AnyFormat(
+  Codec.tupledWithRaw(summon[Pickler[T]].toCodec)
+)
+
+def jsonQuery[T: Pickler](name: String): EndpointInput.Query[T] =
+  queryAnyFormat[T, CodecFormat.Json](name, Codec.jsonQuery(summon[Pickler[T]].toCodec))
