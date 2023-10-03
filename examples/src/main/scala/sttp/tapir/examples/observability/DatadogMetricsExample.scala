0fa398835eb2b156ec2b98442761210c07300e81
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2023-10-03 14:53:19 +0200

    Use Netty-based server in Datadog metrics example

--- examples/src/main/scala/sttp/tapir/examples/observability/DatadogMetricsExample.scala
+++ examples/src/main/scala/sttp/tapir/examples/observability/DatadogMetricsExample.scala
@@ -1,8 +1,5 @@
 package sttp.tapir.examples.observability
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Route
 import com.timgroup.statsd.NonBlockingStatsDClientBuilder
 import com.typesafe.scalalogging.StrictLogging
 import io.circe.generic.auto._
@@ -10,15 +7,15 @@ import sttp.tapir._
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.server.akkahttp.{AkkaHttpServerInterpreter, AkkaHttpServerOptions}
 import sttp.tapir.server.metrics.datadog.DatadogMetrics
+import sttp.tapir.server.netty.{NettyFutureServer, NettyFutureServerOptions}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
+import scala.io.StdIn
 
 object DatadogMetricsExample extends App with StrictLogging {
-  implicit val actorSystem: ActorSystem = ActorSystem()
-  import actorSystem.dispatcher
 
   case class Person(name: String)
 
@@ -46,17 +43,22 @@ object DatadogMetricsExample extends App with StrictLogging {
 
   val datadogMetrics = DatadogMetrics.default[Future](client)
 
-  val serverOptions: AkkaHttpServerOptions =
-    AkkaHttpServerOptions.customiseInterceptors
+  val serverOptions: NettyFutureServerOptions =
+    NettyFutureServerOptions.customiseInterceptors
       // Adds an interceptor which collects metrics by executing callbacks
       .metricsInterceptor(datadogMetrics.metricsInterceptor())
       .options
 
-  val routes: Route = AkkaHttpServerInterpreter(serverOptions).toRoute(personEndpoint)
+  val program = for {
+    binding <- NettyFutureServer().port(8080).addEndpoint(personEndpoint, serverOptions).start()
+    _ <- Future {
+      logger.info(s"""Server started. Try it with: curl -X POST localhost:8080/person -d '{"name": "Jacob"}'""")
+      logger.info(s"The metrics are sent to udp://$hostname:$port")
+      logger.info("Press ENTER key to exit.")
+      StdIn.readLine()
+    }
+    stop <- binding.stop()
+  } yield stop
 
-  Await.result(Http().newServerAt("localhost", 8080).bindFlow(routes), 1.minute)
-
-  logger.info(
-    s"Server started. POST persons under http://localhost:8080/person. The metrics sends to udp://$hostname:$port"
-  )
+  Await.result(program, Duration.Inf)
 }
