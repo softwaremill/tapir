f7cecc352275f2b8735bcfbfb5c9e161642c0352
scala-steward <scala-steward>
2023-11-23 00:11:37 +0000

    Update jwt-circe to 9.4.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -44,7 +44,7 @@ object Versions {
   val vertx = "4.5.0"
   val jsScalaJavaTime = "2.5.0"
   val nativeScalaJavaTime = "2.4.0-M3"
-  val jwtScala = "9.4.4"
+  val jwtScala = "9.4.5"
   val derevo = "0.13.0"
   val newtype = "0.4.4"
   val monixNewtype = "0.2.3"
