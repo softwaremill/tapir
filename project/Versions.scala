e1f5d8d25c43711c73a466122e6a6d1f8ad1c382
softwaremill-ci <ci@softwaremill.pl>
2024-01-03 01:45:35 +0100

    Update http4s-blaze-client to 0.23.16 (#3437)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val http4s = "0.23.24"
   val http4sBlazeServer = "0.23.16"
-  val http4sBlazeClient = "0.23.15"
+  val http4sBlazeClient = "0.23.16"
   val catsCore = "2.10.0"
   val catsEffect = "3.5.2"
   val circe = "0.14.6"
