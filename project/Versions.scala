bfcef645c3753e9285bbe67f140c6b8fd4c434f6
scala-steward <scala-steward>
2024-01-17 00:11:31 +0000

    Update client3:akka-http-backend, ... to 3.9.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val circeGenericExtras = "0.14.3"
   val circeYaml = "0.15.1"
   val helidon = "4.0.0"
-  val sttp = "3.9.1"
+  val sttp = "3.9.2"
   val sttpModel = "1.7.6"
   val sttpShared = "1.3.17"
   val sttpApispec = "0.7.3"
