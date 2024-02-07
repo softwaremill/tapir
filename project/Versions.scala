6ad46b1249c3693f2bf2fe120cf1808496ac2ac9
softwaremill-ci <ci@softwaremill.pl>
2024-02-07 01:54:14 +0100

    Update zio-prelude to 1.0.0-RC23 (#3503)

--- project/Versions.scala
+++ project/Versions.scala
@@ -49,7 +49,7 @@ object Versions {
   val derevo = "0.13.0"
   val newtype = "0.4.4"
   val monixNewtype = "0.2.3"
-  val zioPrelude = "1.0.0-RC22"
+  val zioPrelude = "1.0.0-RC23"
   val awsLambdaInterface = "2.4.1"
   val armeria = "1.27.0"
   val scalaJava8Compat = "1.0.2"
