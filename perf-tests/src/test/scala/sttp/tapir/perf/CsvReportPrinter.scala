d409d8e5fadffaf178e42efc46a6103184d8e5ed
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2024-02-07 13:36:36 +0100

    Improve CSV reports for perf tests (#3505)

--- perf-tests/src/test/scala/sttp/tapir/perf/CsvReportPrinter.scala
+++ perf-tests/src/test/scala/sttp/tapir/perf/CsvReportPrinter.scala
@@ -1,20 +1,21 @@
 package sttp.tapir.perf
 
 object CsvReportPrinter {
-  def print(results: List[GatlingSimulationResult]): String = {
+  def print(results: List[GatlingSimulationResult], initialSimOrdering: List[String]): String = {
 
     val groupedResults = results.groupBy(_.simulationName)
-    val headers = "Simulation" :: groupedResults.values.head.flatMap(r => {
+    val orderedResults = initialSimOrdering.flatMap(simName => groupedResults.get(simName).map(simName -> _))
+    val headers = "Simulation" :: orderedResults.head._2.flatMap(r => {
       val server = r.serverName
       List(
-        s"$server-ops/sec",
-        s"$server-latency-p99",
-        s"$server-latency-p95",
-        s"$server-latency-p75",
-        s"$server-latency-p50"
+        s"$server reqs/s",
+        s"$server latency-p99",
+        s"$server latency-p95",
+        s"$server latency-p75",
+        s"$server latency-p50"
       )
     })
-    val rows: List[String] = groupedResults.toList.map { case (simName, serverResults) =>
+    val rows: List[String] = orderedResults.map { case (simName, serverResults) =>
       (simName :: serverResults.flatMap(singleResult =>
         List(
           singleResult.meanReqsPerSec.toString,
