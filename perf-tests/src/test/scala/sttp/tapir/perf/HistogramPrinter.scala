20240f12dc80ce6d074096a81fa9c3bce4a669a6
kciesielski <kciesielski@users.noreply.github.com>
2024-02-26 10:30:50 +0100

    Nicer timestamp in the histogram filename

--- perf-tests/src/test/scala/sttp/tapir/perf/HistogramPrinter.scala
+++ perf-tests/src/test/scala/sttp/tapir/perf/HistogramPrinter.scala
@@ -5,11 +5,16 @@ import org.HdrHistogram.Histogram
 import java.io.{FileOutputStream, PrintStream}
 import java.nio.file.Paths
 import scala.util.{Failure, Success, Using}
+import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
 
 object HistogramPrinter {
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss")
+
   def saveToFile(histogram: Histogram, histogramName: String): Unit = {
+    val currentTime = LocalDateTime.now().format(formatter)
     val baseDir = System.getProperty("user.dir")
-    val targetFilePath = Paths.get(baseDir).resolve(s"$histogramName-${System.currentTimeMillis()}")
+    val targetFilePath = Paths.get(baseDir).resolve(s"$histogramName-$currentTime")
     Using.Manager { use =>
       val fos = use(new FileOutputStream(targetFilePath.toFile))
       val ps = use(new PrintStream(fos))
