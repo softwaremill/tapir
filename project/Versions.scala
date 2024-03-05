db2519962f0766951e0d36f65805e1037d1fc72f
softwaremill-ci <ci@softwaremill.pl>
2024-03-05 01:41:04 +0100

    Update logback-classic to 1.5.3 (#3567)

--- project/Versions.scala
+++ project/Versions.scala
@@ -61,6 +61,6 @@ object Versions {
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.3.0"
   val nettyAll = "4.1.107.Final"
-  val logback = "1.5.2"
+  val logback = "1.5.3"
   val slf4j = "2.0.12"
 }
