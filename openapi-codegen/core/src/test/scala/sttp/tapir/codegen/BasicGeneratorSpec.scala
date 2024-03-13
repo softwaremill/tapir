c9d8063658bc8f549a0410d541ed6db7aa46b8c2
Hugh Simpson <hsimpson@rzsoftware.com>
2024-03-13 08:17:05 +0000

    add test

--- openapi-codegen/core/src/test/scala/sttp/tapir/codegen/BasicGeneratorSpec.scala
+++ openapi-codegen/core/src/test/scala/sttp/tapir/codegen/BasicGeneratorSpec.scala
@@ -14,4 +14,25 @@ class BasicGeneratorSpec extends CompileCheckTestBase {
     )("TapirGeneratedEndpoints") shouldCompile ()
   }
 
+  it should "split outputs by tag if useHeadTagForObjectNames = true" in {
+    val generated = BasicGenerator.generateObjects(
+      TestHelpers.myBookshopDoc,
+      "sttp.tapir.generated",
+      "TapirGeneratedEndpoints",
+      targetScala3 = false,
+      useHeadTagForObjectNames = true
+    )
+    val schemas = generated("TapirGeneratedEndpoints")
+    val endpoints = generated("Bookshop")
+    // schema file on its own should compile
+    schemas shouldCompile ()
+    // schema file should contain no endpoint definitions
+    schemas.linesIterator.count(_.matches("""^\s*endpoint""")) shouldEqual 0
+    // Bookshop file should contain all endpoint definitions
+    endpoints.linesIterator.count(_.matches("""^\s*endpoint""")) shouldEqual 3
+    // endpoint file depends on schema file. For simplicity of testing, just strip the package declaration from the
+    // endpoint file, and concat the two, before testing for compilation
+    (schemas + "\n" + (endpoints.linesIterator.filterNot(_ startsWith "package").mkString("\n"))) shouldCompile ()
+  }
+
 }
