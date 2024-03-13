17af1369bb89a7f68f9a0125cce41b1d105c90f3
Roman Janusz <r.janusz.rgz@gmail.com>
2024-03-13 13:41:48 +0100

    Automatic propagation of schema name as schema title (#3593)

--- docs/apispec-docs/src/main/scala/sttp/tapir/docs/apispec/schema/TapirSchemaToJsonSchema.scala
+++ docs/apispec-docs/src/main/scala/sttp/tapir/docs/apispec/schema/TapirSchemaToJsonSchema.scala
@@ -9,7 +9,6 @@ object TapirSchemaToJsonSchema {
   def apply(
       schema: TSchema[_],
       markOptionsAsNullable: Boolean,
-      addTitleToDefs: Boolean = true,
       metaSchema: MetaSchema = MetaSchemaDraft04,
       schemaName: TSchema.SName => String = defaultSchemaName
   ): ASchema = {
@@ -19,22 +18,25 @@ object TapirSchemaToJsonSchema {
 
     val keysToIds = calculateUniqueIds(keyedSchemas.map(_._1), (key: SchemaKey) => schemaName(key.name))
     val toSchemaReference = new ToSchemaReference(keysToIds, keyedSchemas.toMap, refRoot = "#/$defs/")
-    val tschemaToASchema = new TSchemaToASchema(toSchemaReference, markOptionsAsNullable)
+    val tschemaToASchema = new TSchemaToASchema(schemaName, toSchemaReference, markOptionsAsNullable)
     val keysToSchemas = keyedSchemas.map(td => (td._1, tschemaToASchema(td._2, allowReference = false))).toListMap
     val schemaIds = keysToSchemas.map { case (k, v) => k -> ((keysToIds(k), v)) }
 
-    val nestedKeyedSchemas = schemaIds.values
+    val defsList = schemaIds.values.toListMap
     val rootApiSpecSchemaOrRef: ASchema = tschemaToASchema(schema, allowReference = false)
-
-    val defsList: ListMap[SchemaId, ASchema] =
-      nestedKeyedSchemas.collect {
-        case (k, nestedSchema: ASchema) if nestedSchema.$ref.isEmpty =>
-          (k, nestedSchema.copy(title = nestedSchema.title.orElse(if (addTitleToDefs) Some(k) else None)))
-      }.toListMap
 
     rootApiSpecSchemaOrRef.copy(
       `$schema` = Some(metaSchema.schemaId),
       `$defs` = if (defsList.nonEmpty) Some(defsList) else None
     )
   }
+
+  // binary compatibility shim
+  private[docs] def apply(
+      schema: TSchema[_],
+      markOptionsAsNullable: Boolean,
+      addTitleToDefs: Boolean,
+      metaSchema: MetaSchema,
+      schemaName: TSchema.SName => String
+  ): ASchema = apply(schema, markOptionsAsNullable, metaSchema, schemaName)
 }
