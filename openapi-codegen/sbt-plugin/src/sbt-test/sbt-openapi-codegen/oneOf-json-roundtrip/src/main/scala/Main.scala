2359a461ab35e5abd75e76b224b418838ff3d064
hughsimpson <hsimpson@rzsoftware.com>
2024-04-09 17:13:13 +0100

    codegen: Remove dead comments (#3647)

--- openapi-codegen/sbt-plugin/src/sbt-test/sbt-openapi-codegen/oneOf-json-roundtrip/src/main/scala/Main.scala
+++ openapi-codegen/sbt-plugin/src/sbt-test/sbt-openapi-codegen/oneOf-json-roundtrip/src/main/scala/Main.scala
@@ -1,23 +1,4 @@
 object Main extends App {
-  /*
-  import sttp.tapir._
-  import sttp.tapir.json.circe._
-  import io.circe.generic.auto._
-
-  type Limit = Int
-  type AuthToken = String
-  case class BooksFromYear(genre: String, year: Int)
-  case class Book(title: String)
-
-  val booksListing: Endpoint[(BooksFromYear, Limit, AuthToken), String, List[Book], Any] =
-    endpoint
-      .get
-      .in(("books" / path[String]("genre") / path[Int]("year")).mapTo[BooksFromYear])
-      .in(query[Limit]("limit").description("Maximum number of books to retrieve"))
-      .in(header[AuthToken]("X-Auth-Token"))
-      .errorOut(stringBody)
-      .out(jsonBody[List[Book]])
-   */
   import sttp.apispec.openapi.circe.yaml._
   import sttp.tapir.generated._
   import sttp.tapir.docs.openapi._
