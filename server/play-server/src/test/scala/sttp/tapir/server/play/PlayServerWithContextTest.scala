003add443609dee7e103d0bd2f11419eac97337b
GreyPlane <709327148@qq.com>
2024-04-10 21:00:19 +0800

    +tests

--- server/play-server/src/test/scala/sttp/tapir/server/play/PlayServerWithContextTest.scala
+++ server/play-server/src/test/scala/sttp/tapir/server/play/PlayServerWithContextTest.scala
@@ -3,17 +3,22 @@ package sttp.tapir.server.play
 import org.apache.pekko.actor.ActorSystem
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import org.apache.pekko.stream.scaladsl.Flow
 import org.scalatest.matchers.should.Matchers._
 import play.api.Mode
 import play.api.routing.Router
 import play.core.server.{DefaultPekkoHttpServerComponents, ServerConfig}
+import sttp.capabilities.WebSockets
+import sttp.capabilities.fs2.Fs2Streams
+import sttp.capabilities.pekko.PekkoStreams
 import sttp.client3._
 import sttp.tapir._
 import sttp.tapir.tests.Test
+import sttp.ws.{WebSocket, WebSocketFrame}
 
 import scala.concurrent.Future
 
-class PlayServerWithContextTest(backend: SttpBackend[IO, Any])(implicit _actorSystem: ActorSystem) {
+class PlayServerWithContextTest(backend: SttpBackend[IO, Fs2Streams[IO] with WebSockets])(implicit _actorSystem: ActorSystem) {
   import _actorSystem.dispatcher
 
   def tests(): List[Test] = List(
@@ -32,6 +37,34 @@ class PlayServerWithContextTest(backend: SttpBackend[IO, Any])(implicit _actorSy
           .map(_.body shouldBe Right("world"))
           .unsafeToFuture()
       }
+      r.onComplete(_ => s.stop())
+      r
+    },
+    Test("websocket with prefixed play route") {
+      val e = endpoint.get
+        .in("hello")
+        .out(webSocketBody[String, CodecFormat.TextPlain, String, CodecFormat.TextPlain](PekkoStreams))
+        .serverLogicSuccess[Future](_ => Future.successful(Flow[String].map(_ => "world")))
+
+      val components = new DefaultPekkoHttpServerComponents {
+        override lazy val serverConfig: ServerConfig = ServerConfig(port = Some(0), address = "127.0.0.1", mode = Mode.Test)
+        override lazy val actorSystem: ActorSystem = ActorSystem("tapir", defaultExecutionContext = Some(_actorSystem.dispatcher))
+        override def router: Router = Router.from(PlayServerInterpreter().toRoutes(e)).withPrefix("/test")
+      }
+      val s = components.server
+      val r = basicRequest
+        .response(asWebSocket { (ws: WebSocket[IO]) =>
+          for {
+            _ <- ws.sendText("test1")
+            m1 <- ws.receiveText()
+            _ <- ws.close()
+            m3 <- ws.eitherClose(ws.receiveText())
+          } yield List(m1, m3)
+        })
+        .get(uri"ws://localhost:${s.mainAddress.getPort}/test/hello")
+        .send(backend)
+        .map(_.body shouldBe Right(List("world", Left(WebSocketFrame.Close(1000, "normal closure")))))
+        .unsafeToFuture()
       r.onComplete(_ => s.stop())
       r
     }
