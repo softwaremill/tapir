9d93ce78ca7474563922a888839a78a617812420
kciesielski <kciesielski@users.noreply.github.com>
2024-04-16 09:26:12 +0200

    Remove debug printlns

--- server/netty-server/loom/src/main/scala/sttp/tapir/server/netty/loom/internal/reactivestreams/OxProcessor.scala
+++ server/netty-server/loom/src/main/scala/sttp/tapir/server/netty/loom/internal/reactivestreams/OxProcessor.scala
@@ -22,7 +22,6 @@ private[loom] class OxProcessor[A, B](a: ActorRef[OxDispatcher], wsPipe: OxStrea
     if (a == null) {
       throw new NullPointerException("Element cannot be null") // Rule 2.13
     } else {
-      // println("onNext called, putting received element in the channel")
       channel.sendSafe(a) match {
         case () => ()
         case _: ChannelClosed => // Rule 2.13
@@ -37,17 +36,14 @@ private[loom] class OxProcessor[A, B](a: ActorRef[OxDispatcher], wsPipe: OxStrea
     } else if (subscription != null) {
       s.cancel() // Rule 2.5: if onSubscribe is called twice, must cancel the second subscription
     } else {
-      // println(s"onSubscribe called in the processor, requesting $demand elements")
       subscription = s
       s.request(1)
     }
 
   override def onComplete(): Unit =
-    println("onComplete!")
     val _ = channel.doneSafe()
 
   def done(): Unit =
-    println("done!")
     val _ = channel.doneSafe()
     if (subscription != null)
       try {
@@ -60,24 +56,18 @@ private[loom] class OxProcessor[A, B](a: ActorRef[OxDispatcher], wsPipe: OxStrea
       }
 
   override def subscribe(subscriber: Subscriber[? >: B]): Unit =
-    // println("Processor.subscribe called")
     if (subscriber == null) throw new NullPointerException("Subscriber cannot be null")
     val internalSource = (channel: Source[A])
-    // println("Internal source is now running, shoving it into the wrapper pipe")
     // Each time a request is read from the channel where we put it in onNext, we can ask for one more
     val transformation: Ox ?=> Source[A] => Source[A] = getAndRequest[A]
     val incomingRequests: Source[A] = a.ask(_.transformSource[A, A](transformation, internalSource))
     // Applying the user-provided pipe
     val outgoingResponses: Source[B] = a.ask(_.transformSource[A, B](wsPipe, incomingRequests))
-    // println("Source prepared")
     val subscription = new ChannelSubscription(a, subscriber, outgoingResponses, () => { val _ = channel.doneSafe() })
-    // println("Subscription prepared, subscribing")
     subscriber.onSubscribe(subscription)
-    // println("Publisher is now subscribed")
 
   def getAndRequest[B]: Ox ?=> Source[B] => Source[B] = { s =>
     s.map { e =>
-      // println(s"Element flowing through source: $e")
       subscription.request(1)
       e
     }
