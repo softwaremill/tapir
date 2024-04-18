29b347e967dcc092d6e1829b541183dcfb4e277b
kciesielski <kciesielski@users.noreply.github.com>
2024-04-18 14:13:21 +0200

    Uncomment the last test to confirm it's our culprit

--- server/tests/src/main/scala/sttp/tapir/server/tests/ServerWebSocketTests.scala
+++ server/tests/src/main/scala/sttp/tapir/server/tests/ServerWebSocketTests.scala
@@ -173,30 +173,30 @@ abstract class ServerWebSocketTests[F[_], S <: Streams[S], OPTIONS, ROUTE](
           )
         )
     },
-    // testServer(
-    //   endpoint.out(
-    //     webSocketBody[String, CodecFormat.TextPlain, String, CodecFormat.TextPlain](streams)
-    //       .autoPing(None)
-    //       .autoPongOnPing(false)
-    //   ),
-    //   "not pong on ping if disabled"
-    // )((_: Unit) => pureResult(stringEcho.asRight[Unit])) { (backend, baseUri) =>
-    //   basicRequest
-    //     .response(asWebSocket { (ws: WebSocket[IO]) =>
-    //       for {
-    //         _ <- ws.sendText("test1")
-    //         _ <- ws.send(WebSocketFrame.Ping("test-ping-text".getBytes()))
-    //         m1 <- ws.receiveText()
-    //         _ <- ws.sendText("test2")
-    //         m2 <- ws.receiveText()
-    //       } yield List(m1, m2)
-    //     })
-    //     .get(baseUri.scheme("ws"))
-    //     .send(backend)
-    //     .map(
-    //       _.body shouldBe Right(List("echo: test1", "echo: test2"))
-    //     )
-    // },
+    testServer(
+      endpoint.out(
+        webSocketBody[String, CodecFormat.TextPlain, String, CodecFormat.TextPlain](streams)
+          .autoPing(None)
+          .autoPongOnPing(false)
+      ),
+      "not pong on ping if disabled"
+    )((_: Unit) => pureResult(stringEcho.asRight[Unit])) { (backend, baseUri) =>
+      basicRequest
+        .response(asWebSocket { (ws: WebSocket[IO]) =>
+          for {
+            _ <- ws.sendText("test1")
+            _ <- ws.send(WebSocketFrame.Ping("test-ping-text".getBytes()))
+            m1 <- ws.receiveText()
+            _ <- ws.sendText("test2")
+            m2 <- ws.receiveText()
+          } yield List(m1, m2)
+        })
+        .get(baseUri.scheme("ws"))
+        .send(backend)
+        .map(
+          _.body shouldBe Right(List("echo: test1", "echo: test2"))
+        )
+    },
     testServer(
       endpoint.out(webSocketBody[String, CodecFormat.TextPlain, String, CodecFormat.TextPlain](streams)),
       "empty client stream"
