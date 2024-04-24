350969628cb94f277befdc702f6459be9755e39b
kciesielski <kciesielski@users.noreply.github.com>
2024-04-24 16:24:08 +0200

    Update scalacheck

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,7 +28,7 @@ object Versions {
   val ox = "0.0.26"
   val reactiveStreams = "1.0.4"
   val sprayJson = "1.3.6"
-  val scalaCheck = "1.17.1"
+  val scalaCheck = "1.18.0"
   val scalaTest = "3.2.18"
   val scalaTags = "0.13.1"
   val scalaTestPlusScalaCheck = "3.2.18.0"
