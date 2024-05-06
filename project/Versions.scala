367b97e85354f081ca408504c0aca0be5c0864c6
softwaremill-ci <ci@softwaremill.pl>
2024-05-06 02:36:04 +0200

    Update play-ahc-ws-standalone to 2.2.7 (#3742)

--- project/Versions.scala
+++ project/Versions.scala
@@ -42,7 +42,7 @@ object Versions {
   val zioJson = "0.6.2"
   val playClient = "3.0.2"
   val playServer = "3.0.2"
-  val play29Client = "2.2.6"
+  val play29Client = "2.2.7"
   val play29Server = "2.9.2"
   val tethys = "0.26.0"
   val vertx = "4.5.7"
