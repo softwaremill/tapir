22f4abb8936f7e99ce493abb539a63e9d53cc564
scala-steward <scala-steward>
2024-05-19 00:06:05 +0000

    Update upickle to 3.3.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,7 @@ object Versions {
   val pekkoHttp = "1.0.1"
   val pekkoStreams = "1.0.2"
   val swaggerUi = "5.17.2"
-  val upickle = "3.3.0"
+  val upickle = "3.3.1"
   val playJson = "3.0.1"
   val play29Json = "3.0.3"
   val finatra = "24.2.0"
