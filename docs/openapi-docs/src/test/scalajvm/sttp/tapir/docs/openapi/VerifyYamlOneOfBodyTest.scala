e2f61dfa8f9019881502ae3a2af4c4cc3d42f9cf
Michał Ossowski <623488+micossow@users.noreply.github.com>
2024-06-05 09:58:34 +0200

    Fix hidden schema visible in OpenAPI generated yml (#3820)

--- docs/openapi-docs/src/test/scalajvm/sttp/tapir/docs/openapi/VerifyYamlOneOfBodyTest.scala
+++ docs/openapi-docs/src/test/scalajvm/sttp/tapir/docs/openapi/VerifyYamlOneOfBodyTest.scala
@@ -6,6 +6,7 @@ import sttp.apispec.openapi.Info
 import sttp.apispec.openapi.circe.yaml._
 import sttp.tapir.tests.OneOfBody.{
   in_one_of_json_text_range_out_string,
+  in_one_of_json_xml_hidden_out_string,
   in_one_of_json_xml_text_out_string,
   in_string_out_one_of_json_xml_text
 }
@@ -37,6 +38,17 @@ class VerifyYamlOneOfBodyTest extends AnyFunSuite with Matchers {
     val expectedYaml = load("oneOfBody/expected_out_json_xml_text.yml")
 
     val e = in_string_out_one_of_json_xml_text
+
+    val actualYaml = OpenAPIDocsInterpreter().toOpenAPI(e, Info("test", "1.0")).toYaml
+    val actualYamlNoIndent = noIndentation(actualYaml)
+
+    actualYamlNoIndent shouldBe expectedYaml
+  }
+
+  test("should respect hidden body") {
+    val expectedYaml = load("oneOfBody/expected_in_json_xml_hidden.yml")
+
+    val e = in_one_of_json_xml_hidden_out_string
 
     val actualYaml = OpenAPIDocsInterpreter().toOpenAPI(e, Info("test", "1.0")).toYaml
     val actualYamlNoIndent = noIndentation(actualYaml)
