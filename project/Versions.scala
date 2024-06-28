e0a73d48e76fdc44dd1dc1ec5ee07ab98966f9f8
scala-steward <scala-steward>
2024-06-28 00:06:22 +0000

    Update play-ahc-ws-standalone to 3.0.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -40,7 +40,7 @@ object Versions {
   val zioInteropCats = "23.0.0.8"
   val zioInteropReactiveStreams = "2.0.2"
   val zioJson = "0.7.0"
-  val playClient = "3.0.3"
+  val playClient = "3.0.4"
   val playServer = "3.0.4"
   val play29Client = "2.2.7"
   val play29Server = "2.9.4"
