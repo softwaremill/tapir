753add358c6fb1e1efde1dab3dada5096c976229
scala-steward <scala-steward>
2024-06-28 00:05:59 +0000

    Update tethys-core, tethys-derivation, ... to 0.28.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -44,7 +44,7 @@ object Versions {
   val playServer = "3.0.4"
   val play29Client = "2.2.7"
   val play29Server = "2.9.4"
-  val tethys = "0.28.3"
+  val tethys = "0.28.4"
   val vertx = "4.5.8"
   val jsScalaJavaTime = "2.6.0"
   val nativeScalaJavaTime = "2.4.0-M3"
