fd7064a5e51ce0f31a3e7b4f06daaf94a101c066
scala-steward <scala-steward>
2024-07-16 00:15:12 +0000

    Update asyncapi-circe-yaml, ... to 0.11.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   val sttp = "3.9.7"
   val sttpModel = "1.7.11"
   val sttpShared = "1.3.19"
-  val sttpApispec = "0.10.0"
+  val sttpApispec = "0.11.0"
   val akkaHttp = "10.2.10"
   val akkaStreams = "2.6.20"
   val pekkoHttp = "1.0.1"
