b0f7640327ed67e42b185910f7b5194ee9d21556
softwaremill-ci <ci@softwaremill.pl>
2024-07-19 14:33:22 +0200

    Update vertx-codegen, vertx-web to 4.5.9 (#3937)

--- project/Versions.scala
+++ project/Versions.scala
@@ -45,7 +45,7 @@ object Versions {
   val play29Client = "2.2.9"
   val play29Server = "2.9.4"
   val tethys = "0.28.4"
-  val vertx = "4.5.8"
+  val vertx = "4.5.9"
   val jsScalaJavaTime = "2.6.0"
   val nativeScalaJavaTime = "2.6.0"
   val jwtScala = "10.0.1"
