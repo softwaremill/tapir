5c3c60db78c12542a9e8ea7d239a25473a04140a
softwaremill-ci <ci@softwaremill.pl>
2024-07-25 10:08:17 +0200

    Update sttp-mock-server, ... to 1.10.15 (#3950)

--- examples/src/main/scala/sttp/tapir/examples/HelloWorldZioHttpServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/HelloWorldZioHttpServer.scala
@@ -1,8 +1,8 @@
 // {cat=Hello, World!; effects=ZIO; server=ZIO HTTP; json=ZIO JSON}: Exposing an endpoint using the ZIO HTTP server
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.10.14
-//> using dep com.softwaremill.sttp.tapir::tapir-json-zio:1.10.14
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.10.14
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.10.15
+//> using dep com.softwaremill.sttp.tapir::tapir-json-zio:1.10.15
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.10.15
 
 package sttp.tapir.examples
 
--- examples/src/main/scala/sttp/tapir/examples/helloWorldNettyFutureServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/helloWorldNettyFutureServer.scala
@@ -1,7 +1,7 @@
 // {cat=Hello, World!; effects=Future; server=Netty}: Exposing an endpoint using the Netty server (Future variant)
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.10.14
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server:1.10.14
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.10.15
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server:1.10.15
 //> using dep com.softwaremill.sttp.client3::core:3.9.7
 
 package sttp.tapir.examples
