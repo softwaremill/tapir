d08a3243512e0050933bebbb1661f9a709fb1849
softwaremill-ci <ci@softwaremill.pl>
2024-08-08 02:44:38 +0200

    Update sttp-mock-server, ... to 1.11.1 (#3979)

--- examples/src/main/scala/sttp/tapir/examples/custom_types/commaSeparatedQueryParameter.scala
+++ examples/src/main/scala/sttp/tapir/examples/custom_types/commaSeparatedQueryParameter.scala
@@ -1,8 +1,8 @@
 // {cat=Custom types; effects=Direct; server=Netty; docs=Swagger UI}: Handling comma-separated query parameters
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.0
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.0
-//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.0
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.1
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.1
+//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.1
 
 package sttp.tapir.examples.custom_types
 
