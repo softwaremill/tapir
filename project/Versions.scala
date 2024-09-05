f39ad924442def2981ce724f86a7a09996a231ab
softwaremill-ci <ci@softwaremill.pl>
2024-09-05 02:41:52 +0200

    Update shared:akka, shared:armeria, ... to 1.3.21 (#4014)

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val helidon = "4.0.10"
   val sttp = "3.9.8"
   val sttpModel = "1.7.11"
-  val sttpShared = "1.3.20"
+  val sttpShared = "1.3.21"
   val sttpApispec = "0.11.3"
   val akkaHttp = "10.2.10"
   val akkaStreams = "2.6.20"
