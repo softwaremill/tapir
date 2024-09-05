732071fc4d124fd926bcbf2d829e29641bcf6ed7
softwaremill-ci <ci@softwaremill.pl>
2024-09-05 02:42:04 +0200

    Update sttp-mock-server, ... to 1.11.2 (#4011)

--- examples/src/main/scala/sttp/tapir/examples/HelloWorldNettyCatsServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/HelloWorldNettyCatsServer.scala
@@ -1,7 +1,7 @@
 // {cat=Hello, World!; effects=cats-effect; server=Netty}: Exposing an endpoint using the Netty server (cats-effect variant)
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.1
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-cats:1.11.1
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.2
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-cats:1.11.2
 //> using dep com.softwaremill.sttp.client3::core:3.9.7
 
 package sttp.tapir.examples
