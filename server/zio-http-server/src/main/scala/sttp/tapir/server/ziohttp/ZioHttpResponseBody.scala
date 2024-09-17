9a3b92aa606aecf0111c65379d643ed2c9d05614
Christian Kleinbölting <seakayone@users.noreply.github.com>
2024-09-17 11:51:14 +0200

    feat: Add zio-http multipart body support (#3690)
    Co-authored-by: Adam Warski <adam@warski.org>

--- server/zio-http-server/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpResponseBody.scala
+++ server/zio-http-server/src/main/scala/sttp/tapir/server/ziohttp/ZioHttpResponseBody.scala
@@ -2,6 +2,7 @@ package sttp.tapir.server.ziohttp
 
 import zio.stream.ZStream
 import zio.Chunk
+import zio.http.FormField
 
 sealed trait ZioHttpResponseBody {
   def contentLength: Option[Long]
@@ -10,3 +11,7 @@ sealed trait ZioHttpResponseBody {
 case class ZioStreamHttpResponseBody(stream: ZStream[Any, Throwable, Byte], contentLength: Option[Long]) extends ZioHttpResponseBody
 
 case class ZioRawHttpResponseBody(bytes: Chunk[Byte], contentLength: Option[Long]) extends ZioHttpResponseBody
+
+case class ZioMultipartHttpResponseBody(formFields: List[FormField]) extends ZioHttpResponseBody {
+  override def contentLength: Option[Long] = None
+}
