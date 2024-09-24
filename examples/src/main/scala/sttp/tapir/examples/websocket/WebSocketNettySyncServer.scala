b1963be3e2cc47c474a017bc2c6011bf41e3a14a
scala-steward <scala-steward>
2024-09-24 00:16:10 +0000

    Update sttp-mock-server, ... to 1.11.5

--- examples/src/main/scala/sttp/tapir/examples/websocket/WebSocketNettySyncServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/websocket/WebSocketNettySyncServer.scala
@@ -1,7 +1,7 @@
 // {cat=WebSocket; effects=Direct; server=Netty}: Describe and implement a WebSocket endpoint
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.4
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.4
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.5
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.5
 
 package sttp.tapir.examples.websocket
 
