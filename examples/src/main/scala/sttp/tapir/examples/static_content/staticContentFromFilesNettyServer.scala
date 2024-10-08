f6792f4b98fbb6ba5337842f9cb6ac23c31b53e7
scala-steward <scala-steward>
2024-10-08 13:14:47 +0000

    Update sttp-mock-server, ... to 1.11.6

--- examples/src/main/scala/sttp/tapir/examples/static_content/staticContentFromFilesNettyServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/static_content/staticContentFromFilesNettyServer.scala
@@ -1,8 +1,8 @@
 // {cat=Static content; effects=Direct; server=Netty}: Serving static files from a directory
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.5
-//> using dep com.softwaremill.sttp.tapir::tapir-files:1.11.5
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.5
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.6
+//> using dep com.softwaremill.sttp.tapir::tapir-files:1.11.6
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.6
 
 package sttp.tapir.examples.static_content
 
