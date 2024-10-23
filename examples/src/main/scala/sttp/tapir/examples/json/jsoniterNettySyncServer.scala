4731c9a3e99111dd130e99baee7d40ea6185dc80
softwaremill-ci <ci@softwaremill.pl>
2024-10-23 02:40:26 +0200

    Update jsoniter-scala-macros to 2.30.15 (#4113)

--- examples/src/main/scala/sttp/tapir/examples/json/jsoniterNettySyncServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/json/jsoniterNettySyncServer.scala
@@ -4,7 +4,7 @@
 //> using dep com.softwaremill.sttp.tapir::tapir-jsoniter-scala:1.11.7
 //> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.7
 //> using dep ch.qos.logback:logback-classic:1.5.8
-//> using dep com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-macros:2.30.11
+//> using dep com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-macros:2.30.15
 
 package sttp.tapir.examples.json
 
