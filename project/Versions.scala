95ecd845a12eb33f3e054ad720613e4eefda69aa
scala-steward <scala-steward>
2024-11-06 00:15:36 +0000

    Update swagger-ui to 5.18.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,7 @@ object Versions {
   val akkaStreams = "2.6.20"
   val pekkoHttp = "1.1.0"
   val pekkoStreams = "1.1.2"
-  val swaggerUi = "5.17.14"
+  val swaggerUi = "5.18.0"
   val upickle = "3.3.1"
   val playJson = "3.0.1"
   val play29Json = "3.0.4"
