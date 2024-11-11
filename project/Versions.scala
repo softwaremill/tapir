7070584773f0cb77e62f15f6d606ce66d39aa946
scala-steward <scala-steward>
2024-11-11 00:16:44 +0000

    Update opentelemetry-api, ... to 1.44.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -60,7 +60,7 @@ object Versions {
   val fs2 = "3.11.0"
   val decline = "2.4.1"
   val quicklens = "1.9.9"
-  val openTelemetry = "1.44.0"
+  val openTelemetry = "1.44.1"
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.4.3"
   val nettyAll = "4.1.114.Final"
