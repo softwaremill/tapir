8c905dff92651441beb39d168b190c4d7f4e3708
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2024-12-24 13:20:17 +0100

    fix

--- serverless/aws/lambda-cats-effect-tests/src/main/scala/sttp/tapir/serverless/aws/lambda/tests/IOLambdaHandlerV2.scala
+++ serverless/aws/lambda-cats-effect-tests/src/main/scala/sttp/tapir/serverless/aws/lambda/tests/IOLambdaHandlerV2.scala
@@ -5,7 +5,7 @@ import cats.effect.unsafe.implicits.global
 import com.amazonaws.services.lambda.runtime.Context
 import io.circe.generic.auto._
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.serverless.aws.lambda.{AwsRequest, LambdaHandler}
+import sttp.tapir.serverless.aws.lambda.{AwsRequest, AwsServerOptions, LambdaHandler}
 import java.io.{InputStream, OutputStream}
 
 class IOLambdaHandlerV2(options: AwsServerOptions[IO]) extends LambdaHandler[IO, AwsRequest](options) {
