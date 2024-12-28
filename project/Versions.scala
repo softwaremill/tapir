21f10f93a7912f62fd41e845529a8c07e9e607a8
scala-steward <scala-steward>
2024-12-28 00:15:11 +0000

    Update zio-prelude to 1.0.0-RC36

--- project/Versions.scala
+++ project/Versions.scala
@@ -52,7 +52,7 @@ object Versions {
   val derevo = "0.13.0"
   val newtype = "0.4.4"
   val monixNewtype = "0.3.0"
-  val zioPrelude = "1.0.0-RC35"
+  val zioPrelude = "1.0.0-RC36"
   val awsLambdaInterface = "2.6.0"
   val armeria = "1.31.3"
   val scalaJava8Compat = "1.0.2"
