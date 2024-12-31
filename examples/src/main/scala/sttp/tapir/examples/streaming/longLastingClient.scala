7069199aedf392f0f793a4d425043ad6549f37fe
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2024-12-31 16:32:54 +0100

    Revert "wip"
    This reverts commit 02ba4b01925ef4a60064330838de4f0c414d2a88.

--- examples/src/main/scala/sttp/tapir/examples/streaming/longLastingClient.scala
+++ examples/src/main/scala/sttp/tapir/examples/streaming/longLastingClient.scala
@@ -1,10 +1,8 @@
 //> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.11
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-cats:1.11.11
 //> using dep org.apache.pekko::pekko-stream:1.1.2
 //> using dep org.typelevel::cats-effect:3.5.7
-//> using dep com.softwaremill.sttp.client3::core:3.10.2
+//> using dep com.softwaremill.sttp.client3::core:3.10.1
 //> using dep com.softwaremill.sttp.client3::pekko-http-backend:3.10.1
-//> using dep com.softwaremill.sttp.client3::fs2:3.10.2
 
 package sttp.tapir.examples.streaming
 
@@ -23,31 +21,27 @@ import pekko.stream.scaladsl.{Flow, Source}
 import pekko.util.ByteString
 import cats.effect.*
 import cats.syntax.all.*
-import sttp.client3.httpclient.fs2.HttpClientFs2Backend
+
 import scala.concurrent.duration.*
 import scala.concurrent.duration.FiniteDuration
-import sttp.capabilities.fs2.Fs2Streams
-import fs2.{Chunk, Stream}
 
 object longLastingClient extends IOApp:
-  private def makeRequest(backend: SttpBackend[IO, Fs2Streams[IO] & WebSockets]): IO[Response[Either[String, String]]] =
-    def createStream(chunkSize: Int, beforeSendingSecondChunk: FiniteDuration): Stream[IO, Byte] = {
-      val chunk = Chunk.array(Array.fill(chunkSize)('A'.toByte))
-      val initialChunks = Stream.chunk(chunk)
-      val delayedChunk = Stream.sleep[IO](beforeSendingSecondChunk) >> Stream.chunk(chunk)
-      initialChunks ++ delayedChunk
+  implicit val actorSystem: ActorSystem = ActorSystem("longLastingClient")
+
+  private def makeRequest(backend: SttpBackend[Future, PekkoStreams & WebSockets]): Future[Response[Either[String, String]]] =
+    val stream: Source[ByteString, Any] = Source.tick(1.seconds, 1.seconds, ByteString(Array.fill(10)('A').map(_.toByte))).map { elem =>
+      println(s"$elem ${java.time.LocalTime.now()}"); elem
     }
-    val stream = createStream(100, 2.seconds)
-  
+
     basicRequest
       .post(uri"http://localhost:9000/chunks")
       .header(Header(HeaderNames.ContentLength, "10000"))
-      .streamBody(Fs2Streams[IO])(stream)
+      .streamBody(PekkoStreams)(stream)
       .send(backend)
-  
+
   override def run(args: List[String]): IO[ExitCode] =
-    HttpClientFs2Backend.resource[IO]().use { backend =>
-      makeRequest(backend).flatMap { response =>
+      val backend = PekkoHttpBackend.usingActorSystem(actorSystem)
+      val responseIO: IO[Response[Either[String, String]]] = IO.fromFuture(IO(makeRequest(backend)))
+      responseIO.flatMap { response =>
         IO(println(response.body))
-      }
-    }.as(ExitCode.Success)
+      }.as(ExitCode.Success)
