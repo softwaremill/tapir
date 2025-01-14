d4ec1cf218965b9428a28a814fe27320a454e15d
Ivan Klass <ivan.klass@performi.ca>
2025-01-14 18:53:39 +0100

    Cleanup code for server resources

--- server/tests/src/main/scala/sttp/tapir/server/tests/TestServerInterpreter.scala
+++ server/tests/src/main/scala/sttp/tapir/server/tests/TestServerInterpreter.scala
@@ -1,7 +1,7 @@
 package sttp.tapir.server.tests
 
 import cats.data.NonEmptyList
-import cats.effect.{IO, Resource}
+import cats.effect.{Deferred, IO, Resource}
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.interceptor.CustomiseInterceptors
 import sttp.tapir.tests._
@@ -21,8 +21,12 @@ trait TestServerInterpreter[F[_], +R, OPTIONS, ROUTE] {
   def serverWithStop(
       routes: NonEmptyList[ROUTE],
       gracefulShutdownTimeout: Option[FiniteDuration] = None
-  ): Resource[IO, (Port, KillSwitch)] =
-    Resource.eval(server(routes, gracefulShutdownTimeout).allocated)
+  ): Resource[IO, (Port, KillSwitch)] = for {
+    stopSignal <- Resource.eval(Deferred[IO, Unit])
+    portValue <- Resource.eval(Deferred[IO, Port])
+    _ <- server(routes, gracefulShutdownTimeout).evalTap(portValue.complete).surround(stopSignal.get).background
+    port <- Resource.eval(portValue.get)
+  } yield (port, stopSignal.complete(()).void)
 
   def server(routes: NonEmptyList[ROUTE], gracefulShutdownTimeout: Option[FiniteDuration] = None): Resource[IO, Port]
 }
