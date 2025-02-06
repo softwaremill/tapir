501b339b08f11e5c051c1112c2ca8dba030e70d1
Michal Stopyra <michal.stopyra@softwaremill.com>
2025-02-06 16:27:42 +0100

    Removes test checking propagation of headers for web sockets * Passing custom headers together with web socket responses is not supported for HttpClientFs2Backend, hence the test was removed

--- client/sttp-client4/src/test/scalajvm/sttp/tapir/client/sttp4/SttpClientWebSocketTests.scala
+++ client/sttp-client4/src/test/scalajvm/sttp/tapir/client/sttp4/SttpClientWebSocketTests.scala
@@ -4,9 +4,6 @@ import cats.effect.IO
 import fs2._
 import sttp.capabilities.WebSockets
 import sttp.capabilities.fs2.Fs2Streams
-import sttp.client4._
-import sttp.client4.httpclient.fs2.HttpClientFs2Backend
-import sttp.tapir._
 import sttp.tapir.client.tests.ClientWebSocketTests
 import sttp.tapir.client.sttp4.ws.fs2._
 import sttp.tapir.client.sttp4.WebSocketToPipe
@@ -21,41 +18,4 @@ class SttpClientWebSocketTests extends SttpClientTests[WebSockets with Fs2Stream
 
   webSocketTests()
 
-  test("web sockets, string echo, custom header") {
-    // HttpClient doesn't expose web socket response headers
-    HttpClientFs2Backend
-      .resource[IO]()
-      .use { httpClientBackend =>
-        def sendAsResponse[A, I, E, O](
-            e: Endpoint[A, I, E, O, WebSockets with Fs2Streams[IO]],
-            port: Port,
-            securityArgs: A,
-            args: I,
-            scheme: String
-        ): IO[Response[Either[E, O]]] = {
-          val genReq = SttpClientInterpreter()
-            .toSecureRequestThrowDecodeFailures(e, Some(uri"$scheme://localhost:$port"))
-            .apply(securityArgs)
-            .apply(args)
-
-          GenericRequestExtensions.sendRequest(httpClientBackend, genReq)
-        }
-
-        sendAsResponse(
-          endpoint.get
-            .in("ws" / "echo" / "header")
-            .out(webSocketBody[String, CodecFormat.TextPlain, String, CodecFormat.TextPlain].apply(streams)),
-          port,
-          (),
-          (),
-          "ws"
-        )
-          .flatMap { r =>
-            r.header("Correlation-id") shouldBe Some("ABC-DEF-123")
-            sendAndReceiveLimited(r.body.toOption.get, 2, List("test1", "test2"))
-          }
-          .map(_ shouldBe List("echo: test1", "echo: test2"))
-      }
-      .unsafeToFuture()
-  }
 }
