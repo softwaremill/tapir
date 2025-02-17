682e486c92027f65c02a29bf00a6411c1d43f09c
LUC DUZAN <strokyl@users.noreply.github.com>
2025-02-17 19:35:08 +0100

    Add support for zio prelude CustomNewType/CustomSubType (#4324)

--- integrations/zio-prelude/src/test/scala-3/sttp/tapir/codec/zio/prelude/newtype/TapirNewtypeSupportTestFixture.scala
+++ integrations/zio-prelude/src/test/scala-3/sttp/tapir/codec/zio/prelude/newtype/TapirNewtypeSupportTestFixture.scala
@@ -1,7 +1,7 @@
 package sttp.tapir.codec.zio.prelude.newtype
 
 import zio.prelude.Assertion.{divisibleBy, endsWith, greaterThan, startsWith}
-import zio.prelude.{Newtype, Subtype}
+import zio.prelude.{Newtype, NewtypeCustom, Subtype}
 
 object TapirNewtypeSupportTestFixture {
   object StringNewtypeWithMixin extends Newtype[String] with TapirNewtypeSupport[String] {
@@ -13,4 +13,10 @@ object TapirNewtypeSupportTestFixture {
     override inline def assertion = divisibleBy(2) && greaterThan(5)
   }
   type IntSubtypeWithMixin = IntSubtypeWithMixin.Type
+
+  object PalindromeWithMixin extends NewtypeCustom[String] with TapirNewtypeSupport[String] {
+    protected def validate(value: String) = PalindromeValidator.validate(value)
+    protected inline def validateInline(inline value: String) = ${ PalindromeValidator.validateInlineImpl('value) }
+  }
+  type PalindromeWithMixin = PalindromeWithMixin.Type
 }
