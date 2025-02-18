533308951b3252133daa2dc4b11fe5a4e1a3a6d0
softwaremill-ci <ci@softwaremill.pl>
2025-02-18 01:40:03 +0100

    Update sttp-mock-server, ... to 1.11.15 (#4348)

--- examples/src/main/scala/sttp/tapir/examples/observability/ZioMetricsExample.scala
+++ examples/src/main/scala/sttp/tapir/examples/observability/ZioMetricsExample.scala
@@ -1,8 +1,8 @@
 // {cat=Observability; effects=ZIO; server=ZIO HTTP}: Reporting Prometheus metrics
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.14
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.11.14
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-metrics:1.11.14
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.15
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.11.15
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-metrics:1.11.15
 
 package sttp.tapir.examples.observability
 
