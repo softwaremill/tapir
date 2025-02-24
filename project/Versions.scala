acba2893a583f87232a36b25db229a367a3eb5b1
scala-steward <scala-steward>
2025-02-24 00:16:05 +0000

    Update play-ahc-ws-standalone to 2.2.11

--- project/Versions.scala
+++ project/Versions.scala
@@ -43,7 +43,7 @@ object Versions {
   val zioJson = "0.7.28"
   val playClient = "3.0.6"
   val playServer = "3.0.6"
-  val play29Client = "2.2.10"
+  val play29Client = "2.2.11"
   val play29Server = "2.9.6"
   val tethys = "0.29.3"
   val vertx = "4.5.13"
