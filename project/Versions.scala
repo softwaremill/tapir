a3296f0adabd3c445d255245675b32b412aefc77
scala-steward <scala-steward>
2025-02-24 00:16:28 +0000

    Update netty-reactive-streams-http to 3.0.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -25,7 +25,7 @@ object Versions {
   val catbird = "21.12.0"
   val json4s = "4.0.7"
   val metrics4Scala = "4.3.3"
-  val nettyReactiveStreams = "3.0.3"
+  val nettyReactiveStreams = "3.0.4"
   val ox = "0.5.1"
   val reactiveStreams = "1.0.4"
   val sprayJson = "1.3.6"
