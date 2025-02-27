0cf04d705ef7b03070278ddd21d926064532c6a9
softwaremill-ci <ci@softwaremill.pl>
2025-02-27 01:55:06 +0100

    Update slf4j-api to 2.0.17 (#4372)

--- project/Versions.scala
+++ project/Versions.scala
@@ -66,7 +66,7 @@ object Versions {
   val dogstatsdClient = "4.4.3"
   val nettyAll = "4.1.119.Final"
   val logback = "1.5.17"
-  val slf4j = "2.0.16"
+  val slf4j = "2.0.17"
   val jsoniter = "2.33.2"
   val otel4s = "0.11.2"
 }
