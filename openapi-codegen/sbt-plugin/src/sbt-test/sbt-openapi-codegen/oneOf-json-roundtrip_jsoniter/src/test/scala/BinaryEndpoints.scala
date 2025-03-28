3767cd2e8c51a36af36944af65eea5f95576a71f
Hugh Simpson <hsimpson@rzsoftware.com>
2025-03-28 16:40:21 +0000

    permit returning correct content type without explicitly looking for the accept header

--- openapi-codegen/sbt-plugin/src/sbt-test/sbt-openapi-codegen/oneOf-json-roundtrip_jsoniter/src/test/scala/BinaryEndpoints.scala
+++ openapi-codegen/sbt-plugin/src/sbt-test/sbt-openapi-codegen/oneOf-json-roundtrip_jsoniter/src/test/scala/BinaryEndpoints.scala
@@ -6,21 +6,19 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 import sttp.capabilities.pekko.PekkoStreams
 import sttp.client3.testing.SttpBackendStub
-import sttp.client3.{Response, UriContext, asString, asStringAlways}
+import sttp.client3.{UriContext, asStringAlways}
 import sttp.monad.FutureMonad
 import sttp.tapir.generated.TapirGeneratedEndpoints
 import sttp.tapir.generated.TapirGeneratedEndpoints.{
   PostCustomContentNegotiationBody0In,
-  PostCustomContentNegotiationBody0Out,
-  PostCustomContentNegotiationBody1Err,
   PostCustomContentNegotiationBody1In,
-  PostCustomContentNegotiationBody1Out,
-  PostCustomContentNegotiationBodyIn,
-  PostCustomContentNegotiationBodyStringErr
+  PostCustomContentNegotiationBodyErrFull,
+  PostCustomContentNegotiationBodyOut,
+  PostCustomContentNegotiationBodyOutFull
 }
 import sttp.tapir.server.stub.TapirStubInterpreter
 
-import scala.collection.{immutable, mutable}
+import scala.collection.immutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
@@ -47,34 +45,39 @@ class BinaryEndpoints extends AnyFreeSpec with Matchers {
     def produceSpreadsheet(): ByteString = ByteString("Spreadsheet")
     val route1 =
       TapirGeneratedEndpoints.postCustomContentNegotiation
-        .serverLogicSuccess({ case (accept, body) => // source: Source[String, PostCustomContentNegotiationBodyIn] =>
+        .serverLogicSuccess({ body =>
           val partial = body match {
             case PostCustomContentNegotiationBody0In(csv)         => csv.map(handleCsv)
             case PostCustomContentNegotiationBody1In(spreadsheet) => spreadsheet.map(handleSpreadsheet)
           }
           val running = partial.run()
-          def out = accept match {
-            case "text/csv" =>
-              PostCustomContentNegotiationBody0Out(
-                org.apache.pekko.stream.scaladsl.Source[ByteString](immutable.Iterable.fill(100)(produceCsv()))
-                  .concat(Source.fromFuture(running).map(_ => ByteString("FIN")))
-              )
-            case _ =>
-              PostCustomContentNegotiationBody1Out(
-                org.apache.pekko.stream.scaladsl.Source[ByteString](immutable.Iterable.fill(100)(produceSpreadsheet()))
-                  .concat(Source.fromFuture(running).map(_ => ByteString("FIN")))
-              )
-          }
+          def out: PostCustomContentNegotiationBodyOut =
+            PostCustomContentNegotiationBodyOutFull(
+              `text/csv` = () => {
+                org.apache.pekko.stream.scaladsl
+                  .Source[ByteString](immutable.Iterable.fill(100)(produceCsv()))
+                  .concat(Source.future(running).map(_ => ByteString("FIN")))
+              },
+              `application/vnd.openxmlformats-officedocument.spreadsheetml.sheet` = () => {
+                org.apache.pekko.stream.scaladsl
+                  .Source[ByteString](immutable.Iterable.fill(100)(produceSpreadsheet()))
+                  .concat(Source.future(running).map(_ => ByteString("FIN")))
+              }
+            )
 
           Future.successful(out)
         })
     val route2 =
       TapirGeneratedEndpoints.postCustomContentNegotiation
-        .serverLogic[Future]({ case (accept, _) =>
-          Future.successful(Left(accept match {
-            case "text/csv" => PostCustomContentNegotiationBodyStringErr("some csv error")
-            case _          => PostCustomContentNegotiationBody1Err("some spreadsheet error".getBytes("utf-8"))
-          }))
+        .serverLogic[Future]({ _ =>
+          Future.successful(
+            Left(
+              PostCustomContentNegotiationBodyErrFull(
+                `text/csv` = () => "some csv error",
+                `application/vnd.openxmlformats-officedocument.spreadsheetml.sheet` = () => "some spreadsheet error".getBytes("utf-8")
+              )
+            )
+          )
         })
 
     val stub1 = TapirStubInterpreter(SttpBackendStub[Future, PekkoStreams](new FutureMonad()))
