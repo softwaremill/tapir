065a89ca8ba324a31c0c515f84c9390f12e0dc60
adamw <adam@warski.org>
2025-04-04 16:32:58 +0200

    Fix vertx test

--- server/vertx-server/cats/src/test/scala/sttp/tapir/server/vertx/cats/CatsVertxTestServerInterpreter.scala
+++ server/vertx-server/cats/src/test/scala/sttp/tapir/server/vertx/cats/CatsVertxTestServerInterpreter.scala
@@ -33,7 +33,8 @@ class CatsVertxTestServerInterpreter(vertx: Vertx, dispatcher: Dispatcher[IO])
   ): Resource[IO, Port] = {
     val router = Router.router(vertx)
     routes.toList.foreach(_.apply(router))
-    val server = vertx.createHttpServer(new HttpServerOptions().setPort(0)).requestHandler(router)
+    // the maxFormAttributeSize must be higher than in ServerMultipartTests.maxContentLengthTests
+    val server = vertx.createHttpServer(new HttpServerOptions().setPort(0).setMaxFormAttributeSize(100000)).requestHandler(router)
     val listenIO = ioFromVFuture(server.listen(0))
     // Vertx doesn't offer graceful shutdown with timeout OOTB
     Resource.make(listenIO)(s => ioFromVFuture(s.close).void).map(_.actualPort())
