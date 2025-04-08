68be5e9e2ff7d7faa796a9afb6ce76c8d38a9aaa
softwaremill-ci <ci@softwaremill.pl>
2025-04-08 15:03:13 +0200

    Update tapir-armeria-server, tapir-core, ... to 1.11.23 (#4485)
    Co-authored-by: scala-steward <scala-steward>

--- examples/src/main/scala/sttp/tapir/examples/security/OAuth2GithubHttp4sServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/security/OAuth2GithubHttp4sServer.scala
@@ -1,8 +1,8 @@
 // {cat=Security; effects=cats-effect; server=http4s; json=circe}: Login using OAuth2, authorization code flow
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.17
-//> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.11.17
-//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.11.17
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.23
+//> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.11.23
+//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.11.23
 //> using dep com.softwaremill.sttp.client4::cats:4.0.0-RC3
 //> using dep org.http4s::http4s-blaze-server:0.23.16
 //> using dep com.github.jwt-scala::jwt-circe:10.0.1
