b41b6bdf4d2c9c2e58e221aac48ed3d7c0ecc484
Hugh Simpson <hsimpson@rzsoftware.com>
2025-04-08 17:37:49 +0100

    add tests

---
+++ openapi-codegen/sbt-plugin/src/sbt-test/sbt-openapi-codegen/oneOf-json-roundtrip_scala3/src/test/scala/ServerSpec.scala
@@ -0,0 +1,11 @@
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import sttp.tapir.generated.TapirGeneratedEndpoints._
+
+class ServerSpec extends AnyFreeSpec with Matchers {
+
+  "foo" in {
+    Servers.`https://{environment}.my-co.org:{port}/api/{customer}/prefix`.uri().toString() shouldEqual
+      "https://prod.my-co.org:1234/api/big-dogs/prefix"
+  }
+}
