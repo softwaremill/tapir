617c6687ddbe809694bd924aa9afaaf686d647d3
adamw <adam@warski.org>
2025-05-28 16:10:41 +0200

    Support SSE in netty-sync-server

--- server/netty-server/sync/src/main/scala/sttp/tapir/server/netty/sync/sync.scala
+++ server/netty-server/sync/src/main/scala/sttp/tapir/server/netty/sync/sync.scala
@@ -1,8 +1,18 @@
 package sttp.tapir.server.netty
 
-import sttp.monad.{MonadError, IdentityMonad}
+import ox.Chunk
+import ox.flow.Flow
+import sttp.model.sse.ServerSentEvent
+import sttp.monad.{IdentityMonad, MonadError}
 import sttp.shared.Identity
+import sttp.tapir.*
+
+import java.nio.charset.StandardCharsets
 
 package object sync:
   type IdRoute = Route[Identity]
   private[sync] implicit val idMonad: MonadError[Identity] = IdentityMonad
+
+  val serverSentEventsBody: StreamBodyIO[Flow[Chunk[Byte]], Flow[ServerSentEvent], OxStreams] =
+    streamTextBody(OxStreams)(CodecFormat.TextEventStream(), Some(StandardCharsets.UTF_8))
+      .map(OxServerSentEvents.parseBytesToSSE)(OxServerSentEvents.serializeSSEToBytes)
