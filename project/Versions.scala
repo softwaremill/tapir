7e11f739f3c3b425f311ed8ea9a37c70a0116a0c
softwaremill-ci <ci@softwaremill.pl>
2025-06-11 02:44:57 +0200

    Update opentelemetry-semconv to 1.34.0 (#4646)

--- project/Versions.scala
+++ project/Versions.scala
@@ -63,7 +63,7 @@ object Versions {
   val decline = "2.5.0"
   val quicklens = "1.9.12"
   val openTelemetry = "1.51.0"
-  val openTelemetrySemconvVersion = "1.32.0"
+  val openTelemetrySemconvVersion = "1.34.0"
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.4.4"
   val nettyAll = "4.2.0.Final"
