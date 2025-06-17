8c2679a19b97574b320ecd60002a10b759d3bf4c
scala-steward <scala-steward>
2025-06-17 00:15:34 +0000

    Update sttp-mock-server, ... to 1.11.34

--- examples/src/main/scala/sttp/tapir/examples/custom_types/commaSeparatedQueryParameter.scala
+++ examples/src/main/scala/sttp/tapir/examples/custom_types/commaSeparatedQueryParameter.scala
@@ -1,8 +1,8 @@
 // {cat=Custom types; effects=Direct; server=Netty; docs=Swagger UI}: Handling comma-separated query parameters
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.33
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.33
-//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.33
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.34
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.34
+//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.34
 
 package sttp.tapir.examples.custom_types
 
--- examples/src/main/scala/sttp/tapir/examples/observability/ZioMetricsExample.scala
+++ examples/src/main/scala/sttp/tapir/examples/observability/ZioMetricsExample.scala
@@ -1,8 +1,8 @@
 // {cat=Observability; effects=ZIO; server=ZIO HTTP}: Reporting Prometheus metrics
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.33
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.11.33
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-metrics:1.11.33
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.34
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.11.34
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-metrics:1.11.34
 
 package sttp.tapir.examples.observability
 
