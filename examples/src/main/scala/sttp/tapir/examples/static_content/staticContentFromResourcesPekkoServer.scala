263c34c8ad357d5e27cf56a80537fa55db4ffbf2
scala-steward <scala-steward>
2025-06-27 00:15:16 +0000

    Update sttp-mock-server, ... to 1.11.35

--- examples/src/main/scala/sttp/tapir/examples/static_content/staticContentFromResourcesPekkoServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/static_content/staticContentFromResourcesPekkoServer.scala
@@ -1,8 +1,8 @@
 // {cat=Static content; effects=Future; server=Pekko HTTP}: Serving static files from resources
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.34
-//> using dep com.softwaremill.sttp.tapir::tapir-files:1.11.34
-//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.11.34
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.35
+//> using dep com.softwaremill.sttp.tapir::tapir-files:1.11.35
+//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.11.35
 
 package sttp.tapir.examples.static_content
 
--- examples/src/main/scala/sttp/tapir/examples/websocket/WebSocketZioHttpCustomCodecServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/websocket/WebSocketZioHttpCustomCodecServer.scala
@@ -1,7 +1,7 @@
 // {cat=WebSocket; effects=ZIO; server=ZIO HTTP}: Describe and implement a WebSocket endpoint, being notified on the server-side that a client closed the socket, using a custom codec
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.34
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.11.34
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.35
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.11.35
 
 package sttp.tapir.examples.websocket
 
