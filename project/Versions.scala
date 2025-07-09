95c9cc463abd3f697b86dac84012807fd8f427c0
softwaremill-ci <ci@softwaremill.pl>
2025-07-09 02:42:07 +0200

    Update cats-effect to 3.6.2 (#4693)

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val http4sBlazeServer = "0.23.17"
   val http4sBlazeClient = "0.23.17"
   val catsCore = "2.13.0"
-  val catsEffect = "3.6.1"
+  val catsEffect = "3.6.2"
   val circe = "0.14.14"
   val circeRefined = "0.15.1"
   val circeGenericExtras = "0.14.3"
