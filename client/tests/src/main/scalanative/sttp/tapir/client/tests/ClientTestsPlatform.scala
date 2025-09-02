c7014b061efcd9826327d984cb1ae3417dfd13c4
Adam Warski <adam@warski.org>
2025-09-02 16:19:19 +0200

    Clean up ClientTestsPlatform by removing unused code

--- client/tests/src/main/scalanative/sttp/tapir/client/tests/ClientTestsPlatform.scala
+++ client/tests/src/main/scalanative/sttp/tapir/client/tests/ClientTestsPlatform.scala
@@ -1,20 +1,13 @@
 package sttp.tapir.client.tests
 
-// import cats.effect.unsafe.IORuntime
 import scala.concurrent.ExecutionContext
 
 object ClientTestsPlatform {
-  // These two lines should define IO Runtime and ExcecutionContext on Scala Native, but ScalaTest AsyncFunSuite fails with Timeout exception
-  // val ioRT:IORuntime = cats.effect.unsafe.implicits.global
-  // val executionContext: ExecutionContext = ioRT.compute
-
   // Inspired by the default ExecutionContext implementation in MUnit, MUnit works fine on Native
   val executionContext: ExecutionContext = new ExecutionContext {
     def execute(runnable: Runnable): Unit = runnable.run()
     def reportFailure(cause: Throwable): Unit = cause.printStackTrace()
   }
-  // private val globalRT: IORuntime = cats.effect.unsafe.implicits.global
-  // val ioRT: IORuntime = IORuntime.apply(executionContext, executionContext, globalRT.scheduler, globalRT.shutdown, globalRT.config)
 
   val platformIsScalaJS: Boolean = false
   val platformIsScalaNative: Boolean = true
