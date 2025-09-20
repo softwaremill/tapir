d09f6664ef68cc188823b996eb8601e17e524364
softwaremill-ci <ci@softwaremill.pl>
2025-09-20 02:42:57 +0200

    Update opentelemetry-exporter-otlp, ... to 1.54.1 (#4826)

--- examples/src/main/scala/sttp/tapir/examples/observability/OpenTelemetryTracingExample.scala
+++ examples/src/main/scala/sttp/tapir/examples/observability/OpenTelemetryTracingExample.scala
@@ -3,8 +3,8 @@
 //> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.18
 //> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.11.18
 //> using dep com.softwaremill.sttp.tapir::tapir-opentelemetry-tracing:1.11.44
-//> using dep io.opentelemetry:opentelemetry-exporter-otlp:1.53.0
-//> using dep io.opentelemetry:opentelemetry-sdk-extension-autoconfigure:1.53.0
+//> using dep io.opentelemetry:opentelemetry-exporter-otlp:1.54.1
+//> using dep io.opentelemetry:opentelemetry-sdk-extension-autoconfigure:1.54.1
 //> using dep ch.qos.logback:logback-classic:1.5.17
 
 package sttp.tapir.examples.observability
