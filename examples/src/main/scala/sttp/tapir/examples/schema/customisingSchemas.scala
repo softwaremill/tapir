fad211f2bc41de468cd284fde2eb660f955dd341
scala-steward <scala-steward>
2025-10-20 00:15:51 +0000

    Update sttp-mock-server, ... to 1.11.50

--- examples/src/main/scala/sttp/tapir/examples/schema/customisingSchemas.scala
+++ examples/src/main/scala/sttp/tapir/examples/schema/customisingSchemas.scala
@@ -1,9 +1,9 @@
 // {cat=Schemas; effects=Future; server=Netty; json=circe; docs=Swagger UI}: Customising a derived schema, using annotations, and using implicits
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.49
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server:1.11.49
-//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.49
-//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.11.49
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.50
+//> using dep com.softwaremill.sttp.tapir::tapir-netty-server:1.11.50
+//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.50
+//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.11.50
 
 package sttp.tapir.examples.schema
 
