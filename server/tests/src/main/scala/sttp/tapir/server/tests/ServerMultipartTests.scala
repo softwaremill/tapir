7d1a6f4014056d218d2846aa34ac75453e5818b0
adamw <adam@warski.org>
2025-10-27 16:29:52 +0100

    Tests for deleting files after request completes

--- server/tests/src/main/scala/sttp/tapir/server/tests/ServerMultipartTests.scala
+++ server/tests/src/main/scala/sttp/tapir/server/tests/ServerMultipartTests.scala
@@ -1,5 +1,6 @@
 package sttp.tapir.server.tests
 
+import cats.effect.IO
 import cats.implicits._
 import org.scalatest.matchers.should.Matchers._
 import sttp.client4.{multipartFile, _}
@@ -21,6 +22,10 @@ import sttp.tapir.server.model.EndpointExtensions._
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
+import scala.collection.JavaConverters.asScalaSetConverter
+import java.nio.file.Files
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.ConcurrentHashMap
 
 class ServerMultipartTests[F[_], OPTIONS, ROUTE](
     createServerTest: CreateServerTest[F, Any, OPTIONS, ROUTE],
@@ -227,6 +232,55 @@ class ServerMultipartTests[F[_], OPTIONS, ROUTE](
                 fail("Expected BadRequest, but got " + r.code)
             }
           }
+      },
+      testServer(
+        endpoint.post
+          .in("api" / "echo" / "multipart")
+          .in(multipartBody[SingleFileBody])
+          .out(stringBody),
+        "large part"
+      )((data: SingleFileBody) => pureResult((Files.readString(data.file.body.toPath, StandardCharsets.UTF_8)).asRight[Unit])) {
+        (backend, baseUri) =>
+          val largeBody = "pineapple".repeat(10000)
+          basicStringRequest
+            .post(uri"$baseUri/api/echo/multipart")
+            .multipartBody(multipart("file", largeBody))
+            .send(backend)
+            .map { r =>
+              r.code shouldBe StatusCode.Ok
+              r.body shouldBe largeBody
+            }
+      }, {
+        val files = ConcurrentHashMap.newKeySet[TapirFile]()
+        testServer(
+          endpoint.post
+            .in("api" / "echo" / "multipart")
+            .in(multipartBody[SingleFileBody])
+            .out(stringBody),
+          "file part deleted after request handling completes"
+        )((data: SingleFileBody) => {
+          files.add(data.file.body)
+          pureResult("ok".asRight[Unit])
+        }) { (backend, baseUri) =>
+          val smallBody = "pineapple"
+          val largeBody = smallBody.repeat(10000)
+          def testWithBody(body: String) = basicStringRequest
+            .post(uri"$baseUri/api/echo/multipart")
+            .multipartBody(multipart("file", body))
+            .send(backend)
+            .map { r =>
+              r.code shouldBe StatusCode.Ok
+            }
+
+          testWithBody(smallBody) >> testWithBody(largeBody) >> IO.blocking {
+            files.asScala.foreach { file =>
+              if (Files.exists(file.toPath)) {
+                fail(s"File ${file.getName} still exists")
+              }
+            }
+            succeed
+          }
+        }
       }
     )
   }
