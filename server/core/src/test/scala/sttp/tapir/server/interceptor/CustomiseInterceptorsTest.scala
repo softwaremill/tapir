7698c41cf70a3fc59bda70ed5344783db6f9a4ef
adamw <adam@warski.org>
2025-11-25 14:43:40 +0100

    Add test for customise interceptors

---
+++ server/core/src/test/scala/sttp/tapir/server/interceptor/CustomiseInterceptorsTest.scala
@@ -0,0 +1,208 @@
+package sttp.tapir.server.interceptor
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import sttp.model._
+import sttp.monad.{IdentityMonad, MonadError}
+import sttp.shared.Identity
+import sttp.tapir._
+import sttp.tapir.capabilities.NoStreams
+import sttp.tapir.model.ServerRequest
+import sttp.tapir.server.TestUtil._
+import sttp.tapir.server.interceptor.log.{ExceptionContext, ServerLog}
+import sttp.tapir.server.interpreter.ServerInterpreter
+import sttp.tapir.server.model.ServerResponse
+
+import scala.collection.mutable.ListBuffer
+import sttp.tapir.server.interceptor.reject.DefaultRejectHandler
+
+class CustomiseInterceptorsTest extends AnyFlatSpec with Matchers {
+  implicit val idMonad: MonadError[Identity] = IdentityMonad
+
+  // Test request that successfully matches an endpoint
+  val successRequest: ServerRequest = createTestRequest(List("test"), List(("x", "1")))
+
+  // Test request that doesn't match any endpoint
+  val notFoundRequest: ServerRequest = createTestRequest(List("notfound"))
+
+  // Stub ServerLog implementation that captures log calls
+  class StubServerLog extends ServerLog[Identity] {
+    override type TOKEN = Unit
+
+    val receivedRequests: ListBuffer[ServerRequest] = ListBuffer.empty
+    val handledRequests: ListBuffer[(DecodeSuccessContext[Identity, ?, ?, ?], ServerResponse[?])] = ListBuffer.empty
+    val exceptions: ListBuffer[(ExceptionContext[?, ?], Throwable)] = ListBuffer.empty
+
+    override def requestToken: TOKEN = ()
+
+    override def requestReceived(request: ServerRequest, token: TOKEN): Identity[Unit] = {
+      receivedRequests.append(request)
+      ()
+    }
+
+    override def decodeFailureNotHandled(ctx: DecodeFailureContext, token: TOKEN): Identity[Unit] = {
+      ()
+    }
+
+    override def decodeFailureHandled(ctx: DecodeFailureContext, response: ServerResponse[?], token: TOKEN): Identity[Unit] = {
+      ()
+    }
+
+    override def securityFailureHandled(
+        ctx: SecurityFailureContext[Identity, ?],
+        response: ServerResponse[?],
+        token: TOKEN
+    ): Identity[Unit] = {
+      ()
+    }
+
+    override def requestHandled(ctx: DecodeSuccessContext[Identity, ?, ?, ?], response: ServerResponse[?], token: TOKEN): Identity[Unit] = {
+      handledRequests.append((ctx, response))
+      ()
+    }
+
+    override def exception(ctx: ExceptionContext[?, ?], ex: Throwable, token: TOKEN): Identity[Unit] = {
+      exceptions.append((ctx, ex))
+      ()
+    }
+  }
+
+  "CustomiseInterceptors.interceptors" should "log info message when request is completed successfully" in {
+    // given
+    val stubLog = new StubServerLog()
+    val customise = CustomiseInterceptors[Identity, List[Interceptor[Identity]]](
+      createOptions = _.interceptors
+    ).serverLog(stubLog)
+
+    val testEndpoint = endpoint.get
+      .in("test")
+      .in(query[String]("x"))
+      .out(stringBody)
+      .serverLogic[Identity](x => Right(s"Hello $x"))
+
+    val interpreter = new ServerInterpreter[Any, Identity, String, NoStreams](
+      _ => List(testEndpoint),
+      TestRequestBody,
+      StringToResponseBody,
+      customise.interceptors,
+      _ => ()
+    )
+
+    // when
+    val _ = interpreter.apply(successRequest)
+
+    // then
+    stubLog.receivedRequests should have size 1
+    stubLog.receivedRequests.head.uri shouldBe successRequest.uri
+    stubLog.handledRequests should have size 1
+    stubLog.handledRequests.head._2.code shouldBe StatusCode.Ok
+  }
+
+  it should "return 500 when request handling throws an exception" in {
+    // given
+    val stubLog = new StubServerLog()
+    val customise = CustomiseInterceptors[Identity, List[Interceptor[Identity]]](
+      createOptions = _.interceptors
+    ).serverLog(stubLog)
+
+    val testEndpoint = endpoint.get
+      .in("test")
+      .in(query[String]("x"))
+      .out(stringBody)
+      .serverLogic[Identity](_ => throw new RuntimeException("Test exception"))
+
+    val interpreter = new ServerInterpreter[Any, Identity, String, NoStreams](
+      _ => List(testEndpoint),
+      TestRequestBody,
+      StringToResponseBody,
+      customise.interceptors,
+      _ => ()
+    )
+
+    // when
+    val response = interpreter.apply(successRequest)
+
+    // then
+    response match {
+      case RequestResult.Response(serverResponse) =>
+        serverResponse.code shouldBe StatusCode.InternalServerError
+        serverResponse.body shouldBe Some("Internal server error")
+      case _ => fail("Expected Response")
+    }
+    stubLog.exceptions should have size 1
+    stubLog.exceptions.head._2.getMessage shouldBe "Test exception"
+  }
+
+  it should "respect reject interceptor set to always return 404" in {
+    // given
+    val stubLog = new StubServerLog()
+    val customise = CustomiseInterceptors[Identity, List[Interceptor[Identity]]](
+      createOptions = _.interceptors
+    )
+      .serverLog(stubLog)
+      .rejectHandler(DefaultRejectHandler.orNotFound[Identity])
+
+    val testEndpoint = endpoint.get
+      .in("test")
+      .in(query[String]("x"))
+      .out(stringBody)
+      .serverLogic[Identity](x => Right(s"Hello $x"))
+
+    val interpreter = new ServerInterpreter[Any, Identity, String, NoStreams](
+      _ => List(testEndpoint),
+      TestRequestBody,
+      StringToResponseBody,
+      customise.interceptors,
+      _ => ()
+    )
+
+    // when
+    val response = interpreter.apply(notFoundRequest)
+
+    // then
+    response match {
+      case RequestResult.Response(serverResponse) =>
+        serverResponse.code shouldBe StatusCode.NotFound
+        serverResponse.body shouldBe Some("Not Found")
+      case _ => fail("Expected Response")
+    }
+    stubLog.handledRequests shouldBe empty
+  }
+
+  it should "apply interceptors in correct order" in {
+    // given
+    val callTrail: ListBuffer[String] = ListBuffer.empty
+    val stubLog = new StubServerLog()
+
+    val prependedInterceptor = new AddToTrailInterceptor(callTrail.append(_), "prepended")
+    val appendedInterceptor = new AddToTrailInterceptor(callTrail.append(_), "appended")
+
+    val customise = CustomiseInterceptors[Identity, List[Interceptor[Identity]]](
+      createOptions = _.interceptors
+    )
+      .prependInterceptor(prependedInterceptor)
+      .serverLog(stubLog)
+      .appendInterceptor(appendedInterceptor)
+
+    val testEndpoint = endpoint.get
+      .in("test")
+      .in(query[String]("x"))
+      .out(stringBody)
+      .serverLogic[Identity](x => Right(s"Hello $x"))
+
+    val interpreter = new ServerInterpreter[Any, Identity, String, NoStreams](
+      _ => List(testEndpoint),
+      TestRequestBody,
+      StringToResponseBody,
+      customise.interceptors,
+      _ => ()
+    )
+
+    // when
+    val _ = interpreter.apply(successRequest)
+
+    // then
+    // Prepended interceptors are called first on request, appended are called last on request
+    callTrail.toList shouldBe List("prepended success", "appended success")
+  }
+}
--- server/core/src/test/scala/sttp/tapir/server/interpreter/ServerInterpreterTest.scala
+++ server/core/src/test/scala/sttp/tapir/server/interpreter/ServerInterpreterTest.scala
@@ -2,36 +2,22 @@ package sttp.tapir.server.interpreter
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import sttp.model.Uri._
 import sttp.model._
 import sttp.monad.MonadError
 import sttp.shared.Identity
-import sttp.tapir.TestUtil._
 import sttp.tapir._
 import sttp.tapir.capabilities.NoStreams
-import sttp.tapir.model.{ConnectionInfo, ServerRequest}
+import sttp.tapir.model.ServerRequest
 import sttp.tapir.server.TestUtil._
 import sttp.tapir.server.interceptor.RequestResult.Response
 import sttp.tapir.server.interceptor._
 import sttp.tapir.server.interceptor.reject.{DefaultRejectHandler, RejectInterceptor}
 import sttp.tapir.server.model.{ServerResponse, ValuedEndpointOutput}
 
-import scala.collection.immutable
-
 class ServerInterpreterTest extends AnyFlatSpec with Matchers {
-  val testRequest: ServerRequest = new ServerRequest {
-    override def protocol: String = ""
-    override def connectionInfo: ConnectionInfo = ConnectionInfo(None, None, None)
-    override def underlying: Any = ()
-    override def pathSegments: List[String] = Nil
-    override def queryParameters: QueryParams = QueryParams.fromSeq(List(("x", "1"), ("y", "2")))
-    override def method: Method = Method.GET
-    override def uri: Uri = uri"http://example.com"
-    override def headers: immutable.Seq[Header] = Nil
-    override def attribute[T](k: AttributeKey[T]): Option[T] = None
-    override def attribute[T](k: AttributeKey[T], v: T): ServerRequest = this
-    override def withUnderlying(underlying: Any): ServerRequest = this
-  }
+  implicit val idMonad: MonadError[Identity] = sttp.monad.IdentityMonad
+
+  val testRequest: ServerRequest = createTestRequest(Nil, List(("x", "1"), ("y", "2")))
 
   it should "call the interceptors in the correct order" in {
     val callTrail: collection.mutable.ListBuffer[String] = collection.mutable.ListBuffer.empty
@@ -61,7 +47,7 @@ class ServerInterpreterTest extends AnyFlatSpec with Matchers {
       )
 
     // when
-    interpreter.apply(testRequest)
+    val _ = interpreter.apply(testRequest)
 
     // then
     callTrail.toList shouldBe List("2 request", "1 success", "2 success", "3 success")
@@ -95,7 +81,7 @@ class ServerInterpreterTest extends AnyFlatSpec with Matchers {
       )
 
     // when
-    interpreter.apply(testRequest)
+    val _ = interpreter.apply(testRequest)
 
     // then
     callTrail.toList shouldBe List("x decode", "y decode", "1 security failure")
@@ -130,30 +116,4 @@ class ServerInterpreterTest extends AnyFlatSpec with Matchers {
     response should matchPattern { case Response(ServerResponse(_, _, Some(_), _)) => }
   }
 
-  class AddToTrailInterceptor(addCallTrail: String => Unit, prefix: String) extends EndpointInterceptor[Identity] {
-    override def apply[B](responder: Responder[Identity, B], endpointHandler: EndpointHandler[Identity, B]): EndpointHandler[Identity, B] =
-      new EndpointHandler[Identity, B] {
-        override def onDecodeSuccess[A, U, I](
-            ctx: DecodeSuccessContext[Identity, A, U, I]
-        )(implicit monad: MonadError[Identity], bodyListener: BodyListener[Identity, B]): Identity[ServerResponse[B]] = {
-          addCallTrail(s"$prefix success")
-          endpointHandler.onDecodeSuccess(ctx)(idMonad, bodyListener)
-        }
-
-        override def onSecurityFailure[A](ctx: SecurityFailureContext[Identity, A])(implicit
-            monad: MonadError[Identity],
-            bodyListener: BodyListener[Identity, B]
-        ): Identity[ServerResponse[B]] = {
-          addCallTrail(s"$prefix security failure")
-          endpointHandler.onSecurityFailure(ctx)(idMonad, bodyListener)
-        }
-
-        override def onDecodeFailure(
-            ctx: DecodeFailureContext
-        )(implicit monad: MonadError[Identity], bodyListener: BodyListener[Identity, B]): Identity[Option[ServerResponse[B]]] = {
-          addCallTrail(s"$prefix failure")
-          endpointHandler.onDecodeFailure(ctx)(idMonad, bodyListener)
-        }
-      }
-  }
 }
