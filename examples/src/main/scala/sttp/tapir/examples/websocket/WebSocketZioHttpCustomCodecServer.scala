da4372171eaa4159c9d8d5556911eb6771a4855a
scala-steward <scala-steward>
2025-12-03 00:14:56 +0000

    Update sttp-mock-server, ... to 1.12.6

--- examples/src/main/scala/sttp/tapir/examples/websocket/WebSocketZioHttpCustomCodecServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/websocket/WebSocketZioHttpCustomCodecServer.scala
@@ -1,7 +1,7 @@
 // {cat=WebSocket; effects=ZIO; server=ZIO HTTP}: Describe and implement a WebSocket endpoint, being notified on the server-side that a client closed the socket, using a custom codec
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.12.5
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.12.5
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.12.6
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.12.6
 
 package sttp.tapir.examples.websocket
 
