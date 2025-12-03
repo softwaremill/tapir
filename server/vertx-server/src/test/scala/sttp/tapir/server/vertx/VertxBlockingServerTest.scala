eda20ac90e326bc5e7be0bc7991697959ee59e05
adamw <adam@warski.org>
2025-12-03 16:47:26 +0100

    Fix vertx

--- server/vertx-server/src/test/scala/sttp/tapir/server/vertx/VertxBlockingServerTest.scala
+++ server/vertx-server/src/test/scala/sttp/tapir/server/vertx/VertxBlockingServerTest.scala
@@ -12,18 +12,22 @@ class VertxBlockingServerTest extends TestSuite {
   def vertxResource: Resource[IO, Vertx] =
     Resource.make(IO.delay(Vertx.vertx()))(vertx => IO.delay(vertx.close()).void)
 
-  override def tests: Resource[IO, List[Test]] = backendResource.flatMap { backend =>
-    vertxResource.map { implicit vertx =>
-      implicit val m: FutureMonad = new FutureMonad()(ExecutionContext.global)
-      val interpreter = new VertxTestServerBlockingInterpreter(vertx)
-      val createServerTest = new DefaultCreateServerTest(backend, interpreter)
+  override def tests: Resource[IO, List[Test]] = backendResource
+    // for streaming requests, vertx responds with transfer-encoding header, which is not supported by http2
+    // however, connections are negotiated with http2; hence, forcing http1 for these tests to work
+    .map(backend => new ForceHttp1BackendWrapper(backend))
+    .flatMap { backend =>
+      vertxResource.map { implicit vertx =>
+        implicit val m: FutureMonad = new FutureMonad()(ExecutionContext.global)
+        val interpreter = new VertxTestServerBlockingInterpreter(vertx)
+        val createServerTest = new DefaultCreateServerTest(backend, interpreter)
 
-      new AllServerTests(createServerTest, interpreter, backend, multipart = false, reject = false, options = false).tests() ++
-        new ServerMultipartTests(
-          createServerTest,
-          partContentTypeHeaderSupport = false, // README: doesn't seem supported but I may be wrong
-          partOtherHeaderSupport = false
-        ).tests()
+        new AllServerTests(createServerTest, interpreter, backend, multipart = false, reject = false, options = false).tests() ++
+          new ServerMultipartTests(
+            createServerTest,
+            partContentTypeHeaderSupport = false, // README: doesn't seem supported but I may be wrong
+            partOtherHeaderSupport = false
+          ).tests()
+      }
     }
-  }
 }
--- server/vertx-server/zio/src/test/scala/sttp/tapir/server/vertx/zio/ZioVertxServerTest.scala
+++ server/vertx-server/zio/src/test/scala/sttp/tapir/server/vertx/zio/ZioVertxServerTest.scala
@@ -19,42 +19,46 @@ class ZioVertxServerTest extends TestSuite with OptionValues {
   def vertxResource: Resource[IO, Vertx] =
     Resource.make(IO.delay(Vertx.vertx()))(vertx => IO.delay(vertx.close()).void)
 
-  override def tests: Resource[IO, List[Test]] = backendResource.flatMap { backend =>
-    vertxResource.map { implicit vertx =>
-      implicit val m: MonadError[Task] = new RIOMonadError[Any]
-      val interpreter = new ZioVertxTestServerInterpreter(vertx)
-      val createServerTest = new DefaultCreateServerTest(backend, interpreter)
+  override def tests: Resource[IO, List[Test]] = backendResource
+    // for streaming requests, vertx responds with transfer-encoding header, which is not supported by http2
+    // however, connections are negotiated with http2; hence, forcing http1 for these tests to work
+    .map(backend => new ForceHttp1BackendWrapper(backend))
+    .flatMap { backend =>
+      vertxResource.map { implicit vertx =>
+        implicit val m: MonadError[Task] = new RIOMonadError[Any]
+        val interpreter = new ZioVertxTestServerInterpreter(vertx)
+        val createServerTest = new DefaultCreateServerTest(backend, interpreter)
 
-      def additionalTests(): List[Test] = List(
-        createServerTest.testServer(
-          endpoint.out(plainBody[String]),
-          "Do not execute effects on vert.x thread"
-        )((_: Unit) => ZIO.attempt(Thread.currentThread().getName).asRight) { (backend, baseUri) =>
-          basicRequest.get(baseUri).send(backend).map(_.body.toOption.value should not include "vert.x-eventloop-thread")
-        }
-      )
-      def drainZStream(zStream: ZioStreams.BinaryStream): Task[Unit] =
-        zStream.run(ZSink.drain)
+        def additionalTests(): List[Test] = List(
+          createServerTest.testServer(
+            endpoint.out(plainBody[String]),
+            "Do not execute effects on vert.x thread"
+          )((_: Unit) => ZIO.attempt(Thread.currentThread().getName).asRight) { (backend, baseUri) =>
+            basicRequest.get(baseUri).send(backend).map(_.body.toOption.value should not include "vert.x-eventloop-thread")
+          }
+        )
+        def drainZStream(zStream: ZioStreams.BinaryStream): Task[Unit] =
+          zStream.run(ZSink.drain)
 
-      new AllServerTests(createServerTest, interpreter, backend, multipart = false, reject = false, options = false, metrics = false)
-        .tests() ++
-        new ServerMultipartTests(
-          createServerTest,
-          partContentTypeHeaderSupport = false, // README: doesn't seem supported but I may be wrong
-          partOtherHeaderSupport = false
-        ).tests() ++ additionalTests() ++
-        new ServerStreamingTests(createServerTest).tests(ZioStreams)(drainZStream) ++
-        new ServerWebSocketTests(
-          createServerTest,
-          ZioStreams,
-          autoPing = true,
-          handlePong = true,
-          expectCloseResponse = false,
-          frameConcatenation = false
-        ) {
-          override def functionToPipe[A, B](f: A => B): streams.Pipe[A, B] = in => in.map(f)
-          override def emptyPipe[A, B]: streams.Pipe[A, B] = _ => ZStream.empty
-        }.tests() ++ new ServerMetricsTest(createServerTest, interpreter, supportsMetricsDecodeFailureCallbacks = false).tests()
+        new AllServerTests(createServerTest, interpreter, backend, multipart = false, reject = false, options = false, metrics = false)
+          .tests() ++
+          new ServerMultipartTests(
+            createServerTest,
+            partContentTypeHeaderSupport = false, // README: doesn't seem supported but I may be wrong
+            partOtherHeaderSupport = false
+          ).tests() ++ additionalTests() ++
+          new ServerStreamingTests(createServerTest).tests(ZioStreams)(drainZStream) ++
+          new ServerWebSocketTests(
+            createServerTest,
+            ZioStreams,
+            autoPing = true,
+            handlePong = true,
+            expectCloseResponse = false,
+            frameConcatenation = false
+          ) {
+            override def functionToPipe[A, B](f: A => B): streams.Pipe[A, B] = in => in.map(f)
+            override def emptyPipe[A, B]: streams.Pipe[A, B] = _ => ZStream.empty
+          }.tests() ++ new ServerMetricsTest(createServerTest, interpreter, supportsMetricsDecodeFailureCallbacks = false).tests()
+      }
     }
-  }
 }
