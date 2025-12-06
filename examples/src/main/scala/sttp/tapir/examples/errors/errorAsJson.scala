9730143578eded7aab8a0977fdca6574cbc4922e
softwaremill-ci <ci@softwaremill.pl>
2025-12-06 01:51:37 +0100

    Update sttp-mock-server, ... to 1.13.0 (#4962)

--- examples/src/main/scala/sttp/tapir/examples/errors/errorAsJson.scala
+++ examples/src/main/scala/sttp/tapir/examples/errors/errorAsJson.scala
@@ -1,8 +1,8 @@
 // {cat=Error handling; effects=Future; server=Pekko HTTP; json=circe}: Default error handler returning errors as JSON
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.12.6
-//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.12.6
-//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.12.6
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.0
+//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.13.0
+//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.13.0
 //> using dep org.apache.pekko::pekko-http:1.0.1
 //> using dep org.apache.pekko::pekko-stream:1.0.3
 //> using dep com.softwaremill.sttp.client4::core:4.0.0-RC3
