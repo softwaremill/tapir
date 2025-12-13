298c8dfc961478f7eacf46144f59236edbb8cbf2
scala-steward <scala-steward>
2025-12-13 00:16:28 +0000

    Update sttp-mock-server, ... to 1.13.3

--- examples/src/main/scala/sttp/tapir/examples/streaming/streamingPekkoServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/streaming/streamingPekkoServer.scala
@@ -1,7 +1,7 @@
 // {cat=Streaming; effects=Future; server=Pekko HTTP}: Stream response as a Pekko stream
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.2
-//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.13.2
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.3
+//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.13.3
 //> using dep com.softwaremill.sttp.client4::core:4.0.0-RC3
 
 package sttp.tapir.examples.streaming
