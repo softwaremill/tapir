7f2b16f91ba328bb9a5e598b41114099e291a3c3
softwaremill-ci <ci@softwaremill.pl>
2026-01-12 01:38:05 +0100

    Update sttp-mock-server, ... to 1.13.4 (#5007)

--- examples/src/main/scala/sttp/tapir/examples/security/serverSecurityLogicPekko.scala
+++ examples/src/main/scala/sttp/tapir/examples/security/serverSecurityLogicPekko.scala
@@ -1,7 +1,7 @@
 // {cat=Security; effects=Future; server=Pekko HTTP}: Separating security and server logic, with a reusable base endpoint
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.2
-//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.13.2
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.4
+//> using dep com.softwaremill.sttp.tapir::tapir-pekko-http-server:1.13.4
 //> using dep com.softwaremill.sttp.client4::core:4.0.0-RC3
 
 package sttp.tapir.examples.security
