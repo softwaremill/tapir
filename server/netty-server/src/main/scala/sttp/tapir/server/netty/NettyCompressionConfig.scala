50ac84386e02370d1b0ed1a990caa3dcb734c755
scala-steward <scala-steward>
2026-02-01 00:19:21 +0000

    Reformat with scalafmt 3.10.6
    Executed command: scalafmt --non-interactive

--- server/netty-server/src/main/scala/sttp/tapir/server/netty/NettyCompressionConfig.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/NettyCompressionConfig.scala
@@ -3,8 +3,8 @@ package sttp.tapir.server.netty
 /** Configuration for HTTP response compression in Netty server.
   *
   * @param enabled
-  *   Whether compression is enabled. When enabled, the server will compress responses based on the client's Accept-Encoding header.
-  *   Netty uses a default compression level of 6 (balanced between speed and compression ratio).
+  *   Whether compression is enabled. When enabled, the server will compress responses based on the client's Accept-Encoding header. Netty
+  *   uses a default compression level of 6 (balanced between speed and compression ratio).
   */
 case class NettyCompressionConfig(
     enabled: Boolean = false
@@ -13,10 +13,10 @@ case class NettyCompressionConfig(
 }
 
 object NettyCompressionConfig {
+
   /** Compression disabled (default) */
   val default: NettyCompressionConfig = NettyCompressionConfig()
 
   /** Compression enabled with Netty's default settings */
   val enabled: NettyCompressionConfig = NettyCompressionConfig(enabled = true)
 }
-
