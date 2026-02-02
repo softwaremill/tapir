66f111e1d514d2f28d0e6eaf15734c25f49c8047
scala-steward <scala-steward>
2026-02-02 00:12:17 +0000

    Update zio-http to 3.8.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -39,7 +39,7 @@ object Versions {
   val iron = "3.0.3"
   val enumeratum = "1.9.0"
   val zio = "2.1.24"
-  val zioHttp = "3.8.0"
+  val zioHttp = "3.8.1"
   val zioInteropCats = "23.1.0.13"
   val zioInteropReactiveStreams = "2.0.2"
   val zioJson = "0.7.44"
