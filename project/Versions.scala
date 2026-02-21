934f425ab3b16bbc6d42e7a17e4ab038d6743a03
scala-steward <scala-steward>
2026-02-21 00:12:12 +0000

    Update opentelemetry-semconv to 1.40.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -64,7 +64,7 @@ object Versions {
   val decline = "2.5.0"
   val quicklens = "1.9.12"
   val openTelemetry = "1.59.0"
-  val openTelemetrySemconvVersion = "1.39.0"
+  val openTelemetrySemconvVersion = "1.40.0"
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.4.5"
   val nettyAll = "4.2.10.Final"
