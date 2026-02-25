9b7f263c570a39efa65ecd6000fae707dc278afa
scala-steward <scala-steward>
2026-02-25 00:20:33 +0000

    Update sttp-mock-server, ... to 1.13.9

--- examples/src/main/scala/sttp/tapir/examples/HelloWorldZioHttpServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/HelloWorldZioHttpServer.scala
@@ -1,8 +1,8 @@
 // {cat=Hello, World!; effects=ZIO; server=ZIO HTTP; json=ZIO JSON}: Exposing an endpoint using the ZIO HTTP server
 
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.8
-//> using dep com.softwaremill.sttp.tapir::tapir-json-zio:1.13.8
-//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.13.8
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.9
+//> using dep com.softwaremill.sttp.tapir::tapir-json-zio:1.13.9
+//> using dep com.softwaremill.sttp.tapir::tapir-zio-http-server:1.13.9
 
 package sttp.tapir.examples
 
