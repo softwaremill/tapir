dbe2acdcc86328e30ee788c97641cd6799d3bf27
Felix Chapman <foc@qad.com>
2026-03-05 17:44:21 +0000

    Only regenerate when input files change

--- openapi-codegen/sbt-plugin/src/main/scala/sttp/tapir/sbt/OpenapiCodegenPlugin.scala
+++ openapi-codegen/sbt-plugin/src/main/scala/sttp/tapir/sbt/OpenapiCodegenPlugin.scala
@@ -2,6 +2,7 @@ package sttp.tapir.sbt
 
 import sbt._
 import Keys._
+import sbt.nio.Keys.fileInputs
 
 object OpenapiCodegenPlugin extends AutoPlugin {
 
@@ -13,12 +14,25 @@ object OpenapiCodegenPlugin extends AutoPlugin {
   override lazy val projectSettings: Seq[Setting[_]] =
     openapiCodegenScopedSettings(Compile) ++ openapiCodegenDefaultSettings
 
-  import TupleSyntax._
-
   def openapiCodegenScopedSettings(conf: Configuration): Seq[Setting[_]] = inConfig(conf)(
     Seq(
-      generateTapirDefinitions := codegen.value,
-      sourceGenerators += (codegen.taskValue).map(_.map(_.toPath.toFile))
+      generateTapirDefinitions := Def.task {
+        val c = openapiOpenApiConfiguration.value
+        val srcDir = sourceManaged.value
+        val sv = scalaVersion.value
+        val log = sLog.value
+        val cacheDir = streams.value.cacheDirectory
+        val swaggerFiles = (c.swaggerFile +: c.additionalPackages.map(_._2)).filter(_.exists()).toSet
+        FileFunction.cached(cacheDir / "openapi-inputs", FileInfo.hash) { _ =>
+          log.info("Generating OpenAPI sources...")
+          codegen(c, srcDir, sv).toSet
+        }(swaggerFiles).toSeq
+      }.value,
+      generateTapirDefinitions / fileInputs ++= {
+        (openapiSwaggerFile.value +: openapiAdditionalPackages.value.map(_._2))
+          .map(f => Glob(f.toPath))
+      },
+      sourceGenerators += generateTapirDefinitions.taskValue
     )
   )
 
@@ -55,57 +69,39 @@ object OpenapiCodegenPlugin extends AutoPlugin {
     standardParamSetting
   )
 
-  private def codegen =
-    Def.task {
-      val log = sLog.value
-      log.info("Zipping file...")
-      (
-        openapiOpenApiConfiguration,
-        sourceManaged,
-        streams,
-        scalaVersion
-      ).flatMap {
-        (
-            c: OpenApiConfiguration,
-            srcDir: File,
-            taskStreams: TaskStreams,
-            sv: String
-        ) =>
-          def genTask(swaggerFile: File, packageName: String, directoryName: Option[String] = None) =
-            OpenapiCodegenTask(
-              swaggerFile,
-              packageName,
-              c.objectName,
-              c.useHeadTagForObjectName,
-              c.jsonSerdeLib,
-              c.xmlSerdeLib,
-              c.streamingImplementation,
-              c.validateNonDiscriminatedOneOfs,
-              c.maxSchemasPerFile,
-              c.generateEndpointTypes,
-              c.disableValidatorGeneration,
-              c.useCustomJsoniterSerdes,
-              srcDir,
-              taskStreams.cacheDirectory,
-              sv.startsWith("3"),
-              directoryName
-            )
+  private def codegen(c: OpenApiConfiguration, srcDir: File, sv: String): Seq[File] = {
+    def genTask(swaggerFile: File, packageName: String, directoryName: Option[String] = None) =
+      OpenapiCodegenTask(
+        swaggerFile,
+        packageName,
+        c.objectName,
+        c.useHeadTagForObjectName,
+        c.jsonSerdeLib,
+        c.xmlSerdeLib,
+        c.streamingImplementation,
+        c.validateNonDiscriminatedOneOfs,
+        c.maxSchemasPerFile,
+        c.generateEndpointTypes,
+        c.disableValidatorGeneration,
+        c.useCustomJsoniterSerdes,
+        srcDir,
+        sv.startsWith("3"),
+        directoryName
+      )
 
-          val overriddenDefaultLocation = c.additionalPackages.find(_._2 == c.swaggerFile)
-          val defaultIsRedeclared = overriddenDefaultLocation.isDefined
-          val maybeDefaultFileTask = {
-            if (defaultIsRedeclared) {
-              System.err.println(s"WARN: Default swagger file is redeclared. Writing to ${overriddenDefaultLocation.get._1}")
-              Nil
-            } else if (!c.swaggerFile.exists() && c.additionalPackages.nonEmpty) {
-              System.err.println(s"WARN: File not found: ${c.swaggerFile.toPath}. Skipping default, only writing additional packages.")
-              Nil
-            } else Seq(genTask(c.swaggerFile, c.packageName).file)
-          }
-          (maybeDefaultFileTask ++ c.additionalPackages.map { case (pkg, defns) =>
-            genTask(defns, pkg, Some(pkg.replace('.', '/'))).file
-          })
-            .reduceLeft((l, r) => l.flatMap(_l => r.map(_l ++ _)))
-      }.value
+    val overriddenDefaultLocation = c.additionalPackages.find(_._2 == c.swaggerFile)
+    val defaultIsRedeclared = overriddenDefaultLocation.isDefined
+    val maybeDefaultFiles: Seq[File] = {
+      if (defaultIsRedeclared) {
+        System.err.println(s"WARN: Default swagger file is redeclared. Writing to ${overriddenDefaultLocation.get._1}")
+        Nil
+      } else if (!c.swaggerFile.exists() && c.additionalPackages.nonEmpty) {
+        System.err.println(s"WARN: File not found: ${c.swaggerFile.toPath}. Skipping default, only writing additional packages.")
+        Nil
+      } else genTask(c.swaggerFile, c.packageName).file
     }
+    maybeDefaultFiles ++ c.additionalPackages.flatMap { case (pkg, defns) =>
+      genTask(defns, pkg, Some(pkg.replace('.', '/'))).file
+    }
+  }
 }
