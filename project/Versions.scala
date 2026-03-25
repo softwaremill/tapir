31429ee5e6315f16362b174f5e840ee55b57610a
scala-steward <scala-steward>
2026-03-25 00:19:44 +0000

    Update netty-all to 4.2.11.Final

--- project/Versions.scala
+++ project/Versions.scala
@@ -67,7 +67,7 @@ object Versions {
   val openTelemetrySemconvVersion = "1.40.0"
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.4.5"
-  val nettyAll = "4.2.10.Final"
+  val nettyAll = "4.2.11.Final"
   val logback = "1.5.32"
   val slf4j = "2.0.17"
   val jsoniter = "2.35.3"
