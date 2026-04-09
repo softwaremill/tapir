67cbb6704860af39caeca44ca4c57767ea03cfdd
scala-steward <scala-steward>
2026-04-09 00:20:36 +0000

    Update aws-lambda-java-runtime-interface-client to 2.10.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -56,7 +56,7 @@ object Versions {
   val newtype = "0.4.4"
   val monixNewtype = "0.4.1"
   val zioPrelude = "1.0.0-RC47"
-  val awsLambdaInterface = "2.6.0"
+  val awsLambdaInterface = "2.10.1"
   val armeria = "1.38.0"
   val scalaJava8Compat = "1.0.2"
   val scalaCollectionCompat = "2.13.0"
