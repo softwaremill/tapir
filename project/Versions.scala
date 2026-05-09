f24db466b03584bd6e6d381fa42767849ef5c2f6
softwaremill-ci <ci@softwaremill.com>
2026-05-09 02:56:50 +0200

    Update opentelemetry-api, ... to 1.62.0 (#5217)

--- project/Versions.scala
+++ project/Versions.scala
@@ -63,7 +63,7 @@ object Versions {
   val fs2 = "3.13.0"
   val decline = "2.6.2"
   val quicklens = "1.9.12"
-  val openTelemetry = "1.61.0"
+  val openTelemetry = "1.62.0"
   val openTelemetrySemconvVersion = "1.41.0"
   val mockServer = "5.15.0"
   val dogstatsdClient = "4.4.5"
