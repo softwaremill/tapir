903f288e518715934f30d448265d0899518d9151
QuadStingray <git@quadstingray.dev>
2026-05-11 13:26:34 +0200

    feat: add ScalarInterpreter for generating Scalar UI with tests

---
+++ docs/scalar-bundle/src/test/scala/sttp/tapir/scalar/bundle/ScalarInterpreterTest.scala
@@ -0,0 +1,122 @@
+package sttp.tapir.scalar.bundle
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import org.http4s.HttpRoutes
+import org.http4s.blaze.server.BlazeServerBuilder
+import org.http4s.server.Router
+import org.scalatest.Assertion
+import org.scalatest.funsuite.AsyncFunSuite
+import org.scalatest.matchers.should.Matchers
+import sttp.client4._
+import sttp.client4.httpclient.HttpClientSyncBackend
+import sttp.model.StatusCode
+import sttp.tapir._
+import sttp.tapir.scalar.ScalarUIOptions
+import sttp.tapir.server.http4s.Http4sServerInterpreter
+
+class ScalarInterpreterTest extends AsyncFunSuite with Matchers {
+
+  val testEndpoint: PublicEndpoint[String, Unit, String, Any] = endpoint.get
+    .in("test")
+    .in(query[String]("q"))
+    .out(stringBody)
+
+  test(s"scalar UI at root, relative path") {
+    scalarTest(Nil, Nil, useRelativePaths = true).unsafeRunSync()
+  }
+
+  test(s"scalar UI at ./docs endpoint, relative path") {
+    scalarTest(List("docs"), Nil, useRelativePaths = true).unsafeRunSync()
+  }
+
+  test("scalar UI at ./api/docs endpoint, relative path") {
+    scalarTest(List("api", "docs"), Nil, useRelativePaths = true).unsafeRunSync()
+  }
+
+  test(s"scalar UI at root, absolute path") {
+    scalarTest(Nil, Nil, useRelativePaths = false).unsafeRunSync()
+  }
+
+  test(s"scalar UI at ./docs endpoint, absolute path") {
+    scalarTest(List("docs"), Nil, useRelativePaths = false).unsafeRunSync()
+  }
+
+  test("scalar UI at ./api/docs endpoint, absolute path") {
+    scalarTest(List("api", "docs"), Nil, useRelativePaths = false).unsafeRunSync()
+  }
+
+  test(s"scalar UI at /api/v1 and empty endpoint, absolute path") {
+    scalarTest(Nil, List("api", "v1"), useRelativePaths = false).unsafeRunSync()
+  }
+
+  test(s"scalar UI at /internal route /docs endpoint, absolute path") {
+    scalarTest(List("docs"), List("internal"), useRelativePaths = false).unsafeRunSync()
+  }
+
+  test(s"scalar UI at /internal/secret route /api/docs endpoint, absolute path") {
+    scalarTest(List("api", "docs"), List("internal", "secret"), useRelativePaths = false).unsafeRunSync()
+  }
+
+  private def scalarTest(prefix: List[String], context: List[String], useRelativePaths: Boolean): IO[Assertion] = {
+    val scalarUIRoutes: HttpRoutes[IO] =
+      Http4sServerInterpreter[IO]().toRoutes(
+        ScalarInterpreter(scalarUIOptions =
+          ScalarUIOptions.default.copy(pathPrefix = prefix, contextPath = context, useRelativePaths = useRelativePaths)
+        )
+          .fromEndpoints[IO](List(testEndpoint), "The tapir library", "1.0.0")
+      )
+
+    BlazeServerBuilder[IO]
+      .bindHttp(0, "localhost")
+      .withHttpApp(Router(s"/${context.mkString("/")}" -> scalarUIRoutes).orNotFound)
+      .resource
+      .use { server =>
+        IO {
+          val port = server.address.getPort
+          val client: SyncBackend = HttpClientSyncBackend()
+
+          // test no-trailing-slash redirect to index.html
+          val resp: Response[String] = basicRequest
+            .response(asStringAlways)
+            .get(uri"http://localhost:$port/${context ++ prefix}")
+            .send(client)
+
+          resp.code shouldBe StatusCode.Ok
+          resp.history should have size (1)
+
+          val docPath =
+            if (useRelativePaths) "."
+            else (if ((context ++ prefix).isEmpty) ""
+                  else s"/${(context ++ prefix).mkString("/")}")
+          resp.body should include(s"$docPath/docs.yaml")
+
+          val redirectPath = if (useRelativePaths) {
+            prefix.lastOption match {
+              case Some(lastPrefix) => s"./$lastPrefix"
+              case None             => "."
+            }
+          } else {
+            (context ++ prefix) match {
+              case Nil => ""
+              case l   => "/" + l.mkString("/")
+            }
+          }
+          val firstHistory = resp.history.head
+          firstHistory.code shouldBe StatusCode.PermanentRedirect
+          println(firstHistory.header("Location"))
+          firstHistory.header("Location") shouldBe Some(s"$redirectPath/${ScalarUIOptions.default.htmlName}")
+
+          // test getting docs.yaml
+          val yamlResp: Response[String] = basicRequest
+            .response(asStringAlways)
+            .get(uri"http://localhost:$port/${context ++ prefix}/docs.yaml")
+            .send(client)
+
+          yamlResp.code shouldBe StatusCode.Ok
+          yamlResp.body should include("The tapir library")
+        }
+      }
+  }
+
+}
