0ed223498c3c4c0fc493a4ce02083e0969be18ca
Adam Warski <adam@warski.org>
2026-05-13 09:17:54 +0000

    Uncomment examples

--- examples/src/main/scala/sttp/tapir/examples/openapi/ScalarContextPathHttp4sServer.scala
+++ examples/src/main/scala/sttp/tapir/examples/openapi/ScalarContextPathHttp4sServer.scala
@@ -1,45 +1,45 @@
-// // {cat=OpenAPI documentation; effects=cats-effect; server=http4s; docs=Scalar}: Exposing documentation using Scalar
+// {cat=OpenAPI documentation; effects=cats-effect; server=http4s; docs=Scalar}: Exposing documentation using Scalar
 
-// //> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.18
-// //> using dep com.softwaremill.sttp.tapir::tapir-scalar-bundle:1.13.18
-// //> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.13.18
-// //> using dep org.http4s::http4s-blaze-server:0.23.16
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.13.19
+//> using dep com.softwaremill.sttp.tapir::tapir-scalar-bundle:1.13.19
+//> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.13.19
+//> using dep org.http4s::http4s-blaze-server:0.23.16
 
-// package sttp.tapir.examples.openapi
+package sttp.tapir.examples.openapi
 
-// import cats.effect.*
-// import cats.syntax.all.*
-// import org.http4s.HttpRoutes
-// import org.http4s.server.Router
-// import org.http4s.blaze.server.BlazeServerBuilder
-// import sttp.tapir.*
-// import sttp.tapir.scalar.ScalarUIOptions
-// import sttp.tapir.scalar.bundle.ScalarInterpreter
-// import sttp.tapir.server.http4s.Http4sServerInterpreter
+import cats.effect.*
+import cats.syntax.all.*
+import org.http4s.HttpRoutes
+import org.http4s.server.Router
+import org.http4s.blaze.server.BlazeServerBuilder
+import sttp.tapir.*
+import sttp.tapir.scalar.ScalarUIOptions
+import sttp.tapir.scalar.bundle.ScalarInterpreter
+import sttp.tapir.server.http4s.Http4sServerInterpreter
 
-// import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext
 
-// object ScalarContextPathHttp4sServer extends IOApp:
-//   val contextPath: List[String] = List("api", "v1")
-//   val docPathPrefix: List[String] = "scalar" :: Nil
+object ScalarContextPathHttp4sServer extends IOApp:
+  val contextPath: List[String] = List("api", "v1")
+  val docPathPrefix: List[String] = "scalar" :: Nil
 
-//   val helloWorld: PublicEndpoint[String, Unit, String, Any] =
-//     endpoint.get.in("hello").in(query[String]("name")).out(stringBody)
+  val helloWorld: PublicEndpoint[String, Unit, String, Any] =
+    endpoint.get.in("hello").in(query[String]("name")).out(stringBody)
 
-//   val routes: HttpRoutes[IO] =
-//     val scalarEndpoints = ScalarInterpreter(scalarUIOptions = ScalarUIOptions.default.contextPath(contextPath).pathPrefix(docPathPrefix))
-//       .fromEndpoints[IO](List(helloWorld), "The tapir library", "1.0.0")
+  val routes: HttpRoutes[IO] =
+    val scalarEndpoints = ScalarInterpreter(scalarUIOptions = ScalarUIOptions.default.contextPath(contextPath).pathPrefix(docPathPrefix))
+      .fromEndpoints[IO](List(helloWorld), "The tapir library", "1.0.0")
 
-//     Http4sServerInterpreter[IO]().toRoutes(helloWorld.serverLogic(name => IO(s"Hello, $name!".asRight[Unit])) :: scalarEndpoints)
-//   end routes
+    Http4sServerInterpreter[IO]().toRoutes(helloWorld.serverLogic(name => IO(s"Hello, $name!".asRight[Unit])) :: scalarEndpoints)
+  end routes
 
-//   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
-//   override def run(args: List[String]): IO[ExitCode] =
-//     BlazeServerBuilder[IO]
-//       .withExecutionContext(ec)
-//       .bindHttp(8080, "localhost")
-//       .withHttpApp(Router(s"/${contextPath.mkString("/")}" -> routes).orNotFound)
-//       .resource
-//       .use { _ => IO.println(s"go to: http://127.0.0.1:8080/${(contextPath ++ docPathPrefix).mkString("/")}") *> IO.never }
-//       .as(ExitCode.Success)
+  override def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .withExecutionContext(ec)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(Router(s"/${contextPath.mkString("/")}" -> routes).orNotFound)
+      .resource
+      .use { _ => IO.println(s"go to: http://127.0.0.1:8080/${(contextPath ++ docPathPrefix).mkString("/")}") *> IO.never }
+      .as(ExitCode.Success)
