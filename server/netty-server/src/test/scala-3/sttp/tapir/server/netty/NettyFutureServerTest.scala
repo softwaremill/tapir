331287aeb32e06a79e6ef594f7d709403ec7200c
ajozwik <andrzej.jozwik@gmail.com>
2026-06-11 20:40:59 +0200

    Working code with java 21+ (Osx Thread.ofVirtual limitation)

--- server/netty-server/src/test/scala-3/sttp/tapir/server/netty/NettyFutureServerTest.scala
+++ server/netty-server/src/test/scala-3/sttp/tapir/server/netty/NettyFutureServerTest.scala
@@ -1,3 +1,3 @@
 package sttp.tapir.server.netty
 
-class NettyFutureServerTest extends NettyFutureServerBaseTest(true)
+class NettyFutureServerTest extends NettyFutureServerBaseTest(scala.util.Properties.isJavaAtLeast("21")) // Thread.ofVirtual
