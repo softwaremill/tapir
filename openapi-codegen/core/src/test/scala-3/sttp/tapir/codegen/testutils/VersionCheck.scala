f45a52c91abd7e7dcdaedf7b31a6e024c0360b1c
Hugh Simpson <hsimpson@rzsoftware.com>
2026-06-26 13:27:49 +0100

    skip some tests on java 11 + zio

--- openapi-codegen/core/src/test/scala-3/sttp/tapir/codegen/testutils/VersionCheck.scala
+++ openapi-codegen/core/src/test/scala-3/sttp/tapir/codegen/testutils/VersionCheck.scala
@@ -1,5 +1,7 @@
 package sttp.tapir.codegen.testutils
 
 object VersionCheck {
-  def runTest(jsonSerde: String)(test: => Unit): Unit = test
+  def runTest(jsonSerde: String)(test: => Unit): Unit =
+    if (jsonSerde == "zio" && Option(System.getProperty("java.version")).exists(_.startsWith("11"))) ()
+    else test
 }
