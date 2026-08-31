96529ba81748746f0037c089781b2f586f04e8e4
rwalerow <robert.walerowicz@virtuslab.com>
2026-08-31 13:24:30 +0200

    Use stateful handler instead of channle attribute

--- server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/RequestBodyCompletedTracker.scala
+++ server/netty-server/src/main/scala/sttp/tapir/server/netty/internal/RequestBodyCompletedTracker.scala
@@ -7,20 +7,22 @@ import io.netty.util.AttributeKey
 /** Has to be included in the pipeline before HttpStreamsServerHandler to observe LastHttpContent and HttpRequest
   */
 object RequestBodyCompletedTracker {
-  val key = "tapir.requestbody.completed"
-
-  val BodyComplete: AttributeKey[Boolean] = AttributeKey.valueOf[Boolean](key)
 
   def wasRequestBodyFullyReceived(ctx: ChannelHandlerContext): Boolean =
-    Option(ctx.channel().attr(RequestBodyCompletedTracker.BodyComplete).get()).getOrElse(true)
+    ctx.pipeline().get(classOf[RequestBodyCompletedTracker]) match {
+      case t: RequestBodyCompletedTracker => t.bodyFullyReceived
+      case _                              => true
+    }
 }
 
 class RequestBodyCompletedTracker extends ChannelInboundHandlerAdapter {
 
+  private[internal] var bodyFullyReceived: Boolean = true
+
   override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = {
     msg match {
-      case _: LastHttpContent => ctx.channel().attr(RequestBodyCompletedTracker.BodyComplete).set(true)
-      case _: HttpRequest     => ctx.channel().attr(RequestBodyCompletedTracker.BodyComplete).set(false)
+      case _: LastHttpContent => bodyFullyReceived = true
+      case _: HttpRequest     => bodyFullyReceived = false
       case _                  => ()
     }
     val _ = ctx.fireChannelRead(msg)
